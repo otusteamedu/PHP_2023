4c244b1080dad2009f43c6f56f958e94f67d3b50
VladimirPavlenko <v.pavlenko@worksolutions.ru>
2023-02-03 00:20:54 +0300

    Формат кода в index.php

--- project/index.php
+++ project/index.php
@@ -5,62 +5,62 @@
     <title>HomeWork 1</title>
 </head>
 <body>
-    <div>
-        <?php
+<div>
+    <?php
 
-        $br = '<br>';
-        echo "Проверка соединения с postgres:" . $br;
-        echo "Создание таблицы test и вывод списка table-column:" . $br;
+    $br = '<br>';
+    echo "Проверка соединения с postgres:" . $br;
+    echo "Создание таблицы test и вывод списка table-column:" . $br;
 
-        $dbname = $_ENV["DB_DATABASE"];
-        $user = $_ENV["DB_USERNAME"];
-        $password = $_ENV["DB_PASSWORD"];
+    $dbname = $_ENV["DB_DATABASE"];
+    $user = $_ENV["DB_USERNAME"];
+    $password = $_ENV["DB_PASSWORD"];
 
-        $dsn = "pgsql:host=postgres;port=5432;dbname=$dbname;user=$user;password=$password";
+    $dsn = "pgsql:host=postgres;port=5432;dbname=$dbname;user=$user;password=$password";
 
-        try {
-            $conn = new PDO($dsn);
+    try {
+        $conn = new PDO($dsn);
 
-            $sql = "CREATE TABLE IF NOT EXISTS test (
+        $sql = "CREATE TABLE IF NOT EXISTS test (
                         id serial PRIMARY KEY,
                         number character varying(20) NOT NULL UNIQUE,
                         name character varying(20) NOT NULL
                         )";
-            $conn->exec($sql);
+        $conn->exec($sql);
 
-            $res = $conn->query("select table_name, column_name from information_schema.columns where table_schema='public'");
+        $res = $conn->query("select table_name, column_name from information_schema.columns where table_schema='public'");
 
-            while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-                echo($row["table_name"].'-'.$row["column_name"] . $br);
-            }
-
-        } catch (PDOException $e){
-            echo $e->getMessage();
+        while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+            echo($row["table_name"] . '-' . $row["column_name"] . $br);
         }
 
-        echo "Проверка соединения с redis:" . $br;
-        echo "Добавление в redis записи 'Test Redis value' с ключом 'testRedisKey':" . $br;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
 
-        $redis = new Redis();
+    echo "Проверка соединения с redis:" . $br;
+    echo "Добавление в redis записи 'Test Redis value' с ключом 'testRedisKey':" . $br;
 
-        try {
-            $redis->connect('redis');
-            $redis->set('testRedisKey', 'Test Redis value');
-            $value = $redis->get('testRedisKey');
-            echo $value . $br;
-        } catch (RedisException $e) {
-            echo $e->getMessage();
-        }
+    $redis = new Redis();
 
-        echo "Проверка соединения с memcached:" . $br;
-        echo "Добавление в memcached записи с ключом testMemcachedKey:" . $br;
+    try {
+        $redis->connect('redis');
+        $redis->set('testRedisKey', 'Test Redis value');
+        $value = $redis->get('testRedisKey');
+        echo $value . $br;
+    } catch (RedisException $e) {
+        echo $e->getMessage();
+    }
 
-        $memcached = new Memcached;
+    echo "Проверка соединения с memcached:" . $br;
+    echo "Добавление в memcached записи с ключом testMemcachedKey:" . $br;
 
-        $memcached->addServer('memcached', 11211);
-        $memcached->set("testMemcachedKey", "Test Memcached value");
-        echo $memcached->get("testMemcachedKey") . $br;
-        ?>
-    </div>
+    $memcached = new Memcached;
+
+    $memcached->addServer('memcached', 11211);
+    $memcached->set("testMemcachedKey", "Test Memcached value");
+    echo $memcached->get("testMemcachedKey") . $br;
+    ?>
+</div>
 </body>
 </html>

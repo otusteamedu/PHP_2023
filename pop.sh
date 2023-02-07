85b9c63a15432771b28b8ffbc7806d150ee67dc6
user <user@user.user>
2023-02-07 22:18:52 +0400

    add pop.sh

---
+++ pop.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#tail -n +2 data | awk -F' ' '{print($3)}'
+#tail -n +2 data | awk '{print($3)}' | sort | uniq -c | sort -r -k1 | head -n3 | awk '{print($2)}'
+
+print_help () {
+    echo "Usage: $0 filename"
+}
+
+if [ -n "$1" ]
+then
+  if [ ! -f "$1" ]; then
+    echo "Error: $1 no exists."
+    exit 1;
+  fi
+
+  tail -n +2 "$1" | awk '{print($3)}' | sort | uniq -c | sort -r -k1 | head -n3 | awk '{print($2)}'
+  exit 0
+else
+  echo -e "Not filename.\n"
+  print_help
+  exit 1
+fi

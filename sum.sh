c1e96c37033ba5aafd49d2442a7145f542653473
Vyacheslav Shevchenko <webdev2030@gmail.com>
2023-02-13 14:56:24 +0600

    hw2 - доработка. Сумма в одну строку через awk.

--- sum.sh
+++ sum.sh
@@ -13,54 +13,6 @@ function str_repeat() {
   echo "$result"
 }
 
-function floatSum() {
-  #вытаскиваем целую часть
-  a1=$(echo "$1" | awk -F "." '{print $1}')
-  b1=$(echo "$2" | awk -F "." '{print $1}')
-
-  #вытаскиваем дробную часть
-  a2=$(echo "$1" | awk -F "." '{print $2}')
-  b2=$(echo "$2" | awk -F "." '{print $2}')
-
-  if [[ ${#a2} -le 0 ]]; then
-    a2=0
-  fi
-
-  if [[ ${#b2} -le 0 ]]; then
-      b2=0
-  fi
-
-  # уравниваем длину дробной части
-  if [[ ${#a2} -ge ${#b2} ]]; then
-    maxLength=${#a2}
-  else
-    maxLength=${#b2}
-  fi
-
-  if [[ $maxLength > ${#a2} ]]; then
-    a2+="$(str_repeat '0' $((maxLength-${#a2})))"
-  fi
-
-  if [[ $maxLength > ${#b2} ]]; then
-      b2+="$(str_repeat '0' $((maxLength-${#b2})))"
-  fi
-
-  #складываем части
-  c1=$((a1+b1))
-  c2=$((a2+b2))
-
-  if [[ ${#c2} > $maxLength ]]; then
-    c1=$((${c2:0:${#c2}-$maxLength}+c1))
-    c2=${c2:${#c2}-$maxLength:${#c2}}
-  fi
-
-  if [[ $c2 -gt 0 ]]; then
-    echo "$c1.$c2"
-  else
-    echo "$c1"
-  fi
-}
-
 if [ $# -ne 2 ]; then
   echo "Ошибка: необходимо ввести два аргумента. Оба аргумента должны быть числом."
   exit 1
@@ -77,9 +29,4 @@ if ! [[ $a =~ $isNumberRegExp ]] || ! [[ $b =~ $isNumberRegExp ]]; then
 fi
 
 #если оба числа целые
-if [[ $a =~ $isIntRegExp ]] && [[ $b =~ $isIntRegExp ]]; then
-  echo "Сумма "$((a+b))
-  exit 0
-else
-  echo "Сумма "$(floatSum "$a" "$b") #можно вызывать только эту функцию она посчитает и целые числа и дробные, но думаю оптимальнее использовать только с дробями.
-fi
+echo "$a $b" | awk '{c=$1+$2; printf "Сумма %s", c}'

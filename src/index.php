d652ed9c2605ee57094658c9548f16ae18fb2a3c
Ivan Mitronov <mitronov.i@2wtrade.com>
2023-02-14 22:20:06 +0300

    [!] поправил ошибку phpcs

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 use Imitronov\CbrfCurrencyRate\Client;
 

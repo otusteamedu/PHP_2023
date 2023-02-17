f34dcb20467eabe9b5bc1e17996c01f42c65d7c2
Vyacheslav Shevchenko <webdev2030@gmail.com>
2023-02-17 22:09:20 +0600

    hw5 - php

--- data/htdocs/public/index.php
+++ data/htdocs/public/index.php
@@ -1,55 +1,31 @@
 <?php
-
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
-
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
 require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
+if(strtolower($_SERVER['REQUEST_METHOD']) == 'post') {
+    $incomingStr = $_POST['string'];
+    $pattern = '(()()()()))((((()()()))(()()()(((()))))))';
+    $openCnt = substr_count($pattern, '(');
+    $closeCnt = substr_count($pattern, ')');
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+    try {
+        if (empty($incomingStr)) {
+            throw new Exception('Строка пустая');
+        }
 
-$kernel = $app->make(Kernel::class);
+        if($openCnt != substr_count($incomingStr, '(')) {
+            throw new Exception('Количество открытых скобок не верное');
+        }
 
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
+        if($closeCnt != substr_count($incomingStr, ')')) {
+            throw new Exception('Количество закрытых скобок не верное');
+        }
 
-$kernel->terminate($request, $response);
+        echo "Everything ok";
+    } catch (Exception $e) {
+        http_response_code(400);
+        echo $e->getMessage();
+    }
+
+
+}
+

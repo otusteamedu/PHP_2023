7dbc3ce14adf21a0a1da68fd1d4f3346a743a3e6
Vyacheslav Shevchenko <webdev2030@gmail.com>
2023-02-18 15:09:46 +0600

    hw7 - решил задачу, насколько оптимально не знаю.

---
+++ data/htdocs/merge-two-sorted-lists.php
@@ -0,0 +1,156 @@
+<?php
+function printList(?ListNode $node): void
+{
+    if ($node != null) {
+        echo "\nThe list contains: ";
+        while ($node != null) {
+            echo $node->val . " ";
+            $node = $node->next;
+        }
+    } else {
+        echo "\nThe list is empty.";
+    }
+}
+
+/**
+ * Definition for a singly-linked list.
+ */
+class ListNode
+{
+    public $val = 0;
+    public ?ListNode $next = null;
+
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists(?ListNode $list1, ?ListNode $list2) {
+        if(is_null($list1) && is_null($list2)) {
+            return null;
+        } elseif(is_null($list1)) {
+            return $list2;
+        } elseif(is_null($list2)) {
+            return $list1;
+        }
+
+        if ($list2->val >= $list1->val) {
+            $tmp1 = $list1;
+            $tmp2 = $list2;
+        } else {
+            $tmp1 = $list2;
+            $tmp2 = $list1;
+        }
+
+        $list3 = new ListNode($tmp1->val);
+        $tmp1 = $tmp1->next;
+        $tmp3 = $list3;
+
+        while ($tmp1 != null) {
+            while ($tmp2 != null && $tmp2->val <= $tmp1->val) {
+                $tmp3->next = new ListNode($tmp2->val);
+                $tmp3 = $tmp3->next;
+                $tmp2 = $tmp2->next;
+            }
+
+            $tmp3->next = new ListNode($tmp1->val);
+            $tmp3 = $tmp3->next;
+
+            $tmp1 = $tmp1->next;
+        }
+
+        while($tmp2) {
+            $tmp3->next = new ListNode($tmp2->val);
+            $tmp3 = $tmp3->next;
+            $tmp2 = $tmp2->next;
+        }
+
+        return $list3;
+    }
+
+    /**
+     * @deprecated
+     * @param ListNode $list
+     * @param ListNode $newNode
+     * @return void
+     */
+    public function addToList(ListNode $list, ListNode $newNode)
+    {
+        if (is_null($list->next)) {
+            $list->next = $newNode;
+        } else {
+            $temp = $list->next;
+            while ($temp->next != null)
+                $temp = $temp->next;
+            $temp->next = $newNode;
+        }
+    }
+}
+
+$solution = new Solution();
+$list1 = new ListNode(1);
+$solution->addToList($list1, new ListNode(2));
+$solution->addToList($list1, new ListNode(4));
+
+$list2 = new ListNode(1);
+$solution->addToList($list2, new ListNode(3));
+$solution->addToList($list2, new ListNode(4));
+
+$first3 = $solution->mergeTwoLists($list1, $list2);
+
+printList($list1);
+printList($list2);
+
+printList($first3);
+
+echo "\n----";
+
+$solution = new Solution();
+$list1 = null;
+$list2 = new ListNode(0);
+
+$first3 = $solution->mergeTwoLists($list1, $list2);
+
+printList($list1);
+printList($list2);
+
+printList($first3);
+
+echo "\n----";
+
+$solution = new Solution();
+$list1 = new ListNode(2);;
+$list2 = new ListNode(1);
+
+$first3 = $solution->mergeTwoLists($list1, $list2);
+
+printList($list1);
+printList($list2);
+
+printList($first3);
+
+echo "\n----";
+
+$solution = new Solution();
+$list1 = new ListNode(-9);;
+$solution->addToList($list1, new ListNode(3));
+
+$list2 = new ListNode(5);
+$solution->addToList($list2, new ListNode(7));
+
+$first3 = $solution->mergeTwoLists($list1, $list2);
+
+printList($list1);
+printList($list2);
+
+printList($first3);

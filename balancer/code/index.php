65cbc80dd6a273cf06283cfa04891cac8ab879aa
Mase4ka <mase4ka87@gmail.com>
2023-02-27 20:17:49 +0300

    Balancer

---
+++ balancer/code/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>OTUS homework #1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+    <?= "Запрос обработал контейнер: " . $_SERVER['HOSTNAME']; ?>
+</div>
+
+
+</body>
+</html>
+
+

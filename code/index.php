7c94cd7630292966f1a23dd8091f75949cb8b3db
fatyanova.maria <fatyanova.m@i20.team>
2023-03-03 00:53:39 +0800

    Сделать верификацию строки со скобками

--- code/index.php
+++ code/index.php
@@ -1,3 +1,45 @@
 <?php
 
-phpinfo();
+function checkBrackets($string)
+{
+    $counter = 0;
+    for ($i = 0; $i < mb_strlen($string); $i++) {
+        if ($string[$i] === '(') {
+            $counter++;
+        } else {
+            $counter--;
+        }
+
+        if ($counter < 0) {
+            return false;
+        }
+    }
+
+    return $counter === 0;
+}
+
+function checkPattern($string) {
+    preg_match('/[^)(]/', $string, $matches);
+    return empty($matches);
+}
+
+if (empty($_POST['string'])) {
+    http_response_code('400');
+    echo "Empty string";
+    return;
+}
+
+if (!checkPattern($_POST['string'])) {
+    http_response_code('400');
+    echo "String contains extra characters";
+    return;
+}
+
+if (!checkBrackets($_POST['string'])) {
+    http_response_code('400');
+    echo "Brackets are placed incorrectly";
+    return;
+}
+
+http_response_code('200');
+echo "Ok!";

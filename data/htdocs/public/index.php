71a69f39ab2e28e84a4eb820c11ac90f5ccd719a
Vyacheslav Shevchenko <webdev2030@gmail.com>
2023-03-02 18:52:42 +0600

    hw6 - из php://input

--- data/htdocs/public/index.php
+++ data/htdocs/public/index.php
@@ -3,9 +3,13 @@ ini_set('display_errors', 1);
 require "../vendor/autoload.php";
 
 $validator = new \Sva\EmailValidator();
+$separator = ',';
+
+$input = file_get_contents('php://input');
+$emails = explode($separator, $input);
 
 $result = [];
-foreach((new \Sva\FileReader)->getLines('../emails.txt') as $key => $email) {
+foreach($emails as $key => $email) {
     $result[$email] = $validator->validate($email);
 };
 echo json_encode($result);

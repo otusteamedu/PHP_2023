bf1cb167e560dea0e9e13df56932690d34f21a3b
kirill.lobkovsky <kiphirrr@gmail.com>
2023-03-03 22:25:39 +0300

    hw1-1: docker-compose

---
+++ src/classes/DBConfig.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Klobkovsky\Hw11;
+
+class DBConfig
+{
+    public static function getNormalizedParams($params) {
+        return [
+            $params['host'],
+            $params['user'],
+            $params['password'],
+            $params['database'],
+            $params['port'],
+        ];
+    }
+}

77aa04e6ddc1b5a5007efd3d43220fb0332ad487
builov <builov@inbox.ru>
2023-03-05 17:59:40 +0300

    add empty lines

--- hw6/index.php
+++ hw6/index.php
@@ -11,7 +11,3 @@ $path = __DIR__ . '/test.txt';
 $result = $ev->validate($path, 'file_local');
 //$result = $ev->validate('https://www.php.net/manual/en/filter.examples.validation.php', 'file_remote');
 //$result = $ev->validate('vnesterenko@basicdecor.ru,agorbunov@basicdecor.ru,o.furletova@basicdecor.ru', 'string');
-
-
-
-

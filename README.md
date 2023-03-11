9136c246bcb423e35e75282c127acab91292fd34
twent <twentprod@gmail.com>
2023-03-11 22:34:21 +0300

    ✅ HW7 Complete

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# PHP_2023
+# PHP_2023 HW 7 - Cinema booking app DB structure
 
-https://otus.ru/lessons/razrabotchik-php/?utm_source=github&utm_medium=free&utm_campaign=otus
+### How to run
+1. Create `.env` from `.env.example`
+2. Run `docker compose up -d`
+3. Connect to DB with credentials from `.env`
+4. Run queries from `query.sql`
+
+### DB Schema
+![cinema_app_db_schema.png](cinema_app_db_schema.png)
---
+++ query.sql
@@ -0,0 +1,60 @@
+/*
+
+Максимально прибыльный фильм
+
+*/
+
+-- Вариант 1
+SELECT
+    movies.title, SUM(sessions.price) as total
+FROM
+    movies, tickets, sessions
+WHERE
+    tickets.session_id = sessions.id
+    AND sessions.movie_id = movies.id
+    AND tickets.is_paid = true
+GROUP BY
+    movies.title
+ORDER BY
+    total DESC
+LIMIT 1;
+
+-- Вариант 2
+SELECT
+    movies.title, SUM(sessions.price) as total
+FROM
+    movies
+INNER JOIN
+    sessions ON sessions.movie_id = movies.id
+INNER JOIN
+    tickets ON tickets.session_id = sessions.id
+WHERE
+    tickets.is_paid = true
+GROUP BY
+    movies.title
+ORDER BY
+    total DESC
+LIMIT 1;
+
+-- Вариант 3
+WITH total AS (
+    SELECT
+        sessions.movie_id, SUM(sessions.price) AS total
+    FROM
+        sessions
+    INNER JOIN
+        tickets ON tickets.session_id = sessions.id
+    WHERE
+        tickets.is_paid = true
+    GROUP BY
+        sessions.movie_id
+)
+SELECT
+    movies.title, total.total
+FROM
+    movies
+INNER JOIN
+    total ON total.movie_id = movies.id
+ORDER BY
+    total DESC
+LIMIT 1;

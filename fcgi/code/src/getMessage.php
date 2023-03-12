843c96ae92ff9cd2acf449552e983fad399ade7e
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2023-03-12 18:07:18 +0300

    hw_4

---
+++ fcgi/code/src/getMessage.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+function getMessage(): string{
+    if (empty($_GET['string'])) {
+        http_response_code(400);
+        return "Empty string";
+    }
+
+    if (!checkBrackets($_GET['string'])){
+        http_response_code(400);
+        return "Brackets are placed incorrectly";
+    }
+
+    if (!checkPattern($_GET['string'])){
+        http_response_code(400);
+        return "String contains extra characters";
+    }
+
+    http_response_code(200);
+    return "Correct!";
+}
+

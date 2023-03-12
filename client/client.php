4ac29abd3282daa01f977e28a4f1d57b3186cf5a
VladimirPavlenko <v.pavlenko@worksolutions.ru>
2023-03-12 11:22:27 +0300

    9. Реализация отдельных клиента и сервера

---
+++ client/client.php
@@ -0,0 +1,47 @@
+<?php
+
+error_reporting(E_ALL);
+
+fwrite(STDOUT, "Соединение через socket" . PHP_EOL);
+
+$address = dirname(__FILE__, 3)."/socket/server.sock";
+
+fwrite(STDOUT, "Создаем socket" . PHP_EOL);
+$socket = socket_create(AF_UNIX, SOCK_STREAM, 0);
+
+if ($socket === false) {
+    $error = "Ошибка. Не удалось создать сокет: " . socket_strerror(socket_last_error());
+    fwrite(STDOUT, $error . PHP_EOL);
+}
+
+fwrite(STDOUT, "Устанавливаем соединение" . PHP_EOL);
+$result = socket_connect($socket, $address, 0);
+
+if ($result === false) {
+    $error = "Ошибка. Не удалось установить соединение: " . socket_strerror(socket_last_error($socket));
+    fwrite(STDOUT, $error . PHP_EOL);
+}
+
+$welcome = "Соединение установлено, можно отправлять сообщения! Чтобы отключиться введите stop.";
+fwrite(STDOUT, $welcome . PHP_EOL . PHP_EOL);
+
+while (true) {
+    $message = stream_get_line(STDIN, 255, PHP_EOL);
+    $write = socket_write($socket, $message, strlen($message));
+
+    if (!$write) {
+        fwrite(STDOUT, "Ошибка. Сервер закрыл соединение" . PHP_EOL);
+        break;
+    }
+
+    if (trim($message) === 'stop') {
+        break;
+    }
+
+    fwrite(STDOUT, PHP_EOL . "Ответ сервера:" . PHP_EOL);
+    $out = socket_read($socket, 255);
+    fwrite(STDOUT, $out . PHP_EOL);
+}
+
+fwrite(STDOUT, "Закрываем сокет." . PHP_EOL);
+socket_close($socket);

d421034ac1e2ddedba7633d719c639b9e8c3a2b1
Mase4ka <mase4ka87@gmail.com>
2023-03-12 19:35:29 +0300

    Merge Two Sorted Lists

---
+++ code/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require './Solution.php';
+
+$solution = new Solution();
+
+$list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+$list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+var_dump($solution->mergeTwoLists($list1, $list2));
+
+$list1 = null;
+$list2 = null;
+var_dump($solution->mergeTwoLists($list1, $list2));
+
+$list1 = null;
+$list2 = new ListNode(0);
+var_dump($solution->mergeTwoLists($list1, $list2));

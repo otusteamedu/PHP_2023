499139e8a823be3871acc9c73641e1611e06a97d
VladimirPavlenko <v.pavlenko@worksolutions.ru>
2023-03-13 22:17:54 +0300

    10. Добавил сущность Price. Привязал ее к Place. Изменил в Ticket поле price на totalPrice

--- demo.sql
+++ demo.sql
@@ -6,13 +6,15 @@ INSERT INTO `rows` (number, hall_id) VALUES
      (1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2),
      (1, 3), (2, 3), (3, 3), (4, 3), (5, 3), (6, 3);
 
-INSERT INTO places (number, price, row_id) VALUES
-     (1, 200, 1), (2, 200, 1), (3, 200, 1), (4, 200, 1), (5, 200, 1), (6, 200, 1),
-     (7, 250, 2), (8, 250, 2), (9, 250, 2), (10, 250, 2), (11, 250, 2), (12, 250, 2),
-     (13, 300, 3), (14, 300, 3), (15, 300, 3), (16, 300, 3), (17, 300, 3), (18, 300, 3),
-     (1, 200, 4), (2, 200, 4), (3, 200, 4), (4, 200, 4), (5, 200, 4), (6, 200, 4),
-     (7, 250, 5), (8, 250, 5), (9, 250, 5), (10, 250, 5), (11, 250, 5), (12, 250, 5),
-     (13, 300, 6), (14, 300, 6), (15, 300, 6), (16, 300, 6), (17, 300, 6), (18, 300, 6);
+INSERT INTO prices (amount) VALUES (200), (250), (300);
+
+INSERT INTO places (number, price_id, row_id) VALUES
+     (1, 1, 1), (2, 1, 1), (3, 1, 1), (4, 1, 1), (5, 1, 1), (6, 1, 1),
+     (7, 2, 2), (8, 2, 2), (9, 2, 2), (10, 2, 2), (11, 2, 2), (12, 2, 2),
+     (13, 3, 3), (14, 3, 3), (15, 3, 3), (16, 3, 3), (17, 3, 3), (18, 3, 3),
+     (1, 1, 4), (2, 1, 4), (3, 1, 4), (4, 1, 4), (5, 1, 4), (6, 1, 4),
+     (7, 2, 5), (8, 2, 5), (9, 2, 5), (10, 2, 5), (11, 2, 5), (12, 2, 5),
+     (13, 3, 6), (14, 3, 6), (15, 3, 6), (16, 3, 6), (17, 3, 6), (18, 3, 6);
 
 INSERT INTO films (name, release_date, country_production, duration) VALUES
     ('Avatar', '2022-08-02', 'USA', 120),
@@ -30,18 +32,18 @@ INSERT INTO sessions (date_start, date_end, hall_id, film_id) VALUES
      ('2023-03-01 12:00:00', '2023-03-01 14:00:00', 1, 3),
      ('2023-03-01 14:00:00', '2023-03-01 16:00:00', 1, 1);
 
-INSERT INTO tickets (price, payed, row_id, place_id, session_id, customer_id) VALUES
-    (250, 1, 1, 1, 1, 1),
-    (250, 1, 1, 2, 1, 1),
-    (250, 1, 1, 3, 1, 1),
-    (250, 1, 1, 4, 1, 1),
-    (250, 1, 1, 5, 1, 2),
-    (250, 1, 1, 6, 1, 2),
-    (250, 1, 2, 7, 2, 2),
-    (250, 1, 2, 8, 2, 2),
-    (250, 1, 2, 9, 3, 3),
-    (250, 1, 2, 10, 3, 3),
-    (250, 1, 2, 11, 3, 3),
-    (250, 1, 2, 12, 3, 3);
+INSERT INTO tickets (total_price, payed, row_id, place_id, session_id, customer_id) VALUES
+    (270, 1, 1, 1, 1, 1),
+    (270, 1, 1, 2, 1, 1),
+    (270, 1, 1, 3, 1, 1),
+    (270, 1, 1, 4, 1, 1),
+    (270, 1, 1, 5, 1, 2),
+    (270, 1, 1, 6, 1, 2),
+    (270, 1, 2, 7, 2, 2),
+    (270, 1, 2, 8, 2, 2),
+    (270, 1, 2, 9, 3, 3),
+    (270, 1, 2, 10, 3, 3),
+    (270, 1, 2, 11, 3, 3),
+    (270, 1, 2, 12, 3, 3);
 
-SELECT f.id, f.name, SUM(t.price) AS sum FROM films AS f JOIN sessions AS s on f.id = s.film_id JOIN tickets AS t on s.id = t.session_id WHERE t.payed=1 GROUP BY f.id ORDER BY sum DESC LIMIT 1;
+SELECT f.id, f.name, SUM(t.total_price) AS sum FROM films AS f JOIN sessions AS s on f.id = s.film_id JOIN tickets AS t on s.id = t.session_id WHERE t.payed=1 GROUP BY f.id ORDER BY sum DESC LIMIT 1;

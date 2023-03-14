e19526e696e3ede058f07d9d1cfab32cd3b75201
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2023-03-14 23:53:13 +0300

    psr-12

--- fcgi/code/src/getMessage.php
+++ fcgi/code/src/getMessage.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 function getMessage(): string
 {
-    if (empty($_GET['string'])) return generateResponse(400,'Empty string');
+    if (empty($_GET['string']))
+        return generateResponse(400, 'Empty string');
 
-    if (!checkBrackets($_GET['string'])) return generateResponse(400,'Brackets are placed incorrectly');
+    if (!checkBrackets($_GET['string']))
+        return generateResponse(400, 'Brackets are placed incorrectly');
 
-    if (!checkPattern($_GET['string'])) return generateResponse(400,'String contains extra characters');
+    if (!checkPattern($_GET['string']))
+        return generateResponse(400, 'String contains extra characters');
 
     http_response_code(200);
     return "Correct!";

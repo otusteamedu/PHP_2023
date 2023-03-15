fb584907326ab9be47e6ac10175b1df07a24c938
Daniil Gibadullin <daniil.gibadullin@mail.ru>
2023-03-15 23:09:08 +0300

    hw_4

--- fcgi/code/src/generator.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-function generateResponse(int $code, string $message): void
-{
-    http_response_code($code);
-    echo json_encode(['error' => $message]);
-}
--- fcgi/code/src/getMessage.php
+++ fcgi/code/src/getMessage.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 function getMessage(): string
 {
-    if (empty($_GET['string']))
-        return generateResponse(400, 'Empty string');
+    if (empty($_GET['string'])) {
+        http_response_code(400);
+        return "Empty string";
+    }
 
-    if (!checkBrackets($_GET['string']))
-        return generateResponse(400, 'Brackets are placed incorrectly');
+    if (!checkBrackets($_GET['string'])) {
+        http_response_code(400);
+        return "Brackets are placed incorrectly";
+    }
 
-    if (!checkPattern($_GET['string']))
-        return generateResponse(400, 'String contains extra characters');
+    if (!checkPattern($_GET['string'])) {
+        http_response_code(400);
+        return "String contains extra characters";
+    }
 
     http_response_code(200);
     return "Correct!";

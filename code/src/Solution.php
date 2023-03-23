1dad4e5960ec068d00aa01a1f4a585624b0e0359
fatyanova.maria <fatyanova.m@i20.team>
2023-03-23 22:22:04 +0800

    Оптимизировать решение

--- code/src/Solution.php
+++ code/src/Solution.php
@@ -21,35 +21,17 @@ class Solution
         $result_head = new ListNode();
         $result = $result_head;
         while (!empty($list1) || !empty($list2)) {
-            if ((!empty($list1) && !empty($list2)) && ($list1->val <= $list2->val)) {
-                $result_head->val = $list1->val;
-                $result_head->next = new ListNode($list2->val);
+            if ((!empty($list1) && !empty($list2) && ($list1->val <= $list2->val)) || empty($list2)) {
+                $result_head->next = $list1;
                 $list1 = $list1->next;
-            } elseif (empty($list2)) {
-                $result_head->val = $list1->val;
-                $list1 = $list1->next;
-                if (!empty($list1)) {
-                    $result_head->next = new ListNode();
-                } else {
-                    $result_head->next = null;
-                }
-            } elseif ((!empty($list1)) && ($list1->val > $list2->val)) {
-                $result_head->val = $list2->val;
-                $result_head->next = new ListNode($list1->val);
+            } elseif ((!empty($list1) && ($list1->val > $list2->val)) || empty($list1)) {
+                $result_head->next = $list2;
                 $list2 = $list2->next;
-            } elseif (empty($list1)) {
-                $result_head->val = $list2->val;
-                $list2 = $list2->next;
-                if (!empty($list2)) {
-                    $result_head->next = new ListNode();
-                } else {
-                    $result_head->next = null;
-                }
             }
 
             $result_head = $result_head->next;
         }
 
-        return $result;
+        return $result->next;
     }
 }

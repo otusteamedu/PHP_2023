5ebce2017382d782a7fad83e0263f286cbce5e73
Pavel Zagainov <p.zagaynov@vk.team>
2023-03-24 23:16:44 +0300

    [hw5] Добавить балансировку

--- public/index.php
+++ public/index.php
@@ -1,31 +1,33 @@
 <?php
 
+session_start();
 $string = $_POST['string'] ?? '';
 
-function check(string $string): bool {
-  if (!$string) {
-    return false;
-  }
-
-  $counter = 0;
-  for ($i = 0; $i < strlen($string); $i++) {
-    if ($string[$i] === '(') {
-      $counter++;
-    } elseif ($string[$i] === ')') {
-      $counter--;
+function check(string $string): bool
+{
+    if (!$string) {
+        return false;
     }
-    if ($counter < 0) {
-      return false;
-    }
-  }
 
-  return $counter === 0;
+    $counter = 0;
+    for ($i = 0; $i < strlen($string); $i++) {
+        if ($string[$i] === '(') {
+            $counter++;
+        } elseif ($string[$i] === ')') {
+            $counter--;
+        }
+        if ($counter < 0) {
+            return false;
+        }
+    }
+
+    return $counter === 0;
 }
 
 if (check($string)) {
-  http_response_code(200);
-  echo 'OK';
+    http_response_code(200);
+    echo 'OK';
 } else {
-  http_response_code(400);
-  echo 'Bad request';
+    http_response_code(400);
+    echo 'Bad request';
 }

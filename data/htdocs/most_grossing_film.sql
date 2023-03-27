85b02fab6b842c4bd876adcb9cfe6ad16e2250f0
Vyacheslav Shevchenko <webdev2030@gmail.com>
2023-03-28 00:09:25 +0600

    hw10 - цены на билет

--- data/htdocs/most_grossing_film.sql
+++ data/htdocs/most_grossing_film.sql
@@ -1,3 +1,17 @@
+-- цены билеты
+SELECT
+    f.id as film_id,
+    f.name as film_name,
+    s.during_time as during_time,
+    ts.name as ticket_name,
+    ts.price as ticket_price
+
+FROM films f
+         INNER JOIN sessions s on f.id = s.film_id
+         INNER JOIN tickets t on t.session_id = s.id
+         INNER JOIN tickets_sessions ts on ts.session_id = s.id
+ORDER BY during_time;
+
 -- фильмы по прибыльности
 SELECT
     f.id as film_id,

2a369ade42e5b798dda79c11bcc1ecbecf5d8ceb
aleksey <pav@el-plat.ru>
2023-03-28 16:24:05 +0400

    solution

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# HW06
+# HW08
 
 ## Запуск
 1. Запуск контейнеров: `docker-compose up -d`
 1. Установка зависимостей: `docker-compose exec php composer install`
 1. `docker-compose exec php php ./public/index.php`
+
+[Leetcode](https://leetcode.com/problems/merge-two-sorted-lists/submissions/923592665)
+
+Сложность `O(n)`
--- app/src/Solution.php
+++ app/src/Solution.php
@@ -13,7 +13,41 @@ class Solution
      */
     public function mergeTwoLists(?ListNode $list1 = null, ?ListNode $list2 = null): ?ListNode
     {
-        return null;
+        if ($list1 === null) {
+            return $list2;
+        }
+        if ($list2 === null) {
+            return $list1;
+        }
+
+        if ($list1->val < $list2->val) {
+            $head = $list1;
+            $list1 = $list1->next;
+        } else {
+            $head = $list2;
+            $list2 = $list2->next;
+        }
+        $cur = $head;
+
+        while ($list1 != null && $list2 != null) {
+            if ($list1->val < $list2->val) {
+                $cur->next = $list1;
+                $list1 = $list1->next;
+            } else {
+                $cur->next = $list2;
+                $list2 = $list2->next;
+            }
+            $cur = $cur->next;
+        }
+
+        if ($list1 != null) {
+            $cur->next = $list1;
+        }
+        if ($list2 != null) {
+            $cur->next = $list2;
+        }
+
+        return $head;
     }
 
     public function compareLists(?ListNode $list1 = null, ?ListNode $list2 = null): bool

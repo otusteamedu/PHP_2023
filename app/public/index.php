edf308e35d512c1ed1c9fcd4e2c1661d5f754773
aleksey <pav@el-plat.ru>
2023-03-28 12:03:31 +0400

    update app

--- app/public/index.php
+++ app/public/index.php
@@ -2,57 +2,27 @@
 
 declare(strict_types=1);
 
+require __DIR__ . '/../vendor/autoload.php';
+
+use Aporivaev\Hw05\Hw05;
+
 session_start();
 
-$history = $_SESSION['history'] ?? [];
-$string = $_POST['string'] ?? '';
-$method = $_SERVER['REQUEST_METHOD'] ?? null;
+$class = new Hw05();
 
-$from = $_SERVER['NGINX_INSTANCE'] ?? 'unknown';
-$php = getenv('PHP_FPM_INSTANCE') !== false ? getenv('PHP_FPM_INSTANCE') : 'unknown';
-
-function parseString(string $string): bool
-{
-    if (empty($string)) {
-        return false;
-    }
-
-    $charOpen = '(';
-    $charClose = ')';
-    $count = 0;
-
-    for ($i = 0; $i < strlen($string); $i++) {
-        if ($string[$i] === $charOpen) {
-            $count++;
-        } else {
-            if ($string[$i] === $charClose) {
-                $count--;
-            } else {
-                return false;
-            }
-        }
-
-        if ($count < 0) {
-            return false;
-        }
-    }
-
-    return $count == 0;
-}
-
-if ($method === 'POST') {
-    $history[] = $string;
-    $result = parseString($string);
-
-    http_response_code($result ? 200 : 400);
-    echo($result ? 'всё хорошо' : 'всё плохо');
+if ($class->isPost()) {
+    $class->validate();
 } else {
-    echo "NGINX instance - {$from}\n<br>\nPHP instance - {$php}\n<br>\n";
+    $env = $class->getEnv();
+    echo "NGINX instance - {$env['nginx']}\n<br>\nPHP instance - {$env['php']}\n<br>\n";
     echo "History:<br>\n<ul>\n";
-    foreach ($history as $item) {
-        echo "<li>{$item}</li>\n";
+
+    $history = $class->historyGet();
+    if (count($history)) {
+        foreach ($history as $item) {
+            echo "<li>{$item}</li>\n";
+        }
     }
+
     echo "</ul>";
 }
-
-$_SESSION['history'] = $history;
---
+++ app/src/Hw05.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aporivaev\Hw05;
+
+class Hw05 {
+    public function isPost(): bool {
+        return $_SERVER['REQUEST_METHOD'] === 'POST';
+    }
+	public function historyGet(): array
+	{
+		return $_SESSION['history'] ?? [];
+	}
+    public function historyAdd($string): void
+    {
+        $history = $_SESSION['history'] ?? [];
+        $history[] = $string;
+        $_SESSION['history'] = $history;
+    }
+
+    /**
+     * @return array {nginx: string, php: string}
+     */
+    public function getEnv(): array
+    {
+        return [
+            'nginx' => $_SERVER['NGINX_INSTANCE'] ?? 'unknown',
+            'php' => getenv('PHP_FPM_INSTANCE') !== false ? getenv('PHP_FPM_INSTANCE') : 'unknown'
+            ];
+    }
+
+    public function validate(bool $output = true): bool {
+        $string = $_POST['string'] ?? '';
+
+        $result = $this->parseString($string);
+        $this->historyAdd($string .' - '. ($result ? 'ok' : 'err'));
+
+        if ($output) {
+            http_response_code($result ? 200 : 400);
+            echo($result ? 'всё хорошо' : 'всё плохо');
+        }
+        return $result;
+    }
+    private function parseString(string $string): bool
+    {
+        if (empty($string)) {
+            return false;
+        }
+
+        $charOpen = '(';
+        $charClose = ')';
+        $count = 0;
+
+        for ($i = 0; $i < strlen($string); $i++) {
+            if ($string[$i] === $charOpen) {
+                $count++;
+            } else {
+                if ($string[$i] === $charClose) {
+                    $count--;
+                } else {
+                    return false;
+                }
+            }
+
+            if ($count < 0) {
+                return false;
+            }
+        }
+        return $count === 0;
+    }
+
+}

4fa697e2fc94be174ad5cdd57f0d601e7ab11cad
VladimirPavlenko <v.pavlenko@worksolutions.ru>
2023-03-31 12:22:23 +0300

    14. phpsniffer fix 2

--- project/src/DTO/SearchParams.php
+++ project/src/DTO/SearchParams.php
@@ -6,12 +6,10 @@ namespace Vp\App\DTO;
 
 class SearchParams
 {
-
     public function __construct(
         public readonly ?string $query = null,
         public readonly ?string $category = null,
         public readonly ?string $maxPrice = null
-    )
-    {
+    ) {
     }
 }

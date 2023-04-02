17f141e0cc489e57d709cc46d8fbed53e8713f52
Mase4ka <mase4ka87@gmail.com>
2023-04-02 22:39:09 +0300

    SQL fix

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
-SELECT `movie`.`name`, SUM(`session_price`.`price`) AS total_price
-FROM `session`
-         INNER JOIN `movie` ON (`session`.`movie_id` = `movie`.`id`)
-         INNER JOIN `session_price` ON (`session_price`.`session_id` = `session`.`id`)
-         INNER JOIN `ticket` ON (`ticket`.`session_price_id` = `session_price`.`id`)
-GROUP BY `session`.`movie_id`
+SELECT movie.name, SUM(session_price.price) AS total_price
+FROM session
+         INNER JOIN movie ON (session.movie_id = movie.id)
+         INNER JOIN session_price ON (session_price.session_id = session.id)
+         INNER JOIN ticket ON (ticket.session_price_id = session_price.id)
+GROUP BY session.movie_id
 ORDER BY total_price DESC LIMIT 1;

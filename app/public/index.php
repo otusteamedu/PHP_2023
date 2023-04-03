fed250886adee870b30c7b508709089190da4f23
aleksey <pav@el-plat.ru>
2023-04-03 09:36:16 +0400

    add exception message

--- app/public/index.php
+++ app/public/index.php
@@ -7,13 +7,11 @@ use Aporivaev\Hw09\AppException;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-set_time_limit(0);
-
 try {
     $app = new App();
     $app->run();
 } catch (AppException $e) {
     echo "App error. ", $e, "\n";
 } catch (Exception | Error $e) {
-    echo "Oops! Something went wrong\n";
+    echo "Oops! Something went wrong.", $e->getMessage(), "\n";
 }

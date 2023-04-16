b7dcab302fce06a25639d38ab52ac4384b8ee880
yakov-home <yakovgulyuta@yandex.ru>
2023-04-16 20:56:40 +0300

    валидация на скобки

--- code/index.php
+++ code/index.php
@@ -1,5 +1,36 @@
 <?php
 
-echo "Привет, Otus!<br>".date("Y-m-d H:i:s")."<br><br>";
+$string = $_POST['string'] ?? '';
+emptyValidate($string);
+bracketsValidate($string);
 
-echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+http_response_code(200);
+echo 'Все хорошо';
+function bracketsValidate(string $string): void
+{
+    $open = 0;
+    $closed = 0;
+    $splits = str_split($string);
+    foreach ($splits as $bracket) {
+        if ($bracket === '(') {
+            $open++;
+        } elseif ($bracket === ')') {
+            $closed++;
+        }
+    }
+
+    if ($open !== $closed) {
+        http_response_code(400);
+        echo 'Не равное количество скобок. Все плохо';
+        exit;
+    }
+}
+
+function emptyValidate(string $string): void
+{
+    if (empty($string)) {
+        http_response_code(400);
+        echo 'Пустая строка. Все плохо';
+        exit;
+    }
+}

3fdbc4c4dfbba2918da4d625b0553b51910c83e9
mehrubon <husaynov888@gmail.com>
2023-05-02 18:13:22 +0500

    add connection to memcached

--- src/public/index.php
+++ src/public/index.php
@@ -1,5 +1,31 @@
 <?php
 
 require_once __DIR__ . "/../vendor/autoload.php";
+//echo phpinfo();
 
-echo phpinfo();
+
+$serverName = "mysql";
+$userName = "root";
+$password = "secret";
+$port = "3306";
+$dbName = "myDB";
+try {
+  $conn = new PDO("mysql:host=$serverName;port=$port;dbname=$dbName", $userName, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo 'Mysql connected successfully <br />';
+} catch(PDOException $e) {
+  echo "Mysql connection failed: " . $e->getMessage() . "<br />";
+}
+
+
+
+$mc = new Memcached();
+$mc->addServer("memcached", 11211);
+
+if ($addMC = $mc->add("test", "success")) {
+    echo "Memcached connect successfully <br />";
+    $mc->delete("test");
+}else{
+    echo "Memcached connection failed <br />";
+}
+

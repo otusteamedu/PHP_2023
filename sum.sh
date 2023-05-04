2c176890307350fd90cc71d9d2b1885d7c122903
vladimir@positron-it.ru <vladimir@positron-it.ru>
2023-05-05 00:39:21 +0300

    Use awk instead of bc

--- sum.sh
+++ sum.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REGEXP='^-?[0-9]+([.][0-9]+)?'
+REGEXP='^-?[0-9]+([,][0-9]+)?'
 if [ $# != 2 ]; then
   echo "Введите 2 числа"
   exit 1
@@ -7,6 +7,6 @@ elif [[ ! $1 =~ $REGEXP ]] || [[ ! $2 =~ $REGEXP ]]; then
   echo "Вы дали неправильные аргументы. Введите 2 числа"
   exit 1
 else
-  echo "$1 + $2" | bc
+  echo $1 $2 | awk '{print $1 + $2}'
   exit 0
 fi

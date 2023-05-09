39b3dd25e89a73aa10e4b18fe5acceaef264f069
VladimirPavlenko <v.pavlenko@worksolutions.ru>
2023-05-09 16:00:45 +0300

    23. Правка схемы

--- project/src/Application/UseCase/InitData.php
+++ project/src/Application/UseCase/InitData.php
@@ -65,9 +65,8 @@ class InitData implements InitDataInterface
     private function createLandPlotTypeTable(): void
     {
         $sql = "CREATE TABLE IF NOT EXISTS types (
-                        id serial PRIMARY KEY,
-                        name VARCHAR(50) NOT NULL,
-                        code VARCHAR(50) NOT NULL
+                        code VARCHAR(50) PRIMARY KEY,
+                        name VARCHAR(50) NOT NULL
                         )";
         $this->conn->exec($sql);
     }
@@ -76,11 +75,10 @@ class InitData implements InitDataInterface
     {
         $sql = "CREATE TABLE IF NOT EXISTS tree_land_plots (
                         id serial PRIMARY KEY,
-                        number INT NOT NULL,
                         name VARCHAR(50) NOT NULL,
                         parent_id INT,
-                        type_id INT NOT NULL,
-                        CONSTRAINT c_fk_type FOREIGN KEY (type_id) REFERENCES types (id) ON DELETE CASCADE,
+                        type VARCHAR(10) NOT NULL,
+                        CONSTRAINT c_fk_type FOREIGN KEY (type) REFERENCES types (code) ON DELETE CASCADE,
                         CONSTRAINT c_fk_parent FOREIGN KEY (parent_id) REFERENCES tree_land_plots (id) ON DELETE CASCADE
                         )";
         $this->conn->exec($sql);
@@ -90,10 +88,9 @@ class InitData implements InitDataInterface
     {
         $sql = "CREATE TABLE IF NOT EXISTS land_plots (
                         id serial PRIMARY KEY,
-                        number INT NOT NULL,
                         name VARCHAR(50) NOT NULL,
-                        type_id INT NOT NULL,
-                        CONSTRAINT c_fk_type FOREIGN KEY (type_id) REFERENCES types (id) ON DELETE CASCADE
+                        type VARCHAR(10) NOT NULL,
+                        CONSTRAINT c_fk_type FOREIGN KEY (type) REFERENCES types (code) ON DELETE CASCADE
                         )";
         $this->conn->exec($sql);
     }
@@ -101,11 +98,11 @@ class InitData implements InitDataInterface
     private function createLandPlotTableForGrid(): void
     {
         $sql = "CREATE TABLE IF NOT EXISTS grid_land_plots (
-                        id serial PRIMARY KEY,
                         x INT NOT NULL,
                         y INT NOT NULL,
                         node_id INT NOT NULL,
                         child_id INT NOT NULL,
+                        PRIMARY KEY (node_id, x, y),
                         CONSTRAINT c_fk_node FOREIGN KEY (node_id) REFERENCES land_plots (id) ON DELETE CASCADE,
                         CONSTRAINT c_fk_child FOREIGN KEY (child_id) REFERENCES land_plots (id) ON DELETE CASCADE
                         )";
@@ -134,44 +131,44 @@ class InitData implements InitDataInterface
     private function fillLandPlotTableForTree(): void
     {
         $level = [];
-        $level[1][0] = $this->sqlLandPlotTableForTreeInsert(NULL, 1, "Участок 1");
-        $level[2][0] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 2, "Участок 1.1");
-        $level[2][1] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 3, "Участок 1.2");
-        $level[2][2] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 4, "Участок 1.3");
-        $level[2][3] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 5, "Участок 1.4");
-        $level[3][0] = $this->sqlLandPlotTableForTreeInsert($level[2][0], 6, "Участок 1.1.1");
-        $level[3][1] = $this->sqlLandPlotTableForTreeInsert($level[2][0], 7, "Участок 1.1.2");
-        $level[3][2] = $this->sqlLandPlotTableForTreeInsert($level[2][1], 8, "Участок 1.2.1");
-        $level[3][3] = $this->sqlLandPlotTableForTreeInsert($level[2][1], 9, "Участок 1.2.2");
-        $level[3][4] = $this->sqlLandPlotTableForTreeInsert($level[2][3], 10, "Участок 1.4.1");
-        $level[3][5] = $this->sqlLandPlotTableForTreeInsert($level[2][3], 11, "Участок 1.4.2");
+        $level[1][0] = $this->sqlLandPlotTableForTreeInsert(NULL, "Участок 1");
+        $level[2][0] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 1.1");
+        $level[2][1] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 1.2");
+        $level[2][2] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 1.3");
+        $level[2][3] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 1.4");
+        $level[3][0] = $this->sqlLandPlotTableForTreeInsert($level[2][0], "Участок 1.1.1");
+        $level[3][1] = $this->sqlLandPlotTableForTreeInsert($level[2][0], "Участок 1.1.2");
+        $level[3][2] = $this->sqlLandPlotTableForTreeInsert($level[2][1], "Участок 1.2.1");
+        $level[3][3] = $this->sqlLandPlotTableForTreeInsert($level[2][1], "Участок 1.2.2");
+        $level[3][4] = $this->sqlLandPlotTableForTreeInsert($level[2][3], "Участок 1.4.1");
+        $level[3][5] = $this->sqlLandPlotTableForTreeInsert($level[2][3], "Участок 1.4.2");
 
         $level = [];
-        $level[1][0] = $this->sqlLandPlotTableForTreeInsert(NULL, 12, "Участок 2");
-        $level[2][0] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 13, "Участок 2.1");
-        $level[2][1] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 14, "Участок 2.2");
-        $level[2][2] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 15, "Участок 2.3");
-        $level[2][3] = $this->sqlLandPlotTableForTreeInsert($level[1][0], 16, "Участок 2.4");
-        $level[3][0] = $this->sqlLandPlotTableForTreeInsert($level[2][0], 17, "Участок 2.1.1");
-        $level[3][1] = $this->sqlLandPlotTableForTreeInsert($level[2][0], 18, "Участок 2.1.2");
-        $level[3][2] = $this->sqlLandPlotTableForTreeInsert($level[2][1], 19, "Участок 2.2.1");
-        $level[3][3] = $this->sqlLandPlotTableForTreeInsert($level[2][1], 20, "Участок 2.2.2");
-        $level[3][4] = $this->sqlLandPlotTableForTreeInsert($level[2][3], 21, "Участок 2.4.1");
-        $level[3][5] = $this->sqlLandPlotTableForTreeInsert($level[2][3], 22, "Участок 2.4.2");
+        $level[1][0] = $this->sqlLandPlotTableForTreeInsert(NULL, "Участок 2");
+        $level[2][0] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 2.1");
+        $level[2][1] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 2.2");
+        $level[2][2] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 2.3");
+        $level[2][3] = $this->sqlLandPlotTableForTreeInsert($level[1][0], "Участок 2.4");
+        $level[3][0] = $this->sqlLandPlotTableForTreeInsert($level[2][0], "Участок 2.1.1");
+        $level[3][1] = $this->sqlLandPlotTableForTreeInsert($level[2][0], "Участок 2.1.2");
+        $level[3][2] = $this->sqlLandPlotTableForTreeInsert($level[2][1], "Участок 2.2.1");
+        $level[3][3] = $this->sqlLandPlotTableForTreeInsert($level[2][1], "Участок 2.2.2");
+        $level[3][4] = $this->sqlLandPlotTableForTreeInsert($level[2][3], "Участок 2.4.1");
+        $level[3][5] = $this->sqlLandPlotTableForTreeInsert($level[2][3], "Участок 2.4.2");
 
-        $this->sqlLandPlotTableForTreeInsert(NULL, 23, "Участок 3");
-        $this->sqlLandPlotTableForTreeInsert(NULL, 24, "Участок 4");
+        $this->sqlLandPlotTableForTreeInsert(NULL, "Участок 3");
+        $this->sqlLandPlotTableForTreeInsert(NULL, "Участок 4");
     }
 
-    private function sqlLandPlotTableForTreeInsert(?string $parentId, int $number, string $name): bool|string
+    private function sqlLandPlotTableForTreeInsert(?string $parentId, string $name): bool|string
     {
-        $typeId = rand(1, 3);
-        $sql = 'INSERT INTO tree_land_plots (number, name, parent_id, type_id) VALUES(:number, :name, :parent_id, :type_id)';
+        $types = array_keys($this->getTypes());
+        $randKey = rand(0, 2);
+        $sql = 'INSERT INTO tree_land_plots (name, parent_id, type) VALUES(:name, :parent_id, :type)';
         $stmt = $this->conn->prepare($sql);
-        $stmt->bindValue(':number', $number);
         $stmt->bindValue(':name', $name);
         $stmt->bindValue(':parent_id', ($parentId === NULL) ? NULL : (int)$parentId);
-        $stmt->bindValue(':type_id', $typeId);
+        $stmt->bindValue(':type', $types[$randKey]);
         $stmt->execute();
         return $this->conn->lastInsertId();
     }

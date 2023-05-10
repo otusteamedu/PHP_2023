d542340c8d7588ddecdde26d779ae32a86901b3b
Roman.Yuzyuk <roman.yuzyuk@lenvendo.ru>
2023-05-10 19:08:32 +0300

    fix sum script

--- sum.sh
+++ sum.sh
@@ -6,13 +6,13 @@ then
     exit 1
 fi
 
-reg='^[0-9]+$'
+reg='^-?[0-9]+([.][0-9]+)?$'
 if ! [[ $1 =~ $reg ]] || ! [[ $2 =~ $reg ]]
 then
     echo "Аргументы должны быть числами"
     exit 1
 fi
 
-let "a=$1 + $2"
+sum=$( echo "$1 + $2" | bc )
 
-echo "$a"
+echo "$sum"

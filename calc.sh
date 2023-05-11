5ca38d44e38677070a34d0f0ee650ac9013e897f
Denis <denis_mesnyankin@mail.ru>
2023-05-11 14:41:23 +0800

    duplicate

--- calc.sh
+++ calc.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
+REGEX="^[-+]?[0-9]+[\.|\,]?[0-9]*$"
+for arg in $@
+do
+    if [[ ! $arg =~ $REGEX ]];
+    then
+        echo "Argument $arg is not a number"
+        exit
+    fi
+done
 FIRST_ARG=$1
 SECOND_ARG=$2
-REGEX="^[-+]?[0-9]+[\.|\,]?[0-9]*$"
-if [[ ! $FIRST_ARG =~ $REGEX ]];
-then
-    echo "Argument 1 is not a number"
-    exit
-fi
-if [[ ! $SECOND_ARG =~ $REGEX ]];
-then
-    echo "Argument 2 is not a number"
-    exit
-fi
 FIRST_ARG=$(sed -e "s/[,]/./g" <<< $FIRST_ARG)
 SECOND_ARG=$(sed -e "s/[,]/./g" <<< $SECOND_ARG)
 if ! command -v bc &> /dev/null

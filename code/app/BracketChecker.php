a17bef3e83e6e7d56902a45ee168bdd4c9fd98ac
vladimir@positron-it.ru <vladimir@positron-it.ru>
2023-05-13 00:21:27 +0300

    Fix codestyle

--- code/app/BracketChecker.php
+++ code/app/BracketChecker.php
@@ -38,4 +38,3 @@ class BracketChecker
         return $response;
     }
 }
-
--- code/app/Response.php
+++ code/app/Response.php
@@ -7,12 +7,14 @@ class Response
     private int $status;
     private string $content;
 
-    public function __construct($content, $status = 200) {
+    public function __construct($content, $status = 200)
+    {
         $this->content = $content;
         $this->status = $status;
     }
 
-    public function provideResponse(): void {
+    public function provideResponse(): void
+    {
         http_response_code($this->status);
         echo $this->content;
     }

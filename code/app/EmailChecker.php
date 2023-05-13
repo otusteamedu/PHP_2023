215dae79ce318858e8460715e80325016ac9790d
vladimir@positron-it.ru <vladimir@positron-it.ru>
2023-05-13 17:24:59 +0300

    Add logic

---
+++ code/app/EmailChecker.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app;
+
+Class EmailChecker
+{
+    public function process(string $filepath): void
+    {
+
+        $handle = fopen($filepath, "r");
+        if ($handle) {
+            while (($email = fgets($handle)) !== false) {
+                if ($this->validateEmail($email)) {
+                    $status = 'OK';
+                }
+                else {
+                    $status = 'WRONG';
+                }
+                echo $email . ' - ' . $status . '<br>';
+            }
+
+            fclose($handle);
+        }
+    }
+
+    public function validateEmail(string $email): bool
+    {
+        // првоеряем регулярным выражением
+        $pattern = '/^[\w\-\.]+@([\w\-]+\.)+[\w]{2,4}$/';
+        preg_match($pattern, $email, $matches);
+        if (!$matches[0]) {
+            return false;
+        }
+
+        // проверяем mx-запись
+        $hosts = [];
+        $hostname = explode('@', $email);
+        $hostname = trim($hostname[1]);
+        getmxrr($hostname, $hosts);
+        if (count($hosts) == 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
---
+++ code/web/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require "../app/EmailChecker.php";
+
+use app\EmailChecker;
+
+$check = new EmailChecker;
+$check->process('emails.txt');

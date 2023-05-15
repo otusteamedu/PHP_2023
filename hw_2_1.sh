5c9db593087102fbba242bf68276c766df4bcba5
mehrubon <husaynov888@gmail.com>
2023-05-15 12:13:28 +0500

    Fixing: summing float numbers without bc

--- hw_2_1.sh
+++ hw_2_1.sh
@@ -19,6 +19,8 @@ fi
 
 # shellcheck disable=SC2034
 # shellcheck disable=SC2004
-sum=$(echo "$num1 + $num2" | bc)
+#sum=$(echo "$num1 + $num2" | bc)
+
+sum=$(echo $num1 $num2 | awk '{print $1 + $2}')
 
 echo "$num1 + $num2" = $sum;

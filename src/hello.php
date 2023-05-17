e01d8b5ba3693d0a40db2a08e3380103bbda2355
Nikita Menshutin <info@nikita.global>
2023-05-17 22:11:22 +0300

    fix: phpcs

--- src/hello.php
+++ src/hello.php
@@ -1,16 +1,18 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Nikitaglobal\hw3
 /**
  * Class Hello
  */
-Class Hello {
-	/**
-	 * Hello constructor.
-	 */
-	function __construct()
-	{
-		echo "Hello World!";
-	}
+class Hello
+{
+    /**
+     * Hello constructor.
+     */
+    function __construct()
+    {
+        echo "Hello World!";
+    }
 }

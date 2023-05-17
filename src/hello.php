fb3bea983aae1a0252232c6c78485d97419e995a
Nikita Menshutin <info@nikita.global>
2023-05-17 22:06:15 +0300

    chore: class Hello

---
+++ src/hello.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Nikitaglobal\hw3
+/**
+ * Class Hello
+ */
+Class Hello {
+	/**
+	 * Hello constructor.
+	 */
+	function __construct()
+	{
+		echo "Hello World!";
+	}
+}

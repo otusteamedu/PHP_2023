bcd88565417cad32fdab46794a1ccc6967a39483
alexeys <lucky.jew@gmail.com>
2023-05-19 22:43:08 +0500

    Fix Codesniffer suggestions.

--- hw3/src/index.php
+++ hw3/src/index.php
@@ -4,9 +4,15 @@ require '../vendor/autoload.php';
 
 use Lujewwy\Capitalizer\Capitalizer;
 
-echo Capitalizer::capitalize(); echo '<br />';
-echo Capitalizer::capitalize(''); echo '<br />';
-echo Capitalizer::capitalize('hello'); echo '<br />';
-echo Capitalizer::capitalize('hello, world!'); echo '<br />';
-echo Capitalizer::capitalize('1950 include require_once ABC123 aX12y Something ImPoRtAnT VERy MUCH'); echo '<br />';
-echo Capitalizer::capitalize('😀 and something more SERIOUS - 👩‍👩‍👧'); echo '<br />';
+echo Capitalizer::capitalize();
+echo '<br />';
+echo Capitalizer::capitalize('');
+echo '<br />';
+echo Capitalizer::capitalize('hello');
+echo '<br />';
+echo Capitalizer::capitalize('hello, world!');
+echo '<br />';
+echo Capitalizer::capitalize('1950 include require_once ABC123 aX12y Something ImPoRtAnT VERy MUCH');
+echo '<br />';
+echo Capitalizer::capitalize('😀 and something more SERIOUS - 👩‍👩‍👧');
+echo '<br />';

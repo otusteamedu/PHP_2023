ee4e0b8e31818cd10ff28242dba6bcce6c9fc584
alexeys <lucky.jew@gmail.com>
2023-05-19 22:37:41 +0500

    Create Capitalizer package, put it to GitHub and Packagist, and use it via Composer in simple project.

---
+++ hw3/src/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Lujewwy\Capitalizer\Capitalizer;
+
+echo Capitalizer::capitalize(); echo '<br />';
+echo Capitalizer::capitalize(''); echo '<br />';
+echo Capitalizer::capitalize('hello'); echo '<br />';
+echo Capitalizer::capitalize('hello, world!'); echo '<br />';
+echo Capitalizer::capitalize('1950 include require_once ABC123 aX12y Something ImPoRtAnT VERy MUCH'); echo '<br />';
+echo Capitalizer::capitalize('😀 and something more SERIOUS - 👩‍👩‍👧'); echo '<br />';

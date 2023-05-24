2f3945adf8410a8bff9e5f0ecd24812a63b90c50
vladimir@positron-it.ru <vladimir@positron-it.ru>
2023-05-25 00:24:37 +0300

    Modify db structure

--- db/ddl-scripts.sql
+++ db/ddl-scripts.sql
@@ -1,4 +1,4 @@
-create table if not exists Halls
+create table Halls
 (
     id          int auto_increment
         primary key,
@@ -8,57 +8,96 @@ create table if not exists Halls
 )
     comment 'Кинозалы';
 
-create table if not exists Movies
+
+create table Movies
 (
-    id          int          auto_increment
+    id          int          not null
         primary key,
     name        varchar(255) not null,
     description text         not null,
-    year        int          not null
+    year        int          not null,
+    movie_type  int          not null
 );
 
-create table if not exists Seats
+
+create table Seats
 (
-    id      int auto_increment
+    id        int auto_increment
         primary key,
-    number  varchar(255) null comment 'номер места',
-    hall_id int          null,
-    constraint Seats_ibfk_1
-        foreign key (hall_id) references Halls (id)
+    number    varchar(255) null comment 'номер места',
+    hall_id   int          null,
+    seat_type int          null
 );
 
-create table if not exists Sessions
+
+create table Sessions
 (
     id               int auto_increment
         primary key,
     session_datetime datetime not null,
     movie_id         int      not null,
     hall_id          int      not null,
+    time_type_id     int      not null,
     constraint Sessions_Halls_null_fk
         foreign key (hall_id) references Halls (id),
     constraint Sessions_Movies_null_fk
         foreign key (movie_id) references Movies (id)
 );
 
-create table if not exists Tickets
+
+create table Movie_types
+(
+    id   int auto_increment
+        primary key,
+    name varchar(255) null
+)
+    comment 'Типы фильмов по ожидаемой прибыльности: блокбастеры, фестивальное кино и тд';
+
+
+create table Seat_types
+(
+    id   int auto_increment
+        primary key,
+    name varchar(255) not null
+)
+    comment 'Типы мест: обычные, VIP, LS и тд';
+
+
+create table Time_types
+(
+    id   int auto_increment
+        primary key,
+    name varchar(255) not null
+)
+    comment 'Типы времени: утро, день, праймтайм и тд ';
+
+
+create table Prices
+(
+    id            int auto_increment
+        primary key,
+    movie_type_id int not null,
+    time_type_id  int not null,
+    seat_type_id  int not null,
+    price         int not null,
+    constraint Prices_Movie_types_null_null_fk
+        foreign key (movie_type_id) references Movie_types (id),
+    constraint Prices_Seat_types_null_fk
+        foreign key (seat_type_id) references Seat_types (id),
+    constraint Prices_Time_types_null_fk
+        foreign key (time_type_id) references Time_types (id)
+)
+    comment 'Справочник цен';
+
+
+create table Tickets
 (
     id         int auto_increment
         primary key,
     session_id int not null,
-    seat_id    int not null,
-    price      int null,
+    price_id   int not null,
     constraint Tickets_Seats_null_fk
-        foreign key (seat_id) references Seats (id),
+        foreign key (price_id) references Seats (id),
     constraint Tickets_Sessions_null_fk
         foreign key (session_id) references Sessions (id)
-);
-
-create table if not exists Ticket_payments
-(
-    id               int                   not null,
-    payment_datetime datetime              not null,
-    payment_type     enum ('cash', 'card') not null,
-    ticket_id        int                   not null,
-    constraint Ticket_payments_Tickets_null_fk
-        foreign key (ticket_id) references Tickets (id)
 );

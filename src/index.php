ef11ee39b03b36909f11a41db45ef2f25b12b975
Nikita Menshutin <info@nikita.global>
2023-05-24 16:31:13 +0300

    chore: ДЗ №5 #2

--- src/index.php
+++ src/index.php
@@ -3,4 +3,4 @@
 require './vendor/autoload.php';
 use nikitaglobal\Validate as Validate;
 $validator = new Validate();
-$validator->checkEmail()->checkEmailMx()->generateResponse() ? http_response_code(200) : http_response_code(400);
+$validator->checkEmails()->generateResponse() ? http_response_code(200) : http_response_code(400);

be5afb0cf3a3361f6383a056c6e14c23ccc3ad7e
DiM <hy0@ya.ru>
2023-05-27 20:45:27 +0700

    email checker

---
+++ balancer/code/App/Form/Template/Form.php
@@ -0,0 +1,5 @@
+<form method="post">
+    <label for="text">Введите email, каждый с новой строки:</label>
+    <textarea id="text" name="emails" rows="10" required><?=$emails ?></textarea>
+    <button type="submit">Проверить</button>
+</form>
--- balancer/code/App/Form/View.php
+++ balancer/code/App/Form/View.php
@@ -6,12 +6,6 @@ class View
 {
     public static function show(string $emails): void
     {
-        ?>
-        <form method="post">
-            <label for="text">Введите email, каждый с новой строки:</label>
-            <textarea id="text" name="emails" rows="10" required><?=$emails ?></textarea>
-            <button type="submit">Проверить</button>
-        </form>
-        <?php
+        include "Template/Form.php";
     }
 }

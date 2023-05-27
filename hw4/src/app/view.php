f56a5ea054a6b4cc32341454bf6f29692268b94b
alexeys <lucky.jew@gmail.com>
2023-05-27 19:21:14 +0500

    Move code to separate files. Create Validator class.

---
+++ hw4/src/app/view.php
@@ -0,0 +1,15 @@
+<?php
+
+$view = <<<HTMLVIEW
+<html lang="en">
+<head>
+    <title>Test</title>
+</head>
+<body>
+<form method="post" action="../index.php">
+    <input type="text" name="string" />
+    <button>Submit</button>
+</form>
+</body>
+</html>
+HTMLVIEW;
--- hw4/src/index.php
+++ hw4/src/index.php
@@ -1,49 +1,15 @@
 <?php
 
+require_once './app/validator.php';
+require_once './app/view.php';
+
 if ($_REQUEST) {
-    if (isset($_POST['string'])) {
-        $string = $_POST['string'];
-        if (empty($string)) {
-            header('HTTP/1.1 400 Bad Request');
-            header('Response: Parameter "string" is empty.');
-            echo 'Parameter "string" is empty.';
-        }
+    $validator = new Validator($_POST);
+    $response = $validator->validate('string');
 
-        $justBracesString = '';
-        foreach (str_split($string) as $symbol) {
-            if (in_array($symbol, ['(', ')'])) {
-                $justBracesString .= $symbol;
-            }
-        }
-
-        while (strpos($justBracesString, '()') !== false) {
-            $justBracesString = str_replace('()', '', $justBracesString);
-        }
-
-        if (strlen($justBracesString) > 0) {
-            header('HTTP/1.1 400 Bad Request');
-            header('Response: Incorrect string - braces are not paired.');
-            echo 'Incorrect string - braces are not paired.';
-        } else {
-            header('HTTP/1.1 200 OK');
-            header('Response: Everything is fine.');
-            echo 'Everything is fine.';
-        }
-    } else {
-        header('HTTP/1.1 400 Bad Request');
-        header('Response: Parameter "string" is missing.');
-        echo 'Parameter "string" is missing.';
-    }
-} else { ?>
-    <html lang="en">
-    <head>
-        <title>Test</title>
-    </head>
-    <body>
-    <form method="post">
-        <input type="text" name="string" />
-        <button>Submit</button>
-    </form>
-    </body>
-    </html>
-<?php }
+    header($response['header_status']);
+    header($response['header_response']);
+    echo $response['response_message'];
+} else {
+    echo $view;
+}

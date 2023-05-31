516fce4c89746c39dc6eb7907705ea19d157b08c
mehrubon <husaynov888@gmail.com>
2023-05-31 17:14:28 +0500

    hw3

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Sekaiichi\SuperApp\Actions\SearchProductAction;
+
+require __DIR__.'/vendor/autoload.php';
+
+$searchProduct = new SearchProductAction();
+
+try {
+    $getData = json_decode($searchProduct('fefe'), true, 512, JSON_THROW_ON_ERROR);
+
+    return ['message' => 'success', 'code' => 200, 'response' => $getData];
+} catch (JsonException $e) {
+    return ['message' => $e->getMessage(), 'code' => 500, 'response' => []];
+}
---
+++ src/Actions/SearchProductAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sekaiichi\SuperApp\Actions;
+use JsonException;
+
+class SearchProductAction
+{
+    /**
+     * @throws JsonException
+     */
+    public function __invoke($searchTerm): string
+    {
+        $ch = curl_init("https://dummyjson.com/products/search?q=$searchTerm");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
+    }
+}

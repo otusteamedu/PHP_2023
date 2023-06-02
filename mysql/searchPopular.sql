26e3e0735fbef28255ced30816ab06956677107a
D.Esaulenko <esaul.work@mail.ru>
2023-06-02 23:39:45 +0300

    hw7. fix popular by sum price

--- mysql/searchPopular.sql
+++ mysql/searchPopular.sql
@@ -1,11 +1,11 @@
 SELECT
-    m.name,
-    count(m.id) as count
+       m.name,
+       SUM(t.price) as sum
 FROM checkout
     JOIN basket b on checkout.id = b.checkout_id
     JOIN ticket t on t.id = b.ticket_id
     JOIN session s on s.id = t.session_id
     JOIN movie m on m.id = s.movie_id
-GROUP BY m.id
-ORDER BY count desc
-    LIMIT 1
+GROUP BY t.id
+ORDER BY sum DESC
+LIMIT 1

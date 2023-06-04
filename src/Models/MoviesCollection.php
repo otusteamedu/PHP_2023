f66863c790d0e35b8747292af420682aa90adee7
vladimir@positron-it.ru <vladimir@positron-it.ru>
2023-06-04 23:45:33 +0300

    Add collection class

---
+++ src/Models/MoviesCollection.php
@@ -0,0 +1,16 @@
+<?php
+
+class MoviesCollection
+{
+    public array $movies = [];
+
+    public static function create(\PDO $pdo, array $array): self
+    {
+        $collection = new self;
+        foreach ($array as $item) {
+            $movie = \Models\Movie::getById($pdo, $item['id']);
+            $collection->movies[] = $movie;
+        }
+        return $collection;
+    }
+}

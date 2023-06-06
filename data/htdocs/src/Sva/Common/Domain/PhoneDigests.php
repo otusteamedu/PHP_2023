fc0ffbd8d89556a073e9a1791cb5dc533a5c6409
Vyacheslav Shevchenko <webdev2030@gmail.com>
2023-06-06 23:37:30 +0600

    hw19 - убран str_split

--- data/htdocs/src/Sva/Common/Domain/PhoneDigests.php
+++ data/htdocs/src/Sva/Common/Domain/PhoneDigests.php
@@ -5,14 +5,14 @@ namespace Sva\Common\Domain;
 class PhoneDigests
 {
     const DIGESTS = [
-        2 => 'abc',
-        3 => 'def',
-        4 => 'ghi',
-        5 => 'jkl',
-        6 => 'mno',
-        7 => 'pqrs',
-        8 => 'tuv',
-        9 => 'wxyz'
+        2 => ['a', 'b', 'c'],
+        3 => ['d', 'e', 'f'],
+        4 => ['g', 'h', 'i'],
+        5 => ['j', 'k', 'l'],
+        6 => ['m', 'n', 'o'],
+        7 => ['p', 'q', 'r', 's'],
+        8 => ['t', 'u', 'v'],
+        9 => ['w', 'x', 'y', 'z']
     ];
 
     /**
@@ -25,20 +25,24 @@ class PhoneDigests
             return [];
         }
 
-        $digits = str_split($digits);
+        if (intval($digits) <= 0) {
+            return [];
+        }
+
         $result = [''];
 
-        foreach ($digits as $key => $digit) {
+        for ($i = 0; $i < strlen($digits); $i++) {
+            $digit = $digits[$i];
             if ($digit < 2 || $digit > 9) {
                 continue;
             }
 
             $r = [];
-            foreach ($result as $k => $item) {
-                $letters = str_split(self::DIGESTS[$digit]);
+            foreach ($result as $item) {
+                $letters = self::DIGESTS[$digit];
 
                 foreach ($letters as $letter) {
-                    $r[] = $result[$k] . $letter;
+                    $r[] = $item . $letter;
                 }
             }
 

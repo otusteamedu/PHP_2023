200363fb10ce505a9f8197705c5c9ab87cc9253f
IoakkunShoik <ioa.sh99@gmail.com>
2023-06-08 10:42:38 +0300

    Update get_most_profitable_film.sql

--- get_most_profitable_film.sql
+++ get_most_profitable_film.sql
@@ -2,7 +2,6 @@ SELECT SUM(price) as summ, f.name as name
 from ticket t
          LEFT JOIN session s on t.session = s.id
          LEFT JOIN film f on s.film = f.id
-         LEFT JOIN seat st on st.price = t.seat
 group by f.id
 ORDER BY summ DESC
 LIMIT 1;

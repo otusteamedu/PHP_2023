236056cb380a6691353a856a34b26a788b6aea57
fdfkc <korabelnikov.va@gmail.com>
2023-06-10 22:39:57 +0300

    Исправил стиль кода

--- Solution.php
+++ Solution.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /**
  * Решение задачи: https://leetcode.com/problems/find-the-pivot-integer/
- * 
+ *
  * @param int $n
  * @return int
  */
@@ -18,9 +18,9 @@ function pivotInteger(int $n): int
         return -1;
     }
 
-    $sum1ToN = $n*($n + 1)/2;
+    $sum1ToN = $n * ($n + 1) / 2;
     for ($i = 2; $i < $n; $i++) {
-        $firstSum = $i*($i + 1)/2;
+        $firstSum = $i * ($i + 1) / 2;
 
         $secondSum = $sum1ToN - $firstSum + $i;
 

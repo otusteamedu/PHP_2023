f81f49c5b9d14554cb81ae9e91fac111a01b95ae
IoakkunShoik <ioa.sh99@gmail.com>
2023-06-11 15:03:11 +0300

    Update Solution.php

--- Solution.php
+++ Solution.php
@@ -4,35 +4,27 @@ declare(strict_types=1);
 
 namespace Solution;
 
-class Solution
-{
+class Solution {
+
     /**
      * @param Integer $n
      * @return Integer
      */
-    public function pivotInteger(int $n): int
-    {
+    function pivotInteger($n) {
+        $left = 0;
+        $right = $this->getFullSum($n);
         for ($i = 1; $i <= $n; $i++) {
-            if ($this->beforeN($i) == $this->afterN($i, $n)) {
+                if (($left += $i) == ($right -= $i-1) 
+                ) {
                 return $i;
             }
         }
         return -1;
     }
 
-    private function beforeN(int $x): int
-    {
+    function getFullSum ($n) {
         $result = 0;
-        for ($i = 1; $i <= $x; $i++) {
-            $result += $i;
-        }
-        return $result;
-    }
-
-    private function afterN(int $x, int $n): int
-    {
-        $result = 0;
-        for ($i = $x; $i <= $n; $i++) {
+        for ($i = 1; $i <= $n; $i++) {
             $result += $i;
         }
         return $result;

877842e4b3cc8f7f0a1369cc270801f8db0ab6c1
IoakkunShoik <ioa.sh99@gmail.com>
2023-06-11 15:06:33 +0300

    Update Solution.php

--- Solution.php
+++ Solution.php
@@ -4,25 +4,25 @@ declare(strict_types=1);
 
 namespace Solution;
 
-class Solution {
+class Solution
+{
     /**
      * @param Integer $n
      * @return Integer
      */
-    public function pivotInteger($n): int
+    public function pivotInteger(int $n): int
     {
         $left = 0;
         $right = $this->getFullSum($n);
         for ($i = 1; $i <= $n; $i++) {
-                if (($left += $i) == ($right -= $i-1) 
-                ) {
+            if (($left += $i) == ($right -= $i - 1)) {
                 return $i;
             }
         }
         return -1;
     }
 
-    private function getFullSum ($n): int
+    private function getFullSum(int $n): int
     {
         $result = 0;
         for ($i = 1; $i <= $n; $i++) {

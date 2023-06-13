3e01c773763ed6ca39f8ef14a2855baea565e618
Ilyos Xizr <ilhomteshaboyev@gmail.com>
2023-06-13 12:49:18 +0500

    attempt-1

---
+++ code/index.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $string = $_POST['string'];
+
+    if (empty($string) or !preg_match('/[()]/', $string)) {
+        http_response_code(400);
+        echo 'Bad Request // Все плохо';
+        exit;
+    }
+
+    $openBrackets = 0;
+    for ($i = 0; $i < strlen($string); $i++) {
+        if ($string[$i] === '(') {
+            $openBrackets++;
+        } elseif ($string[$i] === ')') {
+            $openBrackets--;
+        }
+
+        if ($openBrackets < 0) {
+            http_response_code(400);
+            echo 'Bad Request';
+            exit;
+        }
+    }
+
+    if ($openBrackets !== 0) {
+        http_response_code(400);
+        echo 'Bad Request';
+        exit;
+    }
+
+    http_response_code(200);
+    echo 'OK';
+    exit;
+}
+?>

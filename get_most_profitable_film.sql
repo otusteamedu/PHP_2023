12b210e2fd9614f574c5a732b6bd3e4703af5bfb
IoakkunShoik <ioa.sh99@gmail.com>
2023-06-14 12:24:48 +0300

    Update get_most_profitable_film.sql

--- get_most_profitable_film.sql
+++ get_most_profitable_film.sql
@@ -1,4 +1,4 @@
-SELECT SUM(price) as summ, f.name as name
+SELECT SUM(fix_price) as summ, f.name as name
 from ticket t
          LEFT JOIN session s on t.session = s.id
          LEFT JOIN film f on s.film = f.id

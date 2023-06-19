2eb54f6e02c3b1ddadb49fda67c8ad0d40f376d1
pankratov2010 <112240192+pankratov2010@users.noreply.github.com>
2023-06-19 23:00:54 +0300

    Update leetcode.php

--- leetcode.php
+++ leetcode.php
@@ -1,23 +1,26 @@
-class Solution {
+<?php
 
-/**
- * @param Integer[] $nums
- * @return Integer[]
- */
-function leftRightDifference($nums) {
-    $n = count($nums);
+class Solution
+{
 
-// Calculate leftSum and rightSum
-    for ($i = 1; $i < $n; $i++) {
-        $leftSum[$i] = $leftSum[$i - 1] + $nums[$i - 1];
-        $rightSum[$n - $i - 1] = $rightSum[$n - $i] + $nums[$n - $i];
-        
+    /**
+     * @param Integer[] $nums
+     * @return Integer[]
+     */
+    function leftRightDifference($nums)
+    {
+        $n = count($nums);
+
+        // Calculate leftSum and rightSum
+        for ($i = 1; $i < $n; $i++) {
+            $leftSum[$i] = $leftSum[$i - 1] + $nums[$i - 1];
+            $rightSum[$n - $i - 1] = $rightSum[$n - $i] + $nums[$n - $i];
+        }
+
+        // Calculate answer
+        for ($i = 0; $i < $n; $i++) {
+            $answer[$i] = abs($leftSum[$i] - $rightSum[$i]);
+        }
+        return $answer;
     }
-
-// Calculate answer
-    for ($i = 0; $i < $n; $i++) {
-        $answer[$i] = abs($leftSum[$i] - $rightSum[$i]);
-    }
-    return $answer;
-}
 }

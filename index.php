2a4ae1b6c2a6fea291ad112596e02644cff24b99
Lebedev.vr <prizman201@gmail.com>
2023-06-19 20:24:38 +0300

    HW-5 fix function return format

--- index.php
+++ index.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 function validateEmail(array $strings): array
 {
-    $valid = [];
+    $validated = [];
     foreach ($strings as $string) {
         $words = explode(' ', preg_replace('/^ +| +$|( ) +/m', '$1', $string));
         foreach ($words as $word) {
             if (filter_var($word, FILTER_VALIDATE_EMAIL) && checkdnsrr(explode('@', $word)[1])) {
-                $valid[] = $word;
+                $validated[$word] = true;
+            } else {
+                $validated[$word] = false;
             }
         }
     }
-    return $valid;
+    return $validated;
 }

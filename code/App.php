8d4bf8ad81b78181083b5ec718622ced3bb1974b
Ilyos Xizr <ilhomteshaboyev@gmail.com>
2023-06-19 12:21:21 +0500

    attempt-7

--- code/App.php
+++ code/App.php
@@ -2,12 +2,10 @@
 
 namespace MyApp;
 
-require_once 'Validate.php';
+use MyApp\Validate;
 
-class App
-{
-    public function run()
-    {
+class App {
+    public function run() {
         $validate = new Validate();
 
         // Получаем значение POST-параметра 'string'
@@ -18,10 +16,21 @@ class App
             $validate->validateString($string);
 
             // Если все проверки прошли успешно, возвращаем ответ 200 OK
-            return "200 OK: Все скобки в строке корректны.";
+            $response = '200 OK: Все скобки в строке корректны.';
+            $this->sendHttpResponse(200, $response);
         } catch (\Exception $e) {
             // Если возникла ошибка, возвращаем ответ 400 Bad Request
-            return "400 Bad Request: Ошибка в строке скобок.";
+            $response = '400 Bad Request: Ошибка в строке скобок.';
+            $this->sendHttpResponse(400, $response);
         }
+    }
+
+    private function sendHttpResponse($statusCode, $response) {
+        // Устанавливаем HTTP-статус и заголовки ответа
+        http_response_code($statusCode);
+        header('Content-Type: text/plain');
+
+        // Выводим тело ответа
+        echo $response;
     }
 }

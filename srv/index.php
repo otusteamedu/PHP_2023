8d06d6f1ff5f9ca3dc8b8eebacc722fad77c5766
Archibald01 <85070971+Archibald01@users.noreply.github.com>
2023-06-26 20:22:24 -0400

    #Первая часть первой задачи

--- srv/index.php
+++ srv/index.php
@@ -1,2 +1,17 @@
 <?php
-phpinfo();
+
+require 'Validator.php';
+
+$validator = new Validator;
+$string = '(()()()()))((((()()()))(()()()(((()))))))';
+
+$result = $validator->validateString($_POST['string'] = $string);
+
+if ($result) {
+    http_response_code(200);
+    echo 'Запрос выполнен успешно';
+} else {
+    http_response_code(400);
+    echo 'Запрос невыполнен. Ошибка валидации';
+}
+

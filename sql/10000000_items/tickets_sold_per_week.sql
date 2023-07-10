80228783dfda8d764159a74f10072b888f990787
Roman.Yuzyuk <roman.yuzyuk@lenvendo.ru>
2023-07-10 13:52:21 +0300

    hw 10 tickets_sold_per_week update

--- sql/10000000_items/tickets_sold_per_week.sql
+++ sql/10000000_items/tickets_sold_per_week.sql
@@ -1,62 +1,58 @@
-explain analyse SELECT COUNT(*) AS ticket_count
+explain analyse SELECT COUNT(tickets.id) AS ticket_count
 FROM tickets
      JOIN session_place_price ON tickets.session_place_price_id = session_place_price.id
      JOIN sessions ON session_place_price.session_id = sessions.id
 WHERE sessions.date_start >= CURRENT_DATE - INTERVAL '7 days';
 
 -- Резульат:
--- Aggregate  (cost=3048.25..3048.26 rows=1 width=8) (actual time=224.764..224.785 rows=1 loops=1)
---   ->  Hash Join  (cost=803.25..3043.44 rows=1926 width=0) (actual time=50.751..222.211 rows=2133 loops=1)
+-- Aggregate  (cost=61487.95..61487.96 rows=1 width=8) (actual time=128.709..128.728 rows=1 loops=1)
+--   ->  Hash Join  (cost=423.00..61481.27 rows=2674 width=4) (actual time=26.908..123.361 rows=4017 loops=1)
 --         Hash Cond: (session_place_price.session_id = sessions.id)
---         ->  Merge Join  (cost=366.44..2594.52 rows=4612 width=4) (actual time=15.556..182.952 rows=4612 loops=1)
---               Merge Cond: (session_place_price.id = tickets.session_place_price_id)
---               ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..282367.84 rows=8999960 width=8) (actual time=0.026..78.660 rows=59990 loops=1)
---               ->  Sort  (cost=365.79..377.32 rows=4612 width=4) (actual time=15.507..21.098 rows=4612 loops=1)
---                     Sort Key: tickets.session_place_price_id
---                     Sort Method: quicksort  Memory: 193kB
---                     ->  Seq Scan on tickets  (cost=0.00..85.12 rows=4612 width=4) (actual time=0.014..7.741 rows=4612 loops=1)
---         ->  Hash  (cost=358.50..358.50 rows=6265 width=4) (actual time=30.851..30.855 rows=6258 loops=1)
+--         ->  Nested Loop  (cost=0.43..61037.64 rows=8023 width=8) (actual time=0.059..79.072 rows=8508 loops=1)
+--               ->  Seq Scan on tickets  (cost=0.00..151.23 rows=8023 width=8) (actual time=0.027..11.887 rows=8508 loops=1)
+--               ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..7.59 rows=1 width=8) (actual time=0.004..0.004 rows=1 loops=8508)
+--                     Index Cond: (id = tickets.session_place_price_id)
+--         ->  Hash  (cost=359.76..359.76 rows=5024 width=4) (actual time=26.780..26.784 rows=6268 loops=1)
 --               Buckets: 8192  Batches: 1  Memory Usage: 285kB
---               ->  Seq Scan on sessions  (cost=0.00..358.50 rows=6265 width=4) (actual time=0.017..18.173 rows=6258 loops=1)
+--               ->  Seq Scan on sessions  (cost=0.00..359.76 rows=5024 width=4) (actual time=0.037..15.621 rows=6268 loops=1)
 --                     Filter: (date_start >= (CURRENT_DATE - '7 days'::interval))
---                     Rows Removed by Filter: 8742
--- Planning Time: 0.735 ms
--- Execution Time: 224.849 ms
-
-
-
+--                     Rows Removed by Filter: 8732
+-- Planning Time: 1.887 ms
+-- Execution Time: 128.805 ms
 
 -- Индекс на столбец session_place_price_id в таблице tickets.
-CREATE INDEX idx_sold_date_film_id ON tickets (sold_date, film_id);
+CREATE INDEX idx_session_place_price_id ON tickets (session_place_price_id);
 -- Индекс на столбец session_id в таблице session_place_price.
 CREATE INDEX idx_session_id ON session_place_price (session_id);
 -- Индекс на столбец date_start в таблице sessions.
 CREATE INDEX idx_date_start ON sessions (date_start);
 
-explain analyse SELECT COUNT(*) AS ticket_count
+explain analyse SELECT COUNT(tickets.id) AS ticket_count
 FROM tickets
-         JOIN session_place_price ON tickets.session_place_price_id = session_place_price.id
-         JOIN sessions ON session_place_price.session_id = sessions.id
+     JOIN session_place_price ON tickets.session_place_price_id = session_place_price.id
+     JOIN sessions ON session_place_price.session_id = sessions.id
 WHERE sessions.date_start >= CURRENT_DATE - INTERVAL '7 days';
 
 -- Результат после приминения индексов:
--- Aggregate  (cost=2736.37..2736.38 rows=1 width=8) (actual time=213.974..213.996 rows=1 loops=1)
---   ->  Hash Join  (cost=374.82..2731.56 rows=1926 width=0) (actual time=33.315..211.397 rows=2133 loops=1)
+-- Aggregate  (cost=60636.83..60636.84 rows=1 width=8) (actual time=151.544..151.571 rows=1 loops=1)
+--   ->  Hash Join  (cost=373.62..60627.94 rows=3559 width=4) (actual time=25.435..146.236 rows=4017 loops=1)
 --         Hash Cond: (session_place_price.session_id = sessions.id)
---         ->  Merge Join  (cost=2.03..2346.66 rows=4612 width=4) (actual time=0.086..174.708 rows=4612 loops=1)
---               Merge Cond: (tickets.session_place_price_id = session_place_price.id)
---               ->  Index Only Scan using idx_session_place_price_id on tickets  (cost=0.28..129.46 rows=4612 width=4) (actual time=0.037..6.339 rows=4612 loops=1)
---                     Heap Fetches: 0
---               ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..282368.43 rows=9000000 width=8) (actual time=0.022..82.118 rows=59989 loops=1)
---         ->  Hash  (cost=294.48..294.48 rows=6265 width=4) (actual time=28.008..28.016 rows=6258 loops=1)
+--         ->  Nested Loop  (cost=0.45..60232.42 rows=8508 width=8) (actual time=0.047..103.652 rows=8508 loops=1)
+--               ->  Seq Scan on tickets  (cost=0.00..156.08 rows=8508 width=8) (actual time=0.009..11.393 rows=8508 loops=1)
+--               ->  Memoize  (cost=0.45..7.55 rows=1 width=8) (actual time=0.007..0.007 rows=1 loops=8508)
+--                     Cache Key: tickets.session_place_price_id
+--                     Cache Mode: logical
+--                     Hits: 572  Misses: 7936  Evictions: 0  Overflows: 0  Memory Usage: 837kB
+--                     ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..7.54 rows=1 width=8) (actual time=0.004..0.004 rows=1 loops=7936)
+--                           Index Cond: (id = tickets.session_place_price_id)
+--         ->  Hash  (cost=294.73..294.73 rows=6275 width=4) (actual time=25.310..25.319 rows=6268 loops=1)
 --               Buckets: 8192  Batches: 1  Memory Usage: 285kB
---               ->  Bitmap Heap Scan on sessions  (cost=88.84..294.48 rows=6265 width=4) (actual time=0.807..14.501 rows=6258 loops=1)
+--               ->  Bitmap Heap Scan on sessions  (cost=88.92..294.73 rows=6275 width=4) (actual time=0.539..13.009 rows=6268 loops=1)
 --                     Recheck Cond: (date_start >= (CURRENT_DATE - '7 days'::interval))
 --                     Heap Blocks: exact=96
---                     ->  Bitmap Index Scan on idx_date_start  (cost=0.00..87.28 rows=6265 width=0) (actual time=0.749..0.750 rows=6258 loops=1)
+--                     ->  Bitmap Index Scan on idx_date_start  (cost=0.00..87.35 rows=6275 width=0) (actual time=0.501..0.503 rows=6268 loops=1)
 --                           Index Cond: (date_start >= (CURRENT_DATE - '7 days'::interval))
--- Planning Time: 1.803 ms
--- Execution Time: 214.109 ms
+-- Planning Time: 1.056 ms
+-- Execution Time: 151.705 ms
 
-
--- Вывод: Индексы не повлияли на производительность запроса.
+-- Вывод: Индексы повлияли на производительность запроса негативно.
--- sql/10000_items/tickets_sold_per_week.sql
+++ sql/10000_items/tickets_sold_per_week.sql
@@ -1,62 +1,55 @@
-explain analyse SELECT COUNT(*) AS ticket_count
+explain analyse SELECT COUNT(tickets.id) AS ticket_count
 FROM tickets
      JOIN session_place_price ON tickets.session_place_price_id = session_place_price.id
      JOIN sessions ON session_place_price.session_id = sessions.id
 WHERE sessions.date_start >= CURRENT_DATE - INTERVAL '7 days';
 
 -- Резульат:
--- Aggregate  (cost=3053.53..3053.54 rows=1 width=8) (actual time=210.327..210.348 rows=1 loops=1)
---   ->  Hash Join  (cost=718.91..3047.98 rows=2220 width=0) (actual time=29.409..207.983 rows=1959 loops=1)
+-- Aggregate  (cost=11450.15..11450.16 rows=1 width=8) (actual time=30.403..30.424 rows=1 loops=1)
+--   ->  Hash Join  (cost=282.13..11449.02 rows=452 width=4) (actual time=13.718..29.519 rows=684 loops=1)
 --         Hash Cond: (session_place_price.session_id = sessions.id)
---         ->  Merge Join  (cost=427.71..2742.81 rows=5315 width=4) (actual time=15.156..184.653 rows=5315 loops=1)
---               Merge Cond: (session_place_price.id = tickets.session_place_price_id)
---               ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..188254.01 rows=6000105 width=8) (actual time=0.017..78.603 rows=59987 loops=1)
---               ->  Sort  (cost=427.04..440.33 rows=5315 width=4) (actual time=15.022..21.438 rows=5315 loops=1)
---                     Sort Key: tickets.session_place_price_id
---                     Sort Method: quicksort  Memory: 193kB
---                     ->  Seq Scan on tickets  (cost=0.00..98.15 rows=5315 width=4) (actual time=0.010..7.471 rows=5315 loops=1)
---         ->  Hash  (cost=239.00..239.00 rows=4176 width=4) (actual time=14.231..14.236 rows=4175 loops=1)
---               Buckets: 8192  Batches: 1  Memory Usage: 211kB
---               ->  Seq Scan on sessions  (cost=0.00..239.00 rows=4176 width=4) (actual time=0.026..7.999 rows=4175 loops=1)
+--         ->  Nested Loop  (cost=0.43..11163.76 rows=1356 width=8) (actual time=0.039..13.015 rows=1406 loops=1)
+--               ->  Seq Scan on tickets  (cost=0.00..25.56 rows=1356 width=8) (actual time=0.018..1.933 rows=1406 loops=1)
+--               ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..8.21 rows=1 width=8) (actual time=0.004..0.004 rows=1 loops=1406)
+--                     Index Cond: (id = tickets.session_place_price_id)
+--         ->  Hash  (cost=239.84..239.84 rows=3349 width=4) (actual time=13.664..13.669 rows=4104 loops=1)
+--               Buckets: 8192 (originally 4096)  Batches: 1 (originally 1)  Memory Usage: 209kB
+--               ->  Seq Scan on sessions  (cost=0.00..239.84 rows=3349 width=4) (actual time=0.033..7.664 rows=4104 loops=1)
 --                     Filter: (date_start >= (CURRENT_DATE - '7 days'::interval))
---                     Rows Removed by Filter: 5825
--- Planning Time: 0.579 ms
--- Execution Time: 210.396 ms
-
-
+--                     Rows Removed by Filter: 5896
+-- Planning Time: 0.380 ms
+-- Execution Time: 30.484 ms
 
 -- Индекс на столбец session_place_price_id в таблице tickets.
-CREATE INDEX idx_sold_date_film_id ON tickets (sold_date, film_id);
+CREATE INDEX idx_session_place_price_id ON tickets (session_place_price_id);
 -- Индекс на столбец session_id в таблице session_place_price.
 CREATE INDEX idx_session_id ON session_place_price (session_id);
 -- Индекс на столбец date_start в таблице sessions.
 CREATE INDEX idx_date_start ON sessions (date_start);
 
-explain analyse SELECT COUNT(*) AS ticket_count
+explain analyse SELECT COUNT(tickets.id) AS ticket_count
 FROM tickets
          JOIN session_place_price ON tickets.session_place_price_id = session_place_price.id
          JOIN sessions ON session_place_price.session_id = sessions.id
 WHERE sessions.date_start >= CURRENT_DATE - INTERVAL '7 days';
 
 -- Результат после приминения индексов:
--- Aggregate  (cost=3016.25..3016.26 rows=1 width=8) (actual time=224.546..224.571 rows=1 loops=1)
---   ->  Hash Join  (cost=681.64..3010.70 rows=2220 width=0) (actual time=39.063..222.107 rows=1959 loops=1)
+-- Aggregate  (cost=2363.08..2363.09 rows=1 width=8) (actual time=191.246..191.268 rows=1 loops=1)
+--   ->  Hash Join  (cost=249.42..2361.64 rows=577 width=4) (actual time=17.535..190.373 rows=684 loops=1)
 --         Hash Cond: (session_place_price.session_id = sessions.id)
---         ->  Merge Join  (cost=427.71..2742.80 rows=5315 width=4) (actual time=21.118..194.858 rows=5315 loops=1)
---               Merge Cond: (session_place_price.id = tickets.session_place_price_id)
---               ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..188252.43 rows=6000000 width=8) (actual time=0.027..80.631 rows=59987 loops=1)
---               ->  Sort  (cost=427.04..440.33 rows=5315 width=4) (actual time=20.958..27.475 rows=5315 loops=1)
---                     Sort Key: tickets.session_place_price_id
---                     Sort Method: quicksort  Memory: 193kB
---                     ->  Seq Scan on tickets  (cost=0.00..98.15 rows=5315 width=4) (actual time=0.017..10.546 rows=5315 loops=1)
---         ->  Hash  (cost=201.73..201.73 rows=4176 width=4) (actual time=17.897..17.904 rows=4175 loops=1)
---               Buckets: 8192  Batches: 1  Memory Usage: 211kB
---               ->  Bitmap Heap Scan on sessions  (cost=64.65..201.73 rows=4176 width=4) (actual time=0.527..9.232 rows=4175 loops=1)
+--         ->  Merge Join  (cost=2.20..2110.73 rows=1406 width=8) (actual time=0.241..170.255 rows=1406 loops=1)
+--               Merge Cond: (tickets.session_place_price_id = session_place_price.id)
+--               ->  Index Scan using idx_session_place_price_id on tickets  (cost=0.28..93.37 rows=1406 width=8) (actual time=0.019..2.676 rows=1406 loops=1)
+--               ->  Index Scan using session_place_price_pkey on session_place_price  (cost=0.43..188252.30 rows=5999991 width=8) (actual time=0.018..85.699 rows=59971 loops=1)
+--         ->  Hash  (cost=195.92..195.92 rows=4104 width=4) (actual time=17.270..17.278 rows=4104 loops=1)
+--               Buckets: 8192  Batches: 1  Memory Usage: 209kB
+--               ->  Bitmap Heap Scan on sessions  (cost=60.10..195.92 rows=4104 width=4) (actual time=0.432..8.874 rows=4104 loops=1)
 --                     Recheck Cond: (date_start >= (CURRENT_DATE - '7 days'::interval))
 --                     Heap Blocks: exact=64
---                     ->  Bitmap Index Scan on idx_date_start  (cost=0.00..63.61 rows=4176 width=0) (actual time=0.485..0.487 rows=4175 loops=1)
+--                     ->  Bitmap Index Scan on idx_date_start  (cost=0.00..59.07 rows=4104 width=0) (actual time=0.388..0.390 rows=4104 loops=1)
 --                           Index Cond: (date_start >= (CURRENT_DATE - '7 days'::interval))
--- Planning Time: 1.206 ms
--- Execution Time: 224.674 ms
+-- Planning Time: 1.472 ms
+-- Execution Time: 191.351 ms
+
 
 -- Вывод: Индексы повлияли на производительность запроса негативно.

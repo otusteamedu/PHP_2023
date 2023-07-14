24c7880e13d55a53df3cc6452269e235a34cde80
Vyacheslav Shevchenko <webdev2030@gmail.com>
2023-07-14 23:17:04 +0600

    hw29 - решена задача https://leetcode.com/problems/fraction-to-recurring-decimal/

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ docker-compose up -d
 ```
 
 ## Проверка наличия петли в списке
+Решение тут: \data\htdocs\src\Sva\Common\App\LinkedListUtils.php
 ```shell
 # Выполнение команд внутри контейнера app
 php public/index.php 
+```
+
+## Вывод частного в виде строки с учётом периода
+Решение тут: \data\htdocs\commandes.php
+```shell
+# Выполнение команд внутри контейнера app
+php public/index.php fractionToDecimal
 ```
 
--- data/htdocs/commandes.php
+++ data/htdocs/commandes.php
@@ -24,5 +24,56 @@ return [
         $nodeB2->next = $nodeA3;
 
         \Sva\Common\App\LinkedListUtils::getIntersect($nodeA1, $nodeB1);
+    },
+    'fractionToDecimal' => function () {
+        function fractionToDecimal($numerator, $denominator): string
+        {
+            if ($numerator == 0) {
+                return "0";
+            }
+
+            $result = "";
+
+            // Определяем знак результата
+            if (($numerator < 0) ^ ($denominator < 0)) {
+                $result .= "-";
+            }
+
+            $numerator = abs($numerator);
+            $denominator = abs($denominator);
+
+            $result .= intdiv($numerator, $denominator);
+            $remainder = $numerator % $denominator;
+
+            if ($remainder == 0) {
+                return $result;
+            }
+
+            $result .= '.';
+            $fraction = '';
+            $map = [];
+            while ($remainder != 0) {
+                if (isset($map[$remainder])) {
+                    $start = $map[$remainder];
+                    $nonRepeatingPart = substr($fraction, 0, $start);
+                    $repeatingPart = substr($fraction, $start);
+                    return $result . $nonRepeatingPart . "(" . $repeatingPart . ")";
+                }
+
+                $map[$remainder] = strlen($fraction);
+                $remainder *= 10;
+                $fraction .= floor($remainder / $denominator);
+                $remainder %= $denominator;
+            }
+
+            return $result . $fraction;
+        }
+
+        echo fractionToDecimal(-50, 8) . PHP_EOL;
+        echo fractionToDecimal(1, 6) . PHP_EOL;
+        echo fractionToDecimal(2, 3) . PHP_EOL;
+        echo fractionToDecimal(1, 2) . PHP_EOL;
+        echo fractionToDecimal(2, 1) . PHP_EOL;
+        echo fractionToDecimal(4, 333) . PHP_EOL;
     }
 ];

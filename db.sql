99be2e5fbd1a28294a42c442f54178bc0569edc4
Ilyos Xizr <ilhomteshaboyev@gmail.com>
2023-07-20 20:54:54 +0500

    attempt-3

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
- CREATE TABLE cinemas (
+CREATE TABLE cinemas (
   cinema_id INT PRIMARY KEY,
   name VARCHAR(255),
   location VARCHAR(255)
@@ -37,22 +37,47 @@ CREATE TABLE customers (
   phone_number VARCHAR(255)
 );
 
+CREATE TABLE tariffs (
+  tariff_id INT PRIMARY KEY,
+  name VARCHAR(255),
+  price DECIMAL(10, 2)
+);
+
+CREATE TABLE seats (
+  seat_id INT PRIMARY KEY,
+  hall_id INT,
+  row_number INT,
+  seat_number INT,
+  FOREIGN KEY (hall_id) REFERENCES halls(hall_id)
+);
+
 CREATE TABLE tickets (
   ticket_id INT PRIMARY KEY,
   showtime_id INT,
+  seat_id INT,
+  tariff_id INT,
   customer_id INT,
-  seat_number VARCHAR(255),
-  price DECIMAL(10,2),
   FOREIGN KEY (showtime_id) REFERENCES showtimes(showtime_id),
+  FOREIGN KEY (seat_id) REFERENCES seats(seat_id),
+  FOREIGN KEY (tariff_id) REFERENCES tariffs(tariff_id),
   FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE sales (
+  sale_id INT PRIMARY KEY,
+  ticket_id INT,
+  sale_price DECIMAL(10, 2),
+  sale_date DATETIME,
+  FOREIGN KEY (ticket_id) REFERENCES tickets(ticket_id)
 );
 
 SELECT 
   movies.title,
-  SUM(tickets.price) AS total_revenue
+  SUM(sales.sale_price) AS total_revenue
 FROM movies
 JOIN showtimes ON movies.movie_id = showtimes.movie_id
 JOIN tickets ON showtimes.showtime_id = tickets.showtime_id
+JOIN sales ON tickets.ticket_id = sales.ticket_id
 GROUP BY movies.title
 ORDER BY total_revenue DESC
 LIMIT 1;

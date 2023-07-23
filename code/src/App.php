2926cd66dba27a04d8271bbd64f8576999dc7374
vladimir@positron-it.ru <vladimir@positron-it.ru>
2023-07-23 23:05:05 +0300

    Add app.php

---
+++ code/src/App.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Otus\Hw;
+use Otus\Hw\Client;
+use Otus\Hw\Server;
+
+class App
+{
+    public function run()
+    {
+        global $argv;
+
+        if (is_null($argv[1]) OR count($argv) > 2) {
+            exit('Вы должны передать 1 аргумент'.PHP_EOL);
+        }
+
+        $mode = $argv[1];
+        if (!in_array($mode, ['server','client'])) {
+            exit('Вы должны передать только server или client'.PHP_EOL);
+        }
+
+        if ($mode == 'server') {
+            $app = new Server();
+        }
+        if ($mode == 'client') {
+            $app = new Client();
+        }
+
+        $app->run();
+    }
+
+}

2c93797c7e2076837d69da94c26b63875f197386
fdfkc <korabelnikov.va@gmail.com>
2023-07-23 15:24:35 +0300

    Решение задачи на leetcode

---
+++ Solution.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+    function __construct($val)
+    {
+        $this->val = $val;
+    }
+}
+
+/**
+ * @param ListNode $head
+ * @return Boolean
+ */
+function hasCycle(ListNode $head): bool
+{
+    $arViewedNodes = [];
+    $viewedNodesCount = 0;
+
+    do {
+        for ($i = 0; $i < $viewedNodesCount; $i++)
+        {
+            if ($head === $arViewedNodes[$i])
+            {
+                return true;
+            }
+        }
+
+        $arViewedNodes[] = $head;
+        $viewedNodesCount++;
+    }
+    while (($head = $head->next) !== null);
+    
+    return false;
+}

ebc8c46d7b24e7a37bba30778bdae427643dc8ff
aleksey <pav@el-plat.ru>
2023-08-02 14:37:46 +0400

    init

---
+++ docker/postrgesql-initdb.d/30-insert-person-contacts.sql
@@ -0,0 +1,19 @@
+set my.limit_min = 50;
+set my.limit_max = 80;
+
+do
+$$
+    declare
+        person_r record;
+        num int := 1;
+        "limit" int;
+    begin
+        "limit" := floor(random() * (current_setting('my.limit_max')::int - current_setting('my.limit_min')::int + 1) + current_setting('my.limit_min')::int)::int;
+
+        FOR person_r IN SELECT id from person p order by random() limit "limit" LOOP
+
+                INSERT INTO public."person_contacts" (person_id, value) VALUES(person_r.id, concat('Контакт ', to_char(num, '000')));
+                num := num + 1;
+            END LOOP;
+    end;
+$$;

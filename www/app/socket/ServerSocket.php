654602af589118b181679e232078dac86b6e83e9
igonin-1993 <igonin-1993@yandex.ru>
2023-08-07 02:18:50 +0500

    hw6 - 1

---
+++ www/app/socket/ServerSocket.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MIgonin\ConsoleSocketChat\Socket;
+
+use Generator;
+
+class ServerSocket extends Socket
+{
+    protected function processChat(): Generator
+    {
+        yield 'Awaiting for client message...' . PHP_EOL;
+
+        $client = $this->accept();
+
+        while (true) {
+            $message = $this->receive($client);
+            if (! is_null($message['message'])) {
+                yield $message['message'] . PHP_EOL;
+
+                $this->write($message['length'], $client);
+            }
+        }
+    }
+
+    protected function initSocket(): void
+    {
+        $this->create(true);
+        $this->bind();
+        $this->listen();
+    }
+}

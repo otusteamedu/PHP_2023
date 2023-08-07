e425aca716f5b9a21540da9cf8b2f69eb0c0f401
nalofree <nalofree@gmail.com>
2023-08-08 03:01:58 +0800

    Заменена проверка аргументов на число

--- summ.sh
+++ summ.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 NUM1=$1
 NUM2=$2
-if [[ $NUM1 =~ ^[\-\+]?[0-9]+\.?[0-9]*$ ]]; then
-        if [[ $NUM2 =~ ^[\-\+]?[0-9]+\.?[0-9]*$ ]]; then
+if [[ $NUM1 =~ [[:digit:]] ]]; then
+        if [[ $NUM2 =~ [[:digit:]] ]]; then
                 echo "summ: "
                 echo "$NUM1 $NUM2" | awk '{print $1 + $2}'
         else

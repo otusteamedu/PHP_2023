a046de920ced7cdf9f7fd0390eff034454084234
jasur <jasfayz@gmail.com>
2023-08-08 09:46:33 +0500

    init

---
+++ app/index.php
@@ -0,0 +1,40 @@
+<?php
+$memcache = new Memcached();
+$memcache->addServer('memcache', 11211);
+$memcache->get('connection');
+
+require_once './vendor/autoload.php';
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+    <?php
+
+    //echo phpinfo();
+    echo "Привет!<br>" . date("Y-m-d H:i:s") . "<br><br>";
+
+    echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+
+    ?>
+
+    <form action="bracket.php" method="POST">
+        <input type="text" class="form-control mb-2" name="bracket">
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+</body>
+</html>

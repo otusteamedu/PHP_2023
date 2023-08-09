a57abd348115bce56f20091560936ba12e84be30
jasur <jasfayz@gmail.com>
2023-08-09 09:32:39 +0500

    delete unused files

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,15 +3,14 @@
 use Pecee\SimpleRouter\SimpleRouter;
 
 SimpleRouter::get('/', function () {
-    $content = (new \Devanych\View\Renderer('views'))->render('main');
-    return $content;
+    return (new \Devanych\View\Renderer('views'))->render('main');
 });
 
 SimpleRouter::post('/check-brackets', function () {
     $bracket_string = input()->post('bracket', null);
 
     if (strlen($bracket_string) === 0) {
-        throw new Exception('Provide Empty string');
+        throw new Exception('Provided Empty string');
     }
     $bracket = new JasFayz\BracketChecker\Bracket($bracket_string);
     http_response_code($bracket->checker() ? 200 : 400);

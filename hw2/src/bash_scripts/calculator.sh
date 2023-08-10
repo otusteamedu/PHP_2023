35822da437a895a36d0e55fb7a7aec8ecd5a8414
Iva-So <sochkov1994@mail.ru>
2023-08-10 11:49:22 +0400

    fix: calculator

--- hw2/src/bash_scripts/calculator.sh
+++ hw2/src/bash_scripts/calculator.sh
@@ -5,13 +5,12 @@ echo "Привет! Я - калькулятор, могу сложить два 
 while true; do
   read -p "Введите первое число: " number1
   read -p "Введите второе число: " number2
-  if [[ $number1 =~ [[:digit:]] && $number2 =~ [[:digit:]] ]]; then
-    sum=$(echo "$number1 + $number2" | bc)
+  pattern='^[+-]?[0-9]+(\.[0-9]+)?$'
+  if [[ $number1 =~ $pattern && $number2 =~ $pattern ]]; then
+    sum=$(echo "$number1 $number2" | awk '{printf "%.1f", $1 + $2}')
     echo "Сумма чисел равна: $number1 + $number2 = $sum"
     break
   else
     echo "Где-то ввели не число, попробуем еще раз"
   fi
 done
-
-

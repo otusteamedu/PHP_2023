38ab8baa39e73fe644a5dc3c2e4cb207dd17dc7b
damir <khalikov@beta.agency>
2023-08-14 06:27:09 +0400

    fix

--- www/index.php
+++ www/index.php
@@ -1,20 +1,2 @@
 <?php
-echo 'otus<br/><br/>';
-
-$redis = new Redis();
-$redis->connect('redis');
-
-var_dump($redis->ping());
-
-$memcached = new Memcached();
-$memcached->addServer('memcached', 11211);
-
-echo 'memcached version: ' . $memcached->getVersion()['memcached:11211'] . '<br/>';
-
-try {
-    $dsn = 'mysql:host=db;dbname=otus';
-    $dbh = new \PDO($dsn, 'otus', '123');
-    echo 'mysql connection<br/>';
-} catch (PDOException $exception) {
-    echo $exception->getMessage();
-}
+echo 'hello world';

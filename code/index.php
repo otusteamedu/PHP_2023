c516d8875ea8b777ab657f524ab99ad83ea1cbbf
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2023-08-15 23:39:21 +0300

    подключил свою библиотеку amedvedev/first-package

--- code/index.php
+++ code/index.php
@@ -1,1 +1,10 @@
-hello docker 1
+<?php
+
+declare(strict_types = 1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+use Amedvedev\FirstPackage\MyWords;
+
+$myWords = new MyWords();
+
+echo $myWords->getMyWords()[0];

47ee6e93dab5c8c2b0843b9a858121c819dbaad2
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2023-08-15 23:53:21 +0300

    sm change

--- code/index.php
+++ code/index.php
@@ -1,8 +1,9 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
 use Amedvedev\FirstPackage\MyWords;
 
 $myWords = new MyWords();

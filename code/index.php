623938ff3c70f1827b63d6c581b562a7bb799e78
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2023-08-16 21:48:21 +0300

    sm phpdoc fix

--- code/index.php
+++ code/index.php
@@ -4,11 +4,12 @@
  * php version 8.2.8
  *
  * @category ItIsDepricated
- * @package  AmedvedevPHP2023 Otus
+ * @package  AmedvedevPHP2023Otus
  * @author   Alex 150Rus <alex150rus@outlook.com>
  * @licence  http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version  GIT: @1.0.0@
  * @link     https://github.com/Alex150Rus
+ * @license  GNU Public License
  */
 declare(strict_types=1);
 

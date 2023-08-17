ac88439952d62a1707b6d86c6d9e02fd1c84122f
Lebedev.vr <prizman201@gmail.com>
2023-08-18 00:02:01 +0300

    HW-12 fix cs issues

--- app/public/index.php
+++ app/public/index.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 try {
-    $app = new \Neunet\App\App;
+    $app = new \Neunet\App\App();
     $app->run();
 } catch (Exception $exception) {
     echo $exception->getMessage();

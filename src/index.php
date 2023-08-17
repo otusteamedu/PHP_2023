69219d9cc82f61fc07a8f880d2264de74887fb06
cherepanov.a <art.cherepan@gmail.com>
2023-08-17 13:41:56 +0700

    hw3 edit index.php

--- src/index.php
+++ src/index.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-require __DIR__ . '/vendor/autoload.php';
+use ArtemCherepanov\OtusComposerPackage\StringProcessor;
 
-$service = new \ArtemCherepanov\OtusComposer\Application\StringService();
-$command = new \ArtemCherepanov\OtusComposer\Infrastructure\StringCommand($service);
-$command->execute();
+require __DIR__ . '/../vendor/autoload.php';
+
+$processor = new StringProcessor();
+echo $processor->getLength('foo bar');

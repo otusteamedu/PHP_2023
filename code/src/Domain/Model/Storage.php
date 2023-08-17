81f30f94c54bc58181edc9479f267646ff8a8905
Artur <se6@rep.earth>
2023-08-17 12:55:28 +0300

    fix ci 2

--- code/src/Domain/Model/Storage.php
+++ code/src/Domain/Model/Storage.php
@@ -6,9 +6,20 @@ namespace Art\Code\Domain\Model;
 
 abstract class Storage
 {
+    /**
+     * @param array $params
+     * @return mixed
+     */
     abstract function find(array $params): mixed;
 
+    /**
+     * @param Event $event
+     * @return bool
+     */
     abstract function add(Event $event): bool;
 
+    /**
+     * @return void
+     */
     abstract function clear(): void;
 }

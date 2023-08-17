e971e2101df256e87af233990dfa497ec6602027
nalofree <nalofree@gmail.com>
2023-08-17 23:46:20 +0800

    psr fix

--- www/index.php
+++ www/index.php
@@ -3,4 +3,4 @@ include "./vendor/autoload.php";
 
 $measure = new \nalofree\utils\Pluar;
 $count = 12;
-echo ($count.' '.$measure->getRu(12, ['негритёнок', 'негритёнка', 'негритят']));
+echo($count . ' ' . $measure->getRu(12, ['негритёнок', 'негритёнка', 'негритят']));

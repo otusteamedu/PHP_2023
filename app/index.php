88bd062239973404df47d844697e0827b3df8581
Alexander <alexanderbalabas1993@gmail.com>
2023-08-18 04:20:49 +0300

    configuration docker

---
+++ app/index.php
@@ -0,0 +1,29 @@
+<?php
+// echo '<pre>';
+// var_dump( scandir(__DIR__) );
+
+// exit;
+
+require_once 'vendor/autoload.php';
+
+try {
+    $redis = new \Predis\Client([
+        'host' => 'redis'
+    ]);
+    $redis->set('message', 'Redis is up!');
+
+    echo $redis->get('message') . "<br>";
+} catch (Exception $e) {
+    throw new Exception('Redis error: ' . $e->getMessage());
+}
+
+try {
+    $memcached = new Memcached();
+    $memcached->addServer('memcached', 11211, 42);
+
+    $memcached->add('message', 'Memcached is up!');
+
+    echo $memcached->get('message');
+} catch (Exception $e) {
+    throw new Exception('Memcached error: ' . $e->getMessage());
+}

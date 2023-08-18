cf4cf217c8a6cba618ec0de26092bc2a15c9f28a
Alexander <alexanderbalabas1993@gmail.com>
2023-08-18 16:57:05 +0300

    init hw1

---
+++ app/index.php
@@ -0,0 +1,29 @@
+<?php
+// echo '<pre>';
+echo date('Y-m-d',time());
+// var_dump( scandir(__DIR__) );
+
+// exit;
+
+require_once 'vendor/autoload.php';
+
+try {
+    $redis = new \Predis\Client([
+        'host' => 'redis'
+    ]);
+    $redis->set('message', 'Redis is up!');
+
+    echo $redis->get('message') . "<br>";
+} catch (Exception $e) {
+    throw new Exception('Redis error: ' . $e->getMessage());
+}
+
+try {
+    $memcached = new Memcached();
+    $memcached->addServer('memcached', 11211);
+
+    $memcached->add('message', 'Memcached is up!');
+    echo $memcached->get('message');
+} catch (Exception $e) {
+    throw new Exception('Memcached error: ' . $e->getMessage());
+}

a2a2d40cea310deb01b08d824abed8b1ce36d7d0
neu_net <prizman2000@mail.ru>
2023-08-20 18:49:02 +0300

    HW-14 leetcode 17 task

---
+++ solution.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+class Solution {
+    /**
+     * @param String $digits
+     * @return String[]
+     */
+    function letterCombinations(string $digits): array
+    {
+        if (!strlen($digits)) {
+            return [];
+        }
+
+        $slices = [
+            2 => ['a', 'b', 'c'],
+            3 => ['d', 'e', 'f'],
+            4 => ['g', 'h', 'i'],
+            5 => ['j', 'k', 'l'],
+            6 => ['m', 'n', 'o'],
+            7 => ['p', 'q', 'r', 's'],
+            8 => ['t', 'u', 'v'],
+            9 => ['w', 'x', 'y', 'z']
+        ];
+
+        $res = [];
+        return $this->recursion(0, $digits, $slices, $res);
+    }
+
+    private function recursion(int $index, string $digits, array $slices, array &$res, string $accum = ''): array
+    {
+        foreach ($slices[$digits[$index]] as $slice) {
+            if (isset($digits[$index + 1])) {
+                $this->recursion($index + 1, $digits, $slices, $res, $accum . $slice[0]);
+            } else {
+                $res[] = $accum . $slice[0];
+            }
+        }
+        return $res;
+    }
+}
+
+$solution = new Solution();
+print_r($solution->letterCombinations('234'));

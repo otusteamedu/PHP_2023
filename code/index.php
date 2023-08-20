8cc691f2e3a40fbac54fa2c5cc0d9405ddf417e9
builov <builov@inbox.ru>
2023-08-20 13:31:18 +0300

    php8.2

---
+++ code/index.php
@@ -0,0 +1,19 @@
+<?php
+
+//phpinfo();
+
+require __DIR__ . '/vendor/autoload.php';
+try {
+    $redis = new Predis\Client('tcp://redis:6379');
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+
+$redis->set('hello', "Hi Redis!");
+$hello = $redis->get('hello');
+
+echo $hello;
+
+
+
+

86bd13820fd53ec1da9ebb87ab324e080ed9af06
Nikita <stubbornbattery@gmail.com>
2023-08-22 17:09:34 +0500

    hw1

--- code/index.php
+++ code/index.php
@@ -11,7 +11,6 @@ $memcached->addServer('memcached', 11211);
 var_dump($memcached->getVersion());
 
 
-
 $db = 'db';
 $userName = 'user';
 $pass = 'pswd';
@@ -19,15 +18,8 @@ $pass = 'pswd';
 
 $conn = new mysqli($db, $userName, $pass);
 
-if($conn->connect_error){
-	die("Connection failed: " . $conn->connect_error);
-} 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 echo 'Connected!';
-
-// try {
-//     $dsn = 'mysql:host=' . $_ENV['MYSQL_HOST'] . ';dbname=' . $_ENV['MYSQL_DATABASE'];
-//     $dbh = new \PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
-// } catch (PDOException $exception) {
-//     echo $exception->getMessage();
-// }

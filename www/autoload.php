c355f984b06b6d277427331dac2bc1a772ba3ca7
nalofree <nalofree@gmail.com>
2023-08-23 17:50:16 +0800

    psr fix

--- www/autoload.php
+++ www/autoload.php
@@ -2,17 +2,17 @@
 
 function autoload($className)
 {
-  $prefix = 'Nalofree\\WsTest';
-  $baseDir = './src';
-  if (substr($className, 0, strlen($prefix)) == $prefix) {
-    $className = substr($className, strlen($prefix));
-    $className = $baseDir . $className;
-  }
-  $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-  $fileName = $className . ".php";
-  if (is_readable($fileName)) {
-    require_once $fileName;
-  }
+    $prefix = 'Nalofree\\WsTest';
+    $baseDir = './src';
+    if (substr($className, 0, strlen($prefix)) == $prefix) {
+        $className = substr($className, strlen($prefix));
+        $className = $baseDir . $className;
+    }
+    $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+    $fileName = $className . ".php";
+    if (is_readable($fileName)) {
+        require_once $fileName;
+    }
 }
 
 spl_autoload_register("autoload");

e6d418cead4a6fd444705bc5009ba41544699e71
nalofree <nalofree@gmail.com>
2023-08-29 18:45:24 +0800

    init commit

---
+++ www/index.php
@@ -0,0 +1,30 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use \Nalofree\Hw5\EmailChecker;
+
+if ($_SERVER['REQUEST_URI'] === '/form') {
+    require "./views/form.php";
+} else {
+    if ($_SERVER['REQUEST_URI'] !== '/') { // любой неверный роут ведет к ошибке
+        header('HTTP/1.1 400 Bad Reuqest');
+        exit('Текст, что всё плохо');
+    }
+    if (isset($_POST['string'])) {
+        $checker = new EmailChecker($_POST['string']);
+        if (empty($checker->check())) {
+            header('HTTP/1.1 400 Bad Reuqest');
+            exit('Ни одного емейла. Текст, что всё плохо');
+        } else {
+            header('HTTP/1.1 200 OK');
+            echo "<pre>";
+            print_r($checker->check());
+            echo "</pre>";
+            exit('Текст, что всё хорошо');
+        }
+    } else {
+        header('HTTP/1.1 400 Bad Reuqest');
+        exit('В запросе нет нужных данных. Текст, что всё плохо');
+    }
+}
---
+++ www/views/form.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Bootstrap demo</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+              integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <form action="/" method="post" class="form">
+                <div class="mb-3">
+                    <label for="email-list" class="form-label">Список email через пробел или запятую</label>
+                    <textarea class="form-control" name="string" id="email-list" cols="30" rows="10" autofocus></textarea>
+                </div>
+                <div class="mb-3">
+                    <input type="submit" value="Проверить адреса" class="btn btn-primary">
+                </div>
+            </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+                crossorigin="anonymous"></script>
+    </body>
+</html>

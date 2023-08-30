c52c6ee6cccaa8fd2bbc454a3cdd6361f9a41cb3
cherepanov.a <art.cherepan@gmail.com>
2023-08-30 17:48:33 +0700

    hw4 fix cs sniffer

--- src/Services/StringValidatorService.php
+++ src/Services/StringValidatorService.php
@@ -4,20 +4,21 @@ namespace Services;
 
 class StringValidatorService
 {
-    public function isBracketsValid($string) {
+    public function isBracketsValid($string): bool
+    {
         $stringToArr = str_split($string);
         $openingClosingMatchSymbols = ['(' => ')', '{' => '}', '[' => ']'];
         $closingSymbolOpeningMatch = array_flip($openingClosingMatchSymbols);
         $stack = [];
 
         foreach ($stringToArr as $key) {
-            if(array_key_exists($key, $openingClosingMatchSymbols)) {
+            if (array_key_exists($key, $openingClosingMatchSymbols)) {
                 $stack[] = $key;
             } else {
-                if(count($stack) === 0) {
+                if (count($stack) === 0) {
                     return false;
                 }
-                else if($closingSymbolOpeningMatch[$key] === end($stack)) {
+                elseif ($closingSymbolOpeningMatch[$key] === end($stack)) {
                     array_pop($stack);
                 }
             }

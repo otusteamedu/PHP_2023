b298502293957c539f8b50633fe6ff7cff5529eb
Lebedev.vr <prizman201@gmail.com>
2023-09-01 00:18:20 +0300

    HW-16 Add fixtures and atomicity tests with rollback

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['dev' => true, 'test' => true]
 ];
--- app/tests/Functional/Music/Infrastructure/Repository/UserRepositoryTest.php
+++ app/tests/Functional/Music/Infrastructure/Repository/UserRepositoryTest.php
@@ -6,20 +6,25 @@ namespace App\Tests\Functional\Music\Infrastructure\Repository;
 
 use App\Music\Domain\Factory\UserFactory;
 use App\Music\Infrastructure\Repository\UserRepository;
+use App\Tests\Resource\Fixture\UserFixture;
 use Faker\Factory;
 use Faker\Generator;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserRepositoryTest extends WebTestCase
 {
     private UserRepository $repository;
     private Generator $faker;
+    private AbstractDatabaseTool $databaseTool;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->repository = static::getContainer()->get(UserRepository::class);
         $this->faker = Factory::create();
+        $this->databaseTool = static::getContainer()->get(DatabaseToolCollection::class)->get();
     }
 
     public function test_user_added_successfully(): void
@@ -33,6 +38,19 @@ class UserRepositoryTest extends WebTestCase
 
         // assert
         $existingUser = $this->repository->findById($user->getId());
+        $this->assertEquals($user->getId(), $existingUser->getId());
+    }
+
+    public function test_user_found_successfully(): void
+    {
+        // arrange
+        $executor = $this->databaseTool->loadFixtures([UserFixture::class]);
+        $user = $executor->getReferenceRepository()->getReference(UserFixture::REFERENCE);
+
+        // act
+        $existingUser = $this->repository->findById($user->getId());
+
+        // assert
         $this->assertEquals($user->getId(), $existingUser->getId());
     }
 }

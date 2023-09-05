0bb1fc8470e9b6f86a69a7f141857adfc401cdb9
Evgeny Romanov <e.romanov93@yandex.ru>
2023-09-05 12:02:23 +0500

    добавление таблички с ценами

--- query.sql
+++ query.sql
@@ -2,9 +2,9 @@ SELECT
     m.name,
     SUM(t.price) AS sum_price
 FROM
-    ticket t
-    JOIN `session` s ON s.id = t.session_id
-    JOIN movie m ON m.id = s.movie_id
+    tickets t
+    JOIN `sessions` s ON s.id = t.session_id
+    JOIN movies m ON m.id = s.movie_id
 GROUP BY
     m.name
 ORDER BY

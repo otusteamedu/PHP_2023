154e6b454bcd80e9a6d24337e5bd469680b0b9fd
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2023-09-06 23:53:56 +0300

    sm change

--- code/services/EmailValidatorService.php
+++ code/services/EmailValidatorService.php
@@ -40,7 +40,8 @@ class EmailValidatorService
         $answerArray = [];
         foreach ($array as $email) {
             $answerArray[$email] = (bool)preg_match("/^[-a-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/",
-                $email);
+                $email
+            );
         }
         return $answerArray;
     }

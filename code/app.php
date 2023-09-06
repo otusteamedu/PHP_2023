0644aa6c207ff97df3d2493c6902c70e6e2df03d
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2023-09-06 23:39:41 +0300

    email валидатор

--- code/app.php
+++ code/app.php
@@ -19,19 +19,37 @@ namespace Amedvedev\code;
 use Amedvedev\code\presenters\BracketsHandlerPresenter;
 use Amedvedev\code\presenters\MainPresenter;
 use Amedvedev\code\presenters\MemcachedPresenter;
+use Amedvedev\code\services\EmailValidatorService;
 
 class App
 {
     public function run()
     {
         $mainPresenter = new MainPresenter();
+
         $bracketsHandlerPresenter = new BracketsHandlerPresenter();
         $bracketsInfoHtml = $bracketsHandlerPresenter->render($_POST);
+
         $memcachedPresenter = new MemcachedPresenter();
         $memcachedHtml = $memcachedPresenter->render();
 
         $formHtml = $mainPresenter->render();
-        $html = $bracketsInfoHtml . $formHtml . $memcachedHtml;
+
+        $emails = [
+            'alex150rus@outlook.com',
+            'alekhimkirus58-0@yandex.ru',
+            '----@----.----',
+            'admin@localhost',
+            'бывший-премьер@правительство.рф'
+        ];
+        $emailValidatorService = new EmailValidatorService();
+        $regExpValidityCheckResult = $emailValidatorService->regExpValidityCheck($emails);
+        $simpleValidityCheckResult = $emailValidatorService->simpleValidityCheck($emails);
+
+
+        $html = $bracketsInfoHtml . $formHtml . $memcachedHtml
+            . 'Валидация при помощи регулярки: ' . json_encode($regExpValidityCheckResult) . '<br>'
+            . 'Простая валидация: ' . json_encode($simpleValidityCheckResult);
 
         return $html;
     }
---
+++ code/services/EmailValidatorService.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Валидатор Email
+ * php version 8.2.8
+ *
+ * @category ItIsDepricated
+ * @package  AmedvedevPHP2023Otus
+ * @author   Alex 150Rus <alex150rus@outlook.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @Version  GIT: 1.0.0
+ * @link     http://github.com/Alex150Rus My_GIT_profile
+ */
+
+declare(strict_types=1);
+
+namespace Amedvedev\code\services;
+
+class EmailValidatorService
+{
+    /**
+     * @param array $array
+     * @return array
+     */
+    public function simpleValidityCheck(array $array): array
+    {
+        $answerArray = [];
+        foreach ($array as $email) {
+            $answerArray[$email] = (bool)filter_var($email, FILTER_VALIDATE_EMAIL);
+        }
+        return $answerArray;
+    }
+
+    /**
+     * @param array $array
+     * @return array
+     */
+    public function regExpValidityCheck(array $array): array
+    {
+        $answerArray = [];
+        foreach ($array as $email) {
+            $answerArray[$email] = (bool)preg_match("/^[-a-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-a-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/",
+                $email);
+        }
+        return $answerArray;
+    }
+}

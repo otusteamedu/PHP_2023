e2495213e98cf8a36f649de4822a1509449e0cd0
Dima Teplukhin <dimonvelsk@gmail.com>
2023-09-09 14:46:43 +0300

    refactor(leetcode): v2

--- leetcode.php
+++ leetcode.php
@@ -22,24 +22,18 @@ class Solution
      */
     function mergeTwoLists($list1, $list2)
     {
-        $result = [];
-        $output = null;
+        $output = $result = new ListNode();
         do {
-            while ($list2 && ($list2->val <= $list1->val || !$list1)) {
-                $result[] = $list2->val;
+            if (($list2 && $list2->val < $list1->val) || !$list1) {
+                $result->next = new ListNode($list2->val);
                 $list2 = $list2->next;
-            }
-
-            if ($list1) {
-                $result[] = $list1->val;
+            } else {
+                $result->next = new ListNode($list1->val);
                 $list1 = $list1->next;
             }
+            $result = $result->next;
         } while ($list1 || $list2);
 
-        for ($i = count($result) - 1; $i >= 0; $i--) {
-            $output = new ListNode($result[$i], $output);
-        }
-
-        return $output;
+        return $output->next;
     }
 }

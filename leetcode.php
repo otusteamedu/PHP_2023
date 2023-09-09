9abf9f9236d9c7a125f9ffa556f66775f0e5c9bf
Dima Teplukhin <dimonvelsk@gmail.com>
2023-09-09 11:14:04 +0300

    feat(leetcode): v1

---
+++ leetcode.php
@@ -0,0 +1,44 @@
+<?php
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    public function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2) {
+        $result = [];
+        $output = null;
+        do {
+            while ($list2 && ($list2->val <= $list1->val || !$list1)) {
+                $result[] = $list2->val;
+                $list2 = $list2->next;
+            }
+
+            if ($list1) {
+                $result[] = $list1->val;
+                $list1 = $list1->next;
+            }
+        } while ($list1 || $list2);
+
+        for($i=count($result)-1; $i>=0; $i--){
+            $output = new ListNode($result[$i], $output);
+        }
+
+        return $output;
+    }
+}

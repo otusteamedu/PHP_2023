528cd2a33c88eb235006a6351ff7506fcb1b5bde
fdfkc <korabelnikov.va@gmail.com>
2023-09-10 20:02:38 +0300

    Работа с rabbitMQ

---
+++ code/public/app.php
@@ -0,0 +1,12 @@
+<?php
+
+include '/data/mysite.local/vendor/autoload.php';
+
+use VKorabelnikov\Hw19\AccountStatement\Application;
+
+try {
+    $app = new Application();
+    $app->run();
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
---
+++ code/src/AccountStatement/FormHandler.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VKorabelnikov\Hw19\AccountStatement;
+
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+
+class FormHandler
+{
+    public function run(AMQPStreamConnection $connection, string $queueName): string
+    {
+        $channel = $connection->channel();
+        $channel->queue_declare($queueName, false, true, false, false);
+
+        $paramsValidationRules = [
+            "accountNumber" => "AccountNumber",
+            "dateStart" => "Date",
+            "dateEnd" => "Date",
+            "email" => "Email"
+        ];
+
+        $messageParams = [];
+        foreach ($paramsValidationRules as $fieldName => $validateRule) {
+            $methodName = "is" . $validateRule . "Valid";
+            if ($this->$methodName($_POST[$fieldName])) {
+                $messageParams[] = $_POST[$fieldName];
+            } else {
+                throw new \Exception("Некорректно заполнено поле " . $fieldName);
+            }
+        }
+
+        $msg = new AMQPMessage(
+            json_encode($messageParams, JSON_UNESCAPED_UNICODE),
+            [
+                'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT
+            ]
+        );
+        $channel->basic_publish($msg, '', $queueName);
+
+        $channel->close();
+        $connection->close();
+
+        return "Ваш запрос успешно принят.";
+    }
+
+    protected function isAccountNumberValid(string $accountNumber)
+    {
+        if (preg_match("#100000\/\d{5,15}#",$accountNumber)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function isDateValid($date)
+    {
+        $sFormat = "Y.m.d";
+        $obDate = \DateTime::createFromFormat($sFormat, $date);
+        if ($obDate && ($obDate->format($sFormat) === $date)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function isEmailValid(string $email)
+    {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+
+        return false;
+    }
+}

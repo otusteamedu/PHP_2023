bd8ed0faac68e5e8ce0b9d0153ee66eaba2a8b5d
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2023-09-13 22:44:42 +0300

    задача с литкода по слиянию двух списоков

---
+++ code/leetcode/ListNode.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * php version 8.2.8
+ *
+ * @category ItIsDepricated
+ * @package  AmedvedevPHP2023Otus
+ * @author   Alex 150Rus <alex150rus@outlook.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @Version  GIT: 1.0.0
+ * @link     http://github.com/Alex150Rus My_GIT_profile
+ */
+
+declare(strict_types=1);
+
+namespace Amedvedev\code\leetcode;
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}

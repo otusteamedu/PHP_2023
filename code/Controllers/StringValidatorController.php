25430d10380a16b912e7604dcbd1170c475fc003
Evgeniy Evstifeev <evstifeevea@gmail.com>
2023-09-23 21:35:02 +0300

    Hw4/ add nginx balancer

---
+++ code/Controllers/StringValidatorController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Controllers;
+
+use Exception;
+use Services\StringValidator;
+
+class StringValidatorController
+{
+    public static function validate(string $value): string
+    {
+        try {
+            if (empty($value)) {
+                throw new Exception("String parameter is empty");
+            }
+            $stringValidator = new StringValidator();
+
+            if (!$stringValidator->isValidString($value)) {
+                throw new Exception("Your string '$value' contains invalid parentheses");
+            }
+
+            http_response_code(200);
+            return "OK: Your String '$value' is valid";
+        } catch (Exception $e) {
+            http_response_code(400);
+            return "Bad Request: " . $e->getMessage();
+        }
+
+    }
+}
--- code/index.php
+++ code/index.php
@@ -2,37 +2,11 @@
 
 declare(strict_types=1);
 
-try {
-    $string = $_POST['string'] ?? '';
+require_once __DIR__ . '/Services/StringValidator.php';
+require_once __DIR__ . '/Controllers/StringValidatorController.php';
 
-    if (empty($string)) {
-        throw new Exception("String parameter is empty");
-    }
-    if (!isValidString($string)) {
-        throw new Exception("Your string '$string' contains invalid parentheses");
-    }
+use Controllers\StringValidatorController;
 
-
-    http_response_code(200);
-    echo "OK: Your String '$string' is valid";
-} catch (Exception $e) {
-    http_response_code(400);
-    echo "Bad Request: " . $e->getMessage();
-}
-
-function isValidString($str): bool
-{
-    $count = 0;
-
-    for ($i = 0; $i < strlen($str); $i++) {
-        if ($str[$i] == '(') {
-            $count++;
-        } elseif ($str[$i] == ')') {
-            $count--;
-        }
-        if ($count < 0) {
-            return false;
-        }
-    }
-    return $count == 0;
+if (isset($_POST['string'])) {
+    echo StringValidatorController::validate($_POST['string']);
 }

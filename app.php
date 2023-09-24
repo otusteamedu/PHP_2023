52fc7e9e6d766f92613fe0cbc3acfe7776548f82
DPropastin <dima.propastin@gmail.com>
2023-09-24 17:50:52 +0300

    fix errors

--- app.php
+++ app.php
@@ -1,45 +1,15 @@
 <?php
 
-final class ListNode {
-    public int $val = 0;
-    public ?ListNode $next = null;
-    public function __construct(int $val = 0, ?ListNode $next = null)
-    {
-        $this->val = $val;
-        $this->next = $next;
-    }
-}
+declare(strict_types=1);
 
-function mergeTwoLists(ListNode $list1, ListNode $list2): ListNode
-{
-    $result = new ListNode();
-    $temp = $result;
+use App\ListNode;
+use App\Solution;
 
-    while ($list1 && $list2) {
-        if ($list1->val < $list2->val) {
-            $temp->next = new ListNode($list1->val);
-            $list1 = $list1->next;
-        } elseif ($list1->val > $list2->val) {
-            $temp->next = new ListNode($list2->val);
-            $list2 = $list2->next;
-        } else {
-            $temp->next = new ListNode($list1->val, new ListNode($list2->val));
-            $list1 = $list1->next;
-            $list2 = $list2->next;
-            $temp = $temp->next;
-        }
-
-        $temp = $temp->next;
-    }
-
-    $temp->next = $list1 ?: $list2;
-
-    return $result->next;
-}
+require __DIR__ . '/vendor/autoload.php';
 
 $first = new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3))));
 $second = new ListNode(0, new ListNode(1, new ListNode(3, new ListNode(5))));
 
-$lists = mergeTwoLists($first, $second);
+$lists = Solution::mergeTwoLists($first, $second);
 
 print_r($lists);

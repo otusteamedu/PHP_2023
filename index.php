51c0483741208bd69baab0ad731de9406fc001b3
cherepanov.a <art.cherepan@gmail.com>
2023-09-29 17:11:32 +0700

    hw8 - refactoring

--- index.php
+++ index.php
@@ -1,48 +1,38 @@
 <?php
 
-class ListNode {
-    public $val = 0;
-    public $next = null;
-    function __construct($val = 0, $next = null) {
-        $this->val = $val;
-        $this->next = $next;
-    }
-}
+include __DIR__ . '/vendor/autoload.php';
 
-class Solution {
+/**
+ * @param ListNode $list1
+ * @param ListNode $list2
+ * @return ListNode
+ */
+function mergeTwoLists($list1, $list2) {
+    $head = new ListNode();
+    $headStart = $head;
 
-    /**
-     * @param ListNode $list1
-     * @param ListNode $list2
-     * @return ListNode
-     */
-    function mergeTwoLists($list1, $list2) {
-        $head = new ListNode();
-        $headStart = $head;
-
-        while (true) {
-            if ($list1->val < $list2->val) {
-                $head->val = $list1->val;
-                $list1 = $list1->next;
-            } else {
-                $head->val = $list2->val;
-                $list2 = $list2->next;
-            }
-
-            if ($list1 === null) {
-                $head->next = $list2;
-                break;
-            }
-
-            if ($list2 === null) {
-                $head->next = $list1;
-                break;
-            }
-
-            $head->next = new ListNode();
-            $head = $head->next;
+    while (true) {
+        if ($list1->val < $list2->val) {
+            $head->val = $list1->val;
+            $list1 = $list1->next;
+        } else {
+            $head->val = $list2->val;
+            $list2 = $list2->next;
         }
 
-        return $headStart;
+        if ($list1 === null) {
+            $head->next = $list2;
+            break;
+        }
+
+        if ($list2 === null) {
+            $head->next = $list1;
+            break;
+        }
+
+        $head->next = new ListNode();
+        $head = $head->next;
     }
+
+    return $headStart;
 }
---
+++ src/Classes/ListNode.php
@@ -0,0 +1,12 @@
+<?php
+
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null) {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}

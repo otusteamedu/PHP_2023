a5f735ff74d359bc5625dde0b32621198c0f756b
jasur <jasfayz@gmail.com>
2023-10-03 11:19:11 +0500

    init sql

---
+++ sql/marketing_data_view.sql
@@ -0,0 +1,14 @@
+CREATE VIEW marketing_data_view AS
+SELECT
+    m.title AS movie,
+    t.attribute_type,
+    a.attribute,
+    COALESCE(v.text_value, v.boolean_value::TEXT, v.date_value::TEXT, v.float_value::TEXT, v.int_value::TEXT) AS value
+FROM
+    movies m
+JOIN
+    values v ON m.id = v.movie_id
+JOIN
+    attributes a ON v.attribute_id = a.id
+JOIN
+    attribute_types t ON a.attribute_type_id = t.id;

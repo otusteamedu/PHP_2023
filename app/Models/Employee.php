410161f41888a55de7ee7eaf0efa62e19cf96f9d
mehrubon <husaynov888@gmail.com>
2023-10-10 15:05:51 +0500

    fixes

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -29,5 +29,4 @@ class Employee
 
         return $this;
     }
-
 }
--- index.php
+++ index.php
@@ -2,13 +2,12 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
 use App\DataMapper\EmployeeDataMap;
 
-$getAll = static function () {
-    $dbh = new PDO('mysql:host=localhost;dbname=test', 'root', '');
-    return (new EmployeeDataMap($dbh))->findAll();
-};
 
-return $getAll();
+$dbh = new PDO('mysql:host=localhost;dbname=test', 'root', '');
+
+return (new EmployeeDataMap($dbh))->findAll();
+

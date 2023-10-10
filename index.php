37a83b07865ca533a0451b878efce32391f1f6df
mehrubon <husaynov888@gmail.com>
2023-10-10 15:06:57 +0500

    fixes

--- index.php
+++ index.php
@@ -6,8 +6,6 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use App\DataMapper\EmployeeDataMap;
 
-
 $dbh = new PDO('mysql:host=localhost;dbname=test', 'root', '');
 
 return (new EmployeeDataMap($dbh))->findAll();
-

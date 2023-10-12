7af6838209a25e0722f0d1487d208e839fe12209
Цветков Алексей Александрович <acvetkov@pcr.int>
2023-10-12 15:58:27 +0300

    fix

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/Services/EmailValidator.php';
 
-use \app\validator\EmailValidator;
+use app\validator\EmailValidator;
 
 if (isset($_REQUEST['email'])) {
     $emailValidator = new EmailValidator($_REQUEST['email']);

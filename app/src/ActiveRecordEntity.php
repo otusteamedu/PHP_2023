20cba89489a55fd25c68ac376c7364726504f69e
Evgeny Romanov <e.romanov93@yandex.ru>
2023-10-13 02:15:35 +0500

    hw13

---
+++ app/src/ActiveRecordEntity.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App;
+
+abstract class ActiveRecordEntity
+{
+    /** @var int */
+    protected $id;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function __set(string $name, $value): void
+    {
+        $camelCaseName = $this->underscoreToCamelCase($name);
+        $this->$camelCaseName = $value;
+    }
+
+    private function underscoreToCamelCase(string $source): string
+    {
+        return lcfirst(str_replace('_', '', ucwords($source, '_')));
+    }
+
+    public static function findAll(): array
+    {
+        $db = Db::getInstance();
+
+        return $db->query('SELECT * FROM `' . static::getTableName() . '`;', [], static::class);
+    }
+
+    public static function getById(int $id): ?self
+    {
+        $db = Db::getInstance();
+        $entities = $db->query(
+            'SELECT * FROM `' . static::getTableName() . '` WHERE id=:id;',
+            [':id' => $id],
+            static::class
+        );
+
+        return $entities ? $entities[0] : null;
+    }
+
+    public function save(): void
+    {
+        $mappedProperties = $this->mapPropertiesToDbFormat();
+
+        if ($this->id !== null) {
+            $this->update($mappedProperties);
+        } else {
+            $this->insert($mappedProperties);
+        }
+    }
+
+    public function delete(): void
+    {
+        $db = Db::getInstance();
+        $db->query(
+            'DELETE FROM `' . static::getTableName() . '` WHERE id = :id',
+            [':id' => $this->id]
+        );
+        $this->id = null;
+    }
+
+    public static function findOneByColumn(string $columnName, $value): ?self
+    {
+        $db = Db::getInstance();
+        $result = $db->query(
+            'SELECT * FROM `' . static::getTableName() . '` WHERE `' . $columnName . '` = :value LIMIT 1;',
+            [':value' => $value],
+            static::class
+        );
+
+        if ($result === []) {
+            return null;
+        }
+
+        return $result[0];
+    }
+
+    abstract protected static function getTableName(): string;
+
+    private function update(array $mappedProperties): void
+    {
+        $columns2params = [];
+        $params2values = [];
+        $index = 1;
+
+        foreach ($mappedProperties as $column => $value) {
+            $param = ':param' . $index;
+            $columns2params[] = $column . ' = ' . $param;
+            $params2values[$param] = $value;
+            $index++;
+        }
+
+        $sql = 'UPDATE ' . static::getTableName() . ' SET ' . implode(', ', $columns2params) . ' WHERE id = ' . $this->id;
+        $db = Db::getInstance();
+        $db->query($sql, $params2values, static::class);
+    }
+
+    private function insert(array $mappedProperties): void
+    {
+        $filteredProperties = array_filter($mappedProperties);
+
+        $columns = [];
+        $paramsNames = [];
+        $params2values = [];
+
+        foreach ($filteredProperties as $columnName => $value) {
+            $columns[] = '`' . $columnName. '`';
+            $paramName = ':' . $columnName;
+            $paramsNames[] = $paramName;
+            $params2values[$paramName] = $value;
+        }
+
+        $columnsViaSemicolon = implode(', ', $columns);
+        $paramsNamesViaSemicolon = implode(', ', $paramsNames);
+
+        $sql = 'INSERT INTO ' . static::getTableName() . ' (' . $columnsViaSemicolon . ') VALUES (' . $paramsNamesViaSemicolon . ');';
+        $db = Db::getInstance();
+        $db->query($sql, $params2values, static::class);
+        $this->id = $db->getLastInsertId();
+        $this->refresh();
+    }
+
+    private function refresh(): void
+    {
+        $objectFromDb = static::getById($this->id);
+        $reflector = new \ReflectionObject($objectFromDb);
+        $properties = $reflector->getProperties();
+
+        foreach ($properties as $property) {
+            $property->setAccessible(true);
+            $propertyName = $property->getName();
+            $this->$propertyName = $property->getValue($objectFromDb);
+        }
+    }
+
+    private function mapPropertiesToDbFormat(): array
+    {
+        $reflector = new \ReflectionObject($this);
+        $properties = $reflector->getProperties();
+        $mappedProperties = [];
+
+        foreach ($properties as $property) {
+            $propertyName = $property->getName();
+            $propertyNameAsUnderscore = $this->camelCaseToUnderscore($propertyName);
+            $mappedProperties[$propertyNameAsUnderscore] = $this->$propertyName;
+        }
+
+        return $mappedProperties;
+    }
+
+    private function camelCaseToUnderscore(string $source): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $source));
+    }
+}
---
+++ app/src/Client.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App;
+
+class Client extends ActiveRecordEntity
+{
+    /** @var string */
+    protected $email;
+
+    /** @var string */
+    protected $firstName;
+
+    /** @var string */
+    protected $middleName;
+
+    /** @var string */
+    protected $lastName;
+
+    /** @var string */
+    protected $createdAt;
+
+    /** @var string */
+    protected $updatedAt;
+
+    protected static function getTableName(): string
+    {
+        return 'clients';
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMiddleName(): string
+    {
+        return $this->middleName;
+    }
+
+    /**
+     * @param string $middleName
+     */
+    public function setMiddleName(string $middleName): void
+    {
+        $this->middleName = $middleName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt(): string
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param string $createdAt
+     */
+    public function setCreatedAt(string $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUpdatedAt(): string
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param string $updatedAt
+     */
+    public function setUpdatedAt(string $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+}

b69d491a25f190d5e9d54cbe2b9635852c59074b
Evgeny Romanov <e.romanov93@yandex.ru>
2023-10-13 02:23:13 +0500

    корректировки

--- app/src/Client.php
+++ app/src/Client.php
@@ -27,97 +27,61 @@ class Client extends ActiveRecordEntity
         return 'clients';
     }
 
-    /**
-     * @return string
-     */
     public function getEmail(): string
     {
         return $this->email;
     }
 
-    /**
-     * @param string $email
-     */
     public function setEmail(string $email): void
     {
         $this->email = $email;
     }
 
-    /**
-     * @return string
-     */
     public function getFirstName(): string
     {
         return $this->firstName;
     }
 
-    /**
-     * @param string $firstName
-     */
     public function setFirstName(string $firstName): void
     {
         $this->firstName = $firstName;
     }
 
-    /**
-     * @return string
-     */
     public function getMiddleName(): string
     {
         return $this->middleName;
     }
 
-    /**
-     * @param string $middleName
-     */
     public function setMiddleName(string $middleName): void
     {
         $this->middleName = $middleName;
     }
 
-    /**
-     * @return string
-     */
     public function getLastName(): string
     {
         return $this->lastName;
     }
 
-    /**
-     * @param string $lastName
-     */
     public function setLastName(string $lastName): void
     {
         $this->lastName = $lastName;
     }
 
-    /**
-     * @return string
-     */
     public function getCreatedAt(): string
     {
         return $this->createdAt;
     }
 
-    /**
-     * @param string $createdAt
-     */
     public function setCreatedAt(string $createdAt): void
     {
         $this->createdAt = $createdAt;
     }
 
-    /**
-     * @return string
-     */
     public function getUpdatedAt(): string
     {
         return $this->updatedAt;
     }
 
-    /**
-     * @param string $updatedAt
-     */
     public function setUpdatedAt(string $updatedAt): void
     {
         $this->updatedAt = $updatedAt;

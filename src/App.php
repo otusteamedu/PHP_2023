0e1f20fee82082ce480d1d0411d910ca54aa80ef
adolgov99 <adolgov@marfa-tech.com>
2023-10-15 05:36:56 +0700

    Base init. Nginx + php

---
+++ src/App.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Exception;
+
+class App
+{
+    /**
+     * @throws Exception
+     */
+    public function run(): void
+    {
+        // run your app
+        echo 'Hello world!' . PHP_EOL;
+    }
+}

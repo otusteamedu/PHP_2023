d50aa746ac3e9f444a653ee059474632b628cb4b
DPropastin <dima.propastin@gmail.com>
2023-10-28 14:38:39 +0300

    fix

--- app.php
+++ app.php
@@ -10,8 +10,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/.env');
 
-$db = require __DIR__ . '/src/Config/db.php';
-$pdo = new PDO($db['dsn'], $db['username'], $db['password'], $db['options']);
+$pdo = new PDO(
+    "pgsql:host={$_ENV['POSTGRES_HOST']};port={$_ENV['POSTGRES_PORT']};dbname={$_ENV['POSTGRES_DB']}",
+    $_ENV['POSTGRES_USER'],
+    $_ENV['POSTGRES_PASSWORD'],
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]
+);
 $mapper = new MovieMapper($pdo);
 
 try {

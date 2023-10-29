eee73fce1187ee869e2221ce25d09db727ea8d9e
Nikita Menshutin <info@nikita.global>
2023-10-29 11:20:05 +0200

    feature: html in external file

--- app/src/View/Web.php
+++ app/src/View/Web.php
@@ -6,56 +6,27 @@ class Web
 {
     public static function showForm()
     {
-        ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Запрос на банковскую выписку</title>
-</head>
-<body>
-    <h1>Запрос на банковскую выписку</h1>
-    <form method="POST" action="index.php">
-        <label for="start_date">Дата начала:</label>
-        <input type="date" id="start_date" name="start_date" required>
-        <br>
-        <label for="end_date">Дата окончания:</label>
-        <input type="date" id="end_date" name="end_date" required>
-        <br>
-        <input type="submit" value="Отправить запрос">
-    </form>
-</body>
-</html>
-        <?php
+        $tpl = file_get_contents(__DIR__ . '/../../assets/form.html');
+        header('Content-Type: text/html; charset=utf-8');
+        header('HTTP/1.1 200 OK');
+        echo $tpl;
+        exit();
     }
     public static function showError($message)
     {
-        ?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Ошибка</title>
-</head>
-<body>
-    <h1>Ошибка</h1>
-    <p><?php echo $message ?></p>
-</body>
-</html><?php
+        $tpl = file_get_contents(__DIR__ . '/../../assets/error.html');
+        header('Content-Type: text/html; charset=utf-8');
+        header('HTTP/1.1 400 Bad Request');
+        echo str_replace('{{message}}', $message, $tpl);
+        exit();
     }
 
     public static function showSuccess($message)
     {
-        ?><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Успех</title>
-</head>
-<body>
-    <h1>Успех</h1>
-    <p><?php echo $message ?></p>
-</body>
-</html><?php
+        $tpl = file_get_contents(__DIR__ . '/../../assets/success.html');
+        header('Content-Type: text/html; charset=utf-8');
+        header('HTTP/1.1 200 OK');
+        echo str_replace('{{message}}', $message, $tpl);
+        exit();
     }
 }

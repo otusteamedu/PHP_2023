71a9e9d0b6f38b73b6d242f48a1790041aa71d9c
Evgeny Romanov <e.romanov93@yandex.ru>
2023-11-08 00:27:32 +0500

    эмуляция внешнего сервиса

---
+++ pay_service/server.php
@@ -0,0 +1,14 @@
+<?php
+
+$server = new OpenSwoole\HTTP\Server("localhost", 9501);
+
+$server->on("start", function (OpenSwoole\Http\Server $server) {
+    echo "OpenSwoole http server is started at http://127.0.0.1:9501\n";
+});
+
+$server->on("request", function (OpenSwoole\Http\Request $request, OpenSwoole\Http\Response $response) {
+    $response->header("Content-Type", "text/plain");
+    $response->end("Hello World\n");
+});
+
+$server->start();

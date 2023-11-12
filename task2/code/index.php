de315deb22d4bd6e3e8a6ce81c5d03c0d72944c4
builov <builov@inbox.ru>
2023-11-12 21:30:42 +0300

    task2

--- task2/code/index.php
+++ task2/code/index.php
@@ -1,20 +1,6 @@
 <?php
-
-phpinfo();
-
-echo "Привет, Otus!<br>".date("Y-m-d H:i:s")."<br><br>";
+session_start();
 
 echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME']."<br><br>";
 
-$memcached = new Memcached();
-$memcached->addServer("memcached", 11211);
-
-//var_dump($memcached); exit;
-
-$response = $memcached->get("Bilbo");
-if ($response) {
-    echo $response."<br><br>";
-} else {
-    echo "Adding Keys (K) for Values (V), You can then grab Value (V) for your Key (K) \m/ (-_-) \m/ "."<br><br>";
-    $memcached->set("Bilbo", "Here s Your (Ring) Master stored in MemCached (^_^)") or die(" Keys Couldn't be Created : Bilbo Not Found :'( ")."<br><br>";
-}
+echo "Session ID: " . session_id();

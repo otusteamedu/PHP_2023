e3cd5afc48e31c5bc5899be588cbdac110d7c19b
Shabanov Vyacheslav <saveliy@mail.ru>
2023-11-13 10:23:05 +0300

    ДЗ №2: bash калькулятор и bash сортировщик

---
+++ hw2/script1.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ $1 =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+  echo 'Параметр 1 соотвествует числу'
+else
+  echo 'Параметр 1 не является числом'
+  exit
+fi
+
+if [[ $2 =~ ^-?[0-9]+(\.[0-9]+)?$ ]]; then
+  echo 'Параметр 2 соотвествует числу'
+else
+  echo 'Параметр 2 не является числом'
+  exit
+fi
+
+# Проверка установки пакета bc
+if ! command -v bc &> /dev/null; then  # Проверка, что команда `bc` не найдена
+    echo "Пакет bc не установлен. Устанавливаю..."
+
+    # Проверка операционной системы и использование соответствующего менеджера пакетов
+    if [[ -n $(command -v apt-get) ]]; then  # Проверка, что apt-get доступен (Ubuntu / Debian)
+        sudo apt-get update
+        sudo apt-get install -y bc
+    elif [[ -n $(command -v yum) ]]; then  # Проверка, что yum доступен (CentOS / RHEL)
+        sudo yum install -y bc
+    elif [[ -n $(command -v dnf) ]]; then  # Проверка, что dnf доступен (Fedora)
+        sudo dnf install -y bc
+    else
+        echo "Не удалось выполнить установку. Пожалуйста, установите пакет bc вручную."
+        exit 1
+    fi
+
+    echo "Пакет bc успешно установлен."
+else
+    echo "Пакет bc уже установлен."
+fi
+
+echo $1 + $2 = $(echo "$1 + $2" | bc)

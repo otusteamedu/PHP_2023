2c13da329a222df98055ba27c5dad92a144ba02b
Artem Cherepanov <art.cherepan@gmail.com>
2023-11-20 23:50:25 +0900

    add ClientBuilder

--- index.php
+++ index.php
@@ -1,35 +1,23 @@
 <?php
+
+use Elastic\Elasticsearch\ClientBuilder;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$curl = curl_init();
+$client = ClientBuilder::create()
+    ->setHosts(['http://localhost:9200'])
+    ->build();
 
-$url = "http://localhost:9200/otus-shop/_search";
-
-$headers = array(
-    "Accept: application/json",
-    "Content-Type: application/json",
-);
-
-$data = <<<DATA
-{
-  "from": 0,
-  "size": 1,
-  "sort": [
-        {"price": "desc"}
-    ],
-  "query": {
-    "match_all": {}
-  }
-}
-DATA;
-
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$resp = curl_exec($curl);
-curl_close($curl);
-
-echo $resp;
+$params = [
+    'index' => 'otus-shop',
+    'body' => [
+        'query' => [
+            'match' => [
+                'sku' => '500-003',
+            ]
+        ],
+        'size' => 1,
+    ]
+];
+$response = $client->search($params)->asArray();
+var_dump($response);

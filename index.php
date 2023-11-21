9b34eb5f4439562ca684a64f2cf50bd693e364ba
cherepanov.a <art.cherepan@gmail.com>
2023-11-21 19:57:33 +0700

    example query

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ use Elastic\Elasticsearch\ClientBuilder;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$category = (string)readline('Type book category: ');
+$price = (float)readline('Type book price: ');
+$title = (string)readline('Type book title: ');
+
 $client = ClientBuilder::create()
     ->setHosts(['http://localhost:9200'])
     ->build();
@@ -12,12 +16,26 @@ $params = [
     'index' => 'otus-shop',
     'body' => [
         'query' => [
-            'match' => [
-                'sku' => '500-003',
-            ]
+            'bool' => [
+                'filter' => [
+//                    'range' => [
+//                        'price' => [
+//                            'gte' => $price,
+//                        ],
+//                    ],
+//                    'term' => [
+//                        'category' => $category,
+//                    ],
+                ],
+                'should' => [
+                    'match' => [
+                        'title' => $title,
+                    ],
+                ],
+            ],
         ],
         'size' => 1,
     ]
 ];
-$response = $client->search($params)->asArray();
+$response = $client->search($params)->asObject();
 var_dump($response);

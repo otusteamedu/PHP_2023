1b77837fb65e78c1ec281efa722fcccb4259eab3
igorKAznov <malverdo@yandex.ru>
2023-11-22 10:14:41 +0300

    lessons 4 php fmp

--- course/public/index.php
+++ course/public/index.php
@@ -1,1 +1,11 @@
 <?php
+$serverName = $_SERVER['HOSTNAME'];
+echo "This request was processed by PHP in container: $serverName\n";
+$serverName = $_SERVER['SERVER_ADDR'];
+echo "This request was processed by PHP in container: $serverName\n";
+
+$containerId = $_SERVER['SERVER_NAME'];
+echo "This request was processed by PHP in container: $containerId\n";
+
+
+echo "This request was processed by PHP in container: " . gethostname();

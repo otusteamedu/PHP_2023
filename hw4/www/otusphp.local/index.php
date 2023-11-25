c7ac24f76dd5ad02f6f50b24f491d5f67b1c25ef
Shabanov Vyacheslav <saveliy@mail.ru>
2023-11-25 12:11:50 +0300

    ДЗ №4: Задачка с круглыми скобками и балансировщик php-fpm

---
+++ hw4/www/otusphp.local/index.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+$str = isset($_POST['string']) ? trim($_POST['string']) : '';
+$error = '';
+if (!empty($str)) {
+    if ($str[0] == ')') {
+        $error = 'Строка не может начинаться с закрытой скобки';
+    } elseif ($str[mb_strlen($str)-1] == '(') {
+        $error = 'Строка не может заканчиваться открытой скобкой';
+    }
+    $stack = $countOpen = $countClose = 0;
+    for($i=0; $i<mb_strlen($str); $i++) {
+        if ($str[$i] == '(') {
+            $countOpen++;
+            $stack++;
+        } elseif ($str[$i] == ')') {
+            $countClose++;
+            $stack--;
+        } else {
+            $error = 'Строка не может содержать отличных от скобок символов';
+            break;
+        }
+    }
+} else {
+    $error = 'Пустая строка';
+}
+
+if (empty($error)
+    && ($countOpen != $countClose
+        || $stack>0)
+) {
+    $error = 'Количество открытых скобок не равно количеству закрытых скобок';
+}
+
+$arReturn = [];
+if (!empty($error)) {
+    http_response_code(400);
+    header('Content-type: application/json');
+    $arReturn['error'] = $error;
+} else {
+    http_response_code(200);
+    header('Content-type: application/json');
+    $arReturn['success'] = 'Строка содержит корректное количество открытых и закрытых скобок';
+}
+echo json_encode($arReturn);

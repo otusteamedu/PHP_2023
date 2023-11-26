0f4108cd75b217689c1b6aaf82d40a67330852d9
Vasilaki <vasilaki.t@gmail.com>
2023-11-26 18:59:11 +0500

    HW5

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+const EMAIL_FILED_NAME = 'email';
+
+$errors = [];
+$email = $_POST[EMAIL_FILED_NAME] ?? null;
+$isValidEmail = false;
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    list($emailName, $hostName) = explode('@', $email);
+    $mailList = [];
+    getmxrr($hostName, $mailList);
+    if (0 < count($mailList)) {
+        $isValidEmail = true;
+    } else {
+
+        $errors[] = sprintf('Host %s is not available', $hostName);
+    }
+} else {
+    $errors[] = sprintf('%s is not email address', $email);
+}
+
+if (0 == $isValidEmail) {
+    http_response_code(200);
+} else {
+    http_response_code(400);
+}

d35d377d93f65fb5d1f6e9ec0743f764a4dc60b4
test name <test@mail.loc>
2023-11-26 10:54:04 +0300

    feat: move logic to class

---
+++ app/Service/isValidBrackets.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+class isValidBrackets
+{
+    /**
+     * @var string
+     */
+    private string $string;
+
+    /**
+     * @param $string
+     */
+    public function __construct($string)
+    {
+        if (!$string) {
+            throw new \Exception('String must not be empty');
+        }
+
+        $this->string = preg_replace('/[^\(\)]/', '', $string);
+    }
+
+    /**
+     * @return void
+     */
+    public function check(): void
+    {
+        $i = 0;
+
+        $opened = 0;
+
+        while ($i < strlen($this->string)) {
+            if ($this->string[$i] == '(') {
+                $opened++;
+            }
+
+            if ($this->string[$i] == ')') {
+                $opened--;
+            }
+
+            if ($opened < 0) {
+                throw new \Exception('Invalid string');
+            }
+
+            $i++;
+        }
+
+        if ($opened != 0) {
+            throw new \Exception('Invalid string');
+        }
+    }
+}
--- app/index.php
+++ app/index.php
@@ -2,54 +2,15 @@
 
 declare(strict_types=1);
 
-$mc = new Memcached();
-
-$mc->addServer("memcached", 11211);
-
-echo "Connection to Memcached successfully" . PHP_EOL;
-
-$mc->set("key", "Memcached");
-
-$mc->set("status", "ok");
-
-echo $mc->get("key") . " is " . $mc->get("status") . PHP_EOL;
+require('./Service/isValidBrackets.php');
 
 $string = $_POST['string'] ?? null;
 
-if (!$string) {
-    http_response_code(400);
-    exit("Parameter must not be empty");
-}
-
-$string = preg_replace('/[^\(\)]/', '', $string);
-
-$i = 0;
-
-$opened = 0;
-
 try {
-    while ($i < strlen($string)) {
-        if ($string[$i] == '(') {
-            $opened++;
-        }
-
-        if ($string[$i] == ')') {
-            $opened--;
-        }
-
-        if ($opened < 0) {
-            throw new Exception("Invalid string");
-        }
-
-        $i++;
-    }
-
-    if ($opened != 0) {
-        throw new Exception("Invalid string");
-    }
+    $isValidBrackets = new isValidBrackets($string);
+    $status = $isValidBrackets->check();
 } catch (Exception $e) {
     http_response_code(400);
     exit($e->getMessage());
 }
-
-echo 'Is ok';
+http_response_code(200);

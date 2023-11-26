315b4d9cda5f990d65d51e9d283144247b5ce2a4
Evgeny Romanov <e.romanov93@yandex.ru>
2023-11-26 16:05:36 +0500

    hw18

---
+++ src/SolutionTask1.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * https://leetcode.com/problems/intersection-of-two-linked-lists/
+ */
+
+namespace App;
+
+class SolutionTask1
+{
+    /**
+     * @param ListNode $headA
+     * @param ListNode $headB
+     * @return ListNode|null
+     */
+    public function getIntersectionNode(ListNode $headA, ListNode $headB): ?ListNode
+    {
+        $b = $headB;
+
+        do {
+            do {
+                if ($headA === $headB) {
+                    return $headA;
+                } else {
+                    $headB = $headB->next;
+                }
+            } while ($headB !== null);
+
+            $headA = $headA->next;
+            $headB = $b;
+        } while ($headA !== null);
+
+        return null;
+    }
+}

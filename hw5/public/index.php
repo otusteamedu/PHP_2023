6195ba01f4b1e21e56a2ebd1459d5ebc87a8ca4d
Елена Спиктаренко <elena.spiktarenko@gmail.com>
2023-11-27 16:04:45 +0300

    +render

--- hw5/public/index.php
+++ hw5/public/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 require_once __DIR__ .'/../vendor/autoload.php';
 require_once __DIR__ .'/../src/app.php';
 
-use Elena\Hw5\App;
+use Elena\hw5\App;
 
 $app = new App();
 echo $app->run();
--- hw5/src/app.php
+++ hw5/src/app.php
@@ -3,16 +3,31 @@ declare(strict_types=1);
 
 namespace Elena\Hw5;
 
+use Exception;
+
 class App
 {
    function run()
     {
-        require __DIR__ .'/../src/view/form.php' ;
-        if($_REQUEST['email_str'])
-        {
-            $validation= new EmailValidation();
+        $viewFile = __DIR__ .'/../src/view/form.php' ;
+        $renders = new MyRender();
+
+        try {
+            if (!$view = $renders->render_view($viewFile)){
+                throw new Exception('Файл не найден');
+            }
+        }
+        catch ( Exception $e) {
+            return  "</br> Ошибка открытия страницы - ". $e->getMessage() ;
+        }
+
+        echo($view);
+
+        if (isset($_REQUEST['email_str'])) {
+            $validation = new EmailValidation();
             return $validation->check_email();
         }
+
     }
 
 }

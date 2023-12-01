807095da5e40daa6b1c863f2f174c0217b601841
Dmitriy Matrenin <levitanoff@gmail.com>
2023-12-02 00:49:02 +0400

    hw1

--- code/app/index.php
+++ code/app/index.php
@@ -1,29 +1,37 @@
 <?php
+
 // Test Redis
 $redis = new Redis();
 $redis->connect('redis');
 echo "REDIS: " . $redis->info('server')['redis_version'] . "</br></br>";
 
 // Test Memcache
-$memcache = new Memcache;
-$memcache->connect('memcached',11211);
+$memcache = new Memcache();
+$memcache->connect('memcached', 11211);
 echo "Memcache: " . $memcache->getVersion() . "</br></br>";
 
 // Test MySQL (Docker)
 $mysqli_connection = new MySQLi('db', 'docker', 'secret', 'test');
-if ($mysqli_connection->connect_error) {
-	echo "MySQL Not connected, error: " . $mysqli_connection->connect_error;
+
+if ($mysqli_connection->connect_error)
+{
+    echo "MySQL Not connected, error: " . $mysqli_connection->connect_error;
 }
-else {
-	echo "MySQL Connected to Docker container.<br><br>";
+else
+{
+    echo "MySQL Connected to Docker container.<br><br>";
 }
 
 // Test MySQL (Homestead)
 $mysqli_homestead_connection = new MySQLi('192.168.56.56', 'homestead', 'secret', 'homestead', 3306);
-if ($mysqli_homestead_connection->connect_error) {
+
+if ($mysqli_homestead_connection->connect_error)
+{
 	echo "MySQL Not connected to Homestead, error: " . $mysqli_homestead_connection->connect_error;
 }
-else {
+else
+{
 	echo "MySQL Connected to Homestead";
 }
+
 phpinfo();

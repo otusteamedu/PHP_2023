8e2bca5639fd334c36b2a306d7f47b5f33966468
Dmitriy Matrenin <levitanoff@gmail.com>
2023-12-02 01:14:38 +0400

    hw1

--- code/app/index.php
+++ code/app/index.php
@@ -13,21 +13,13 @@ echo "Memcache: " . $memcache->getVersion() . "</br></br>";
 // Test MySQL (Docker)
 $mysqli_connection = new MySQLi('db', 'docker', 'secret', 'test');
 
-if ($mysqli_connection->connect_error) {
-    echo "MySQL Not connected, error: " . $mysqli_connection->connect_error;
-}
-else {
-    echo "MySQL Connected to Docker container.<br><br>";
-}
+if ($mysqli_connection->connect_error) echo "MySQL Not connected, error: " . $mysqli_connection->connect_error;
+else echo "MySQL Connected to Docker container.<br><br>";
 
 // Test MySQL (Homestead)
 $mysqli_homestead_connection = new MySQLi('192.168.56.56', 'homestead', 'secret', 'homestead', 3306);
 
-if ($mysqli_homestead_connection->connect_error) {
-    echo "MySQL Connected to Homestead";
-} 
-else {
-    echo "MySQL Not connected to Homestead, error: " . $mysqli_homestead_connection->connect_error;
-}
+if ($mysqli_homestead_connection->connect_error) echo "MySQL Connected to Homestead";
+else echo "MySQL Not connected to Homestead, error: " . $mysqli_homestead_connection->connect_error;
 
 phpinfo();

143e9a245984208354d1b63a6e37772f62e90fb2
Антон Ардышев <toni.ardyshev@mail.ru>
2023-12-02 22:19:32 +0300

    add hw5

--- code/index.php
+++ code/index.php
@@ -4,19 +4,6 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use App\ValidatorString;
-use App\ValidatorRequest;
+$app = new Otus\App();
+$app->run();
 
-http_response_code(400);
-
-try {
-    ValidatorRequest::check();
-    ValidatorString::check($_POST['text']);
-} catch (Exception $e) {
-    echo $e->getMessage();
-    exit();
-}
-
-http_response_code(200);
-header('Content-Type: text/plain; charset=utf-8');
-echo 'This text has been verified!';
---
+++ code/src/App.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace Otus;
+
+use Otus\Framework\Core\Email;
+use Otus\Framework\Http\Request;
+
+class App
+{
+    public function run(): void
+    {
+        header('Content-Type: text/plain; charset=utf-8');
+        try {
+            $request = new Request();
+            Email::validate($request);
+            http_response_code(200);
+            echo 'Emails have been successfully verified';
+        } catch (\Exception $e) {
+            http_response_code(400);
+            echo $e->getMessage();
+        }
+    }
+}

7f188f512bfd433c522194a5dafe2bc4f93d0627
Iva-So <sochkov1994@mail.ru>
2023-12-03 13:11:15 +0400

    fix: code-style

--- src/After/Application/Service/SalaryManager/ContractType/Contract.php
+++ src/After/Application/Service/SalaryManager/ContractType/Contract.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace App\After\Application\Service\SalaryManager\ContractType;
 
-enum Contract: string {
+enum Contract: string
+{
     case GPH = 'ГПХ';
     case TD = 'ТД';
     case EP = 'ИП';

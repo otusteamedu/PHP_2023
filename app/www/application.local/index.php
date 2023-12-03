42842362f3660fcc8346bf4a27f2cb76b5f4d959
Artyrcha <sevrevdan@yandex.ru>
2023-12-03 23:35:28 +0500

    feat: добавил balancer, добавил валидацию скобок в строке из post запроса

--- app/www/application.local/index.php
+++ app/www/application.local/index.php
@@ -1,42 +1,11 @@
 <?php
 
-require "vendor/autoload.php";
+declare(strict_types=1);
 
-try {
-    $redis = new Predis\Client([
-        'scheme' => 'tcp',
-        'host' => 'redis',
-        'port' => '6379',
-    ]);
-    $redis->set('app:redis:key', 'redis:value');
-    echo "Redis: {$redis->get('app:redis:key')}";
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+include_once __DIR__ . '/vendor/autoload.php';
 
-echo '<br>';
+use App\App;
 
-try {
-    $memcached = new Memcached();
-    $memcached->addServer('memcached', 11211);
-    $memcached->set('memcached_key', 'memcached_value');
-    echo "Memcached: {$memcached->get('memcached_key')}";
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
-
-echo '<br>';
-
-try {
-    $postgres = pg_connect("host=postgres port=5432 dbname=test user=test password=test");
-
-    if (!$postgres) {
-        throw new Error("Postgres: " . pg_last_error());
-    }
-
-    echo "Postgres: enabled";
-
-    pg_close($postgres);
-} catch (Exception $e) {
-    echo $e->getMessage();
-}
+$app = new App();
+$app->run();
+?>

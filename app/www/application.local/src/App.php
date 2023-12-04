5ddb9c994f6e99f1cd2c8a8e2505a571d498c2de
Artyrcha <sevrevdan@yandex.ru>
2023-12-04 21:34:13 +0500

    feat: added email verification

--- app/www/application.local/src/App.php
+++ app/www/application.local/src/App.php
@@ -9,61 +9,35 @@ class App
     public function run()
     {
         try {
-            if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-                throw new Exception('Invalid request method. Only POST is allowed.');
+            $emails = [
+                'user@example.com',
+                'invalid.email',
+                'another@test.com',
+            ];
+
+            foreach ($emails as $email) {
+                if ($this->checkValidEmail($email)) {
+                    echo "$email - Валидный email\n";
+                } else {
+                    echo "$email - Невалидный email\n";
+                }
             }
-
-            $json_data = file_get_contents('php://input');
-            $data = json_decode($json_data, true);
-
-            if (!isset($data['string'])) {
-                throw new Exception('Invalid request: missing "string" parameter');
-            }
-
-            $string = $data['string'];
-
-            if (empty($string)) {
-                throw new Exception('Invalid string: empty input');
-            }
-
-            if ($this->checkValidString($string)) {
-                http_response_code(200);
-                echo 'Success: valid string';
-            } else {
-                throw new Exception('Invalid string');
-            }
-
 
         } catch (Exception $e) {
-            http_response_code(400);
             echo $e->getMessage();
         }
     }
 
 
-    private function checkValidString(string $input): bool
+    private function checkValidEmail(string $email): bool
     {
-        $stack = [];
-
-        if ($input[0] === ")" || $input[mb_strlen($input) - 1] === "(") {
+        $pattern = '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/';
+        if (!preg_match($pattern, $email)) {
             return false;
         }
 
-        for ($i = 0; $i < strlen($input); $i++) {
-            $char = $input[$i];
-
-            if ($char == '(') {
-                array_push($stack, $char);
-            } elseif ($char == ')') {
-                if (empty($stack)) {
-                    return false;
-                }
-
-                array_pop($stack);
-            }
-        }
-
-        if (!empty($stack)) {
+        list(, $domain) = explode('@', $email);
+        if (!checkdnsrr($domain)) {
             return false;
         }
 

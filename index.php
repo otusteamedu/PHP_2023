bd9b0afaf01c82f43de2a316f6bb7f287160f64b
Возный Роман <unholy.viroos@gmail.com>
2023-12-08 10:24:18 +0300

    feat(hw8): Решение алгоритмических задач

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+use Rvoznyi\ComposerHello\ListNode;
+
+/**
+ * @param ListNode $list1
+ * @param ListNode $list2
+ * @return ListNode
+ */
+function mergeTwoLists(ListNode $list1, ListNode $list2): ListNode
+{
+    $head      = new ListNode();
+    $headStart = $head;
+    
+    while (true) {
+        if ($list1->val < $list2->val) {
+            $head->val = $list1->val;
+            $list1     = $list1->next;
+        } else {
+            $head->val = $list2->val;
+            $list2     = $list2->next;
+        }
+        
+        if ($list1 === null) {
+            $head->next = $list2;
+            break;
+        }
+        
+        if ($list2 === null) {
+            $head->next = $list1;
+            break;
+        }
+        
+        $head->next = new ListNode();
+        $head       = $head->next;
+    }
+    
+    return $headStart;
+}

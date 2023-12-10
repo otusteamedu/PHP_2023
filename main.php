ed0107a5a4c125e2ad88b993236edcf0151cc7a2
Alexandr Chugunov <alexchugunov@mail.ru>
2023-12-10 15:54:12 +0700

    Homework 8

--- main.php
+++ main.php
@@ -2,18 +2,6 @@
 
 declare(strict_types=1);
 
-class ListNode
-{
-    public $val = 0;
-    public $next = null;
-
-    function __construct($val = 0, $next = null)
-    {
-        $this->val = $val;
-        $this->next = $next;
-    }
-}
-
 class Solution
 {
     function mergeTwoLists($list1, $list2)

e77a9951318440e081336858009075b6551c1410
test name <test@mail.loc>
2023-12-10 10:54:41 +0300

    feat: init chat app

---
+++ app/src/Services/Client.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yevgen87\App\Services;
+
+use Exception;
+
+class Client
+{
+    /**
+     * @var string
+     */
+    private string $address;
+
+    /**
+     * @var integer
+     */
+    private int $port;
+
+    public function __construct(string $address, int $port)
+    {
+        $this->address = $address;
+        $this->port = $port;
+    }
+
+    public function __destruct()
+    {
+        if (
+            $this->address
+            && file_exists($this->address)
+        ) {
+            unlink($this->address);
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function run(): void
+    {
+        if (false == ($socket = socket_create(AF_UNIX, SOCK_STREAM, 0))) {
+            throw new Exception("Could not create socket");
+        }
+
+        if (!socket_connect($socket, $this->address)) {
+            throw new Exception("Could not connect socket");
+        }
+
+        do {
+            $stdin = fopen('php://stdin', 'r');
+            $line = fgets($stdin);
+
+            socket_write($socket, $line);
+
+            if ($message = socket_read($socket, 2048)) {
+                echo $message  . PHP_EOL;;
+            }
+        } while (true);
+
+        socket_close($socket);
+    }
+}
---
+++ app/src/config/socket.php
@@ -0,0 +1,9 @@
+<?php
+
+declare (strict_types=1);
+
+return [
+    'host' => '/var/run/app.sock',
+    'port' => 0,
+    'timeout' => 30
+];

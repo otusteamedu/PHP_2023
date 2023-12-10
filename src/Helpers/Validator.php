3c8617af0998878ad5bdea48a80023c7bdd01164
e.kovalev <knuklus@ya.ru>
2023-12-10 14:05:03 +0300

    Исправлены замечания

--- src/Helpers/Validator.php
+++ src/Helpers/Validator.php
@@ -4,29 +4,24 @@ namespace Ekovalev\Otus\Helpers;
 
 class Validator
 {
-    public static function openingClosingChar(string | null $stringChars, string $openChar, string $closingChar):string | bool
+    public static function openingClosingChar(string | null $stringChars, string $openChar, string $closingChar)
     {
-        try {
-            if (!is_string($stringChars) || strlen($stringChars) < 1) {
-                throw new \Exception("Не передали строку с кол-ом символов");
+        if (!is_string($stringChars) || strlen($stringChars) < 1) {
+            throw new \Exception("Не передали строку с кол-ом символов");
+        }
+        $charCnt = 0;
+        for($i = 0; $i < strlen($stringChars); $i++){
+            if($stringChars[$i] === $openChar){
+                $charCnt++;
+            }elseif ($stringChars[$i] === $closingChar){
+                $charCnt--;
             }
-            $charCnt = 0;
-            for($i = 0; $i < strlen($stringChars); $i++){
-                if($stringChars[$i] === $openChar){
-                    $charCnt++;
-                }elseif ($stringChars[$i] === $closingChar){
-                    $charCnt--;
-                }
-                if($charCnt < 0){
-                    throw new \Exception("Нарушен порядок открывающих - закрывающих символов");
-                }
-            }
-            if($charCnt !== 0){
+            if($charCnt < 0){
                 throw new \Exception("Нарушен порядок открывающих - закрывающих символов");
             }
-        } catch (\Exception $e) {
-            return $e->getMessage();
         }
-        return true;
+        if($charCnt !== 0){
+            throw new \Exception("Нарушен порядок открывающих - закрывающих символов");
+        }
     }
 }

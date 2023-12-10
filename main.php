2732a7f82638013dd7bc9b9d4c3fa7467b10c9dc
Alexandr Chugunov <alexchugunov@mail.ru>
2023-12-10 15:59:51 +0700

    Homework 8

--- main.php
+++ main.php
@@ -1,36 +1,29 @@
 <?php
 
-declare(strict_types=1);
-
-use Alexchugun\App;
-
-class Solution
+function mergeTwoLists($list1, $list2)
 {
-    function mergeTwoLists($list1, $list2)
-    {
-        $list = new ListNode();
-        $current = $list;
+    $list = new ListNode();
+    $current = $list;
 
-        while ($list1 !== null && $list2 !== null) {
-            if ($list1->val < $list2->val) {
-                $current->next = $list1;
-                $list1 = $list1->next;
-            } else {
-                $current->next = $list2;
-                $list2 = $list2->next;
-            }
-
-            $current = $current->next;
-        }
-
-        if ($list1 !== null) {
+    while ($list1 !== null && $list2 !== null) {
+        if ($list1->val < $list2->val) {
             $current->next = $list1;
-        }
-
-        if ($list2 !== null) {
+            $list1 = $list1->next;
+        } else {
             $current->next = $list2;
+            $list2 = $list2->next;
         }
 
-        return $list->next;
+        $current = $current->next;
     }
+
+    if ($list1 !== null) {
+        $current->next = $list1;
+    }
+
+    if ($list2 !== null) {
+        $current->next = $list2;
+    }
+
+    return $list->next;
 }

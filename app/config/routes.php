a23c17e6ea1bca2417a1725b8b8d2dc658734216
Evgeny Romanov <e.romanov93@yandex.ru>
2023-12-13 00:14:11 +0500

    реализовано создание заявки со статусом

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,11 +1,9 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use App\Infrastructure\Http\CreateApplicationFormAction;
+use App\Infrastructure\Http\{CreateApplicationFormAction, TestAction};
 use Slim\App;
 
 return function (App $app) {
-    $app->get('/', \App\Infrastructure\Http\TestAction::class)->setName('test');
+    $app->get('/', TestAction::class)->setName('test');
     $app->post('/application_form', CreateApplicationFormAction::class);
 };

4ace9b9c0929bc3094d75f013a21ee5fddf02efe
Возный Роман <unholy.viroos@gmail.com>
2023-12-14 11:05:43 +0300

    fix(hw8): очередные правки

---
+++ src/Solution.php
@@ -0,0 +1,38 @@
+<?php
+
+use Rvoznyi\ComposerHello\ListNode;
+
+class Solution
+{
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2)
+    {
+        $listNode = [];
+        $node     = $list1;
+        while (is_numeric($node->val)) {
+            $listNode[] = $node->val;
+            $node       = $node->next;
+        }
+        $node = $list2;
+        while (is_numeric($node->val)) {
+            $listNode[] = $node->val;
+            $node       = $node->next;
+        }
+        sort($listNode);
+        $len = count($listNode);
+        if ($len > 0) {
+            $node = new ListNode($listNode[$len - 1]);
+            --$len;
+            while (0 < $len) {
+                --$len;
+                $tmpNode = new ListNode($listNode[$len], $node);
+                $node    = $tmpNode;
+            }
+        }
+        return $node;
+    }
+}

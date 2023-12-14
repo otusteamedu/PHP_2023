81d389d802c152ed2006e75070315518c95160ac
Возный Роман <unholy.viroos@gmail.com>
2023-12-14 14:58:48 +0300

    fix(hw8): очередные правки

--- src/Solution.php
+++ src/Solution.php
@@ -9,28 +9,29 @@ class Solution
      * @param ListNode $list2
      * @return ListNode
      */
-    function mergeTwoLists($list1, $list2): ListNode
+    public function mergeTwoLists($list1, $list2): ?ListNode
     {
         $listNode = [];
-        $node     = $list1;
+        $node = $list1;
         while (is_numeric($node->val)) {
             $listNode[] = $node->val;
-            $node       = $node->next;
+            $node = $node->next;
         }
         $node = $list2;
         while (is_numeric($node->val)) {
             $listNode[] = $node->val;
-            $node       = $node->next;
+            $node = $node->next;
         }
         sort($listNode);
         $len = count($listNode);
+        $node = null;
         if ($len > 0) {
             $node = new ListNode($listNode[$len - 1]);
             --$len;
-            while (0 < $len) {
+            while ($len > 0) {
                 --$len;
                 $tmpNode = new ListNode($listNode[$len], $node);
-                $node    = $tmpNode;
+                $node = $tmpNode;
             }
         }
         return $node;

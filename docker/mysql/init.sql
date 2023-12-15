dfe2618599bcb7b6390e78025909d27d2cf6b105
Sergey Antonov <santosb@mail.ru>
2023-12-15 17:19:40 +0300

    Homework 1

---
+++ docker/mysql/init.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS `test` CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+GRANT ALL PRIVILEGES ON *.* TO 'mysql'@'%';
--- public/index.php
+++ public/index.php
@@ -2,3 +2,15 @@
 declare(strict_types=1);
 
 echo 'Запрос принял сервер ' . $_SERVER['HOSTNAME'];
+echo "<br>";
+
+//hostname название docker контейнера
+$connect = mysqli_connect('mysql-db', 'mysql', 'mysql', 'test');
+
+if ($connect->connect_error) {
+    die("Ошибка подключения: " . $connect->connect_error);
+}
+
+echo "Подключение успешно!";
+
+phpinfo();

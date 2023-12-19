324513a4ffd659d06db287984a35d7b95a054cde
moldirkaliyas <kaliyas@kolesa.kz>
2023-12-20 01:58:10 +0500

    Вторая попытка настроить

--- code/index.php
+++ code/index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello Otusddfdlfjdfh!";
-echo phpinfo();
+echo "Hello Otus!!!!";
+
+echo "<br>";
+$redis = new Redis();
+$redis->connect('redis', 6379);
+$redis->set('key', 'value');
+$value = $redis->get('key');
+echo "Redis:" . $value;
+
+echo "<br>";
+$memcached = new Memcached();
+$memcached->addServer('memcached', 11211); // Используем имя службы Docker Compose "memcached"
+$memcached->set('key', 'value', 60);
+$value = $memcached->get('key');
+echo "Memcached:" . $value;

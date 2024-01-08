a42029e4d07f1d351a2adfce36a86ea41b120c81
Антон Ардышев <toni.ardyshev@mail.ru>
2024-01-08 20:37:19 +0300

    up hw7

--- profitable_movie.sql
+++ profitable_movie.sql
@@ -1,11 +1,9 @@
-SELECT m.title, SUM(s.price * sc.increase) as total_price
-	FROM tickets t 
-	JOIN sessions s ON t.session_id = s.id
-	JOIN locations l ON t.location_id = l.id
-	JOIN seats_categories sc ON l.category_id = sc.id
-	JOIN movies m ON s.movie_id = m.id
-	GROUP BY m.title
-	ORDER BY total_price DESC
-	LIMIT 1
+SELECT m.title, sum(total_price) as total
+    FROM tickets t
+    JOIN sessions s ON t.session_id = s.id
+    JOIN movies m ON s.movie_id = m.id
+    GROUP BY m.title
+    ORDER BY total DESC
+    LIMIT 1
 
 

876515f243aae2ca0f344b262958153ea99c0219
Sergey Antonov <santosb@mail.ru>
2024-01-11 20:05:25 +0300

    Homework 4

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+use Santonov\Otus\Application;
+
+require_once '../vendor/autoload.php';
+
+$app = new Application();
+$result = $app->process();
+echo implode("<br>", $result);
---
+++ src/Application.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace Santonov\Otus;
+
+class Application
+{
+    public function process(): array
+    {
+        $result = [
+            'hostname' => 'Запрос обработан ' . $_SERVER['HOSTNAME'],
+        ];
+        $baseString = $_POST['string'] ?? null;
+        if (!is_string($baseString) || empty($baseString))
+        {
+            http_response_code(400);
+            $result['message'] = 'Неверные входные данные';
+        } else {
+            if (Validator::openedClosedBrackets($baseString)) {
+                http_response_code(200);
+                $result['message'] = 'Все хорошо!';
+            } else {
+                http_response_code(400);
+                $result['message'] = 'Неправильно установлены скобки!';
+            }
+        }
+
+        return $result;
+    }
+}

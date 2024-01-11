4bfcf09d6a848a0742a415d0bbe20bda89d2723c
PetrovSpace <vovapetrov94@gmail.com>
2024-01-11 16:18:47 +0300

    balancer

--- code/index.php
+++ code/index.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-function controlBrackets(string $string) : bool
+function controlBrackets(string $string): bool
 {
     // 0. Чистим строку от лишних символов
     $string = preg_replace('/[^(\)]+/', '', $string);
@@ -25,7 +25,7 @@ function controlBrackets(string $string) : bool
 
     // 2. Проверка последовательности на 1-й и N-й символ
     $array = str_split($string, 1);
-    if ($array[0] <> '(' || $array[$lenght-1] <> ')') {
+    if ($array[0] <> '(' || $array[$lenght - 1] <> ')') {
         return false;
     }
 

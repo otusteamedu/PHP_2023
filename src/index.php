be8cc0c826bee14f777369f4e75f7a52a007dfa1
kirill.lobkovsky <kiphirrr@gmail.com>
2024-01-13 22:13:41 +0300

    main: change environment

--- src/index.php
+++ src/index.php
@@ -4,11 +4,3 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-use Klobkovsky\App\DB;
-
-try {
-    $DB = new DB();
-
-} catch (Throwable $e) {
-    echo 'Error: ' . $e->getMessage();
-}

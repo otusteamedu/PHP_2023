499c6e6cf8cdd1c0d5eed09140f7d93a471e1f82
Maxim <x0ttabbl4.2@gmail.com>
2024-01-14 15:45:20 +0300

    refactor(App):

--- public/index.php
+++ public/index.php
@@ -2,21 +2,10 @@
 
 declare(strict_types=1);
 
-use src\Application\UseCase\SearchUseCase;
-use src\Infrastructure\Repositories\BookRepository;
+use App\App;
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/Application/UseCase/SearchUseCase.php';
-require __DIR__ . '/../src/Infrastructure/Repositories/BookRepository.php';
+require __DIR__ . '/../App/App.php';
 
-$useCase = new SearchUseCase(new BookRepository());
-
-$values = getopt('t:c:p:');
-
-$response = $useCase(
-    title: $values['t'],
-    category: $values['c'],
-    price: $values['p']
-);
-
-print_r($response['hits']['hits']);
+$app = new App();
+$app->run();

39ecae1b475fba8ee706eb9cdcaa7d831b2761c0
damir <khalikov@beta.agency>
2024-01-17 21:22:04 +0400

    init

---
+++ www/index.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+class Solution {
+
+    /**
+     * @param ListNode $list1
+     * @param ListNode $list2
+     * @return ListNode
+     */
+    function mergeTwoLists($list1, $list2)
+    {
+        $head = $tail = new ListNode();
+
+        while (isset($list1, $list2)) {
+            if ($list1->val < $list2->val) {
+                $tail->next = $list1;
+                $list1 = $list1->next;
+            } else {
+                $tail->next = $list2;
+                $list2 = $list2->next;
+            }
+
+            $tail = $tail->next;
+        }
+
+        $tail->next = $list1 ?? $list2;
+
+        return $head->next;
+    }
+}
+
+class ListNode {
+    public $val = 0;
+    public $next = null;
+    function __construct($val = 0, $next = null) {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}

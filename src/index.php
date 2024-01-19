b3379316968617685e3739f5d0ad7f9c2141c859
cherepanov.a <art.cherepan@gmail.com>
2024-01-19 15:27:31 +0700

    refactor index.php

--- src/index.php
+++ src/index.php
@@ -9,10 +9,3 @@ $memcached = new Memcached();
 $memcached->addServer('memcached', 11211);
 
 var_dump($memcached->getVersion());
-
-try {
-    $dsn = 'mysql:host=' . $_ENV['MYSQL_HOST'] . ';dbname=' . $_ENV['MYSQL_DATABASE'];
-    $dbh = new \PDO($dsn, $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
-} catch (PDOException $exception) {
-    echo $exception->getMessage();
-}

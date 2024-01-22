fd535ad8cb62f8b4731dbe049cf11516e7f02a01
Vlad Galushka <galushka.vp@gmail.com>
2024-01-22 17:27:05 +0700

    after review

--- data/database_schema.sql
+++ data/database_schema.sql
@@ -1,15 +1,20 @@
+--film
 CREATE TABLE IF NOT EXISTS film (
     id          bigint PRIMARY KEY,
     name        varchar(255) NOT NULL,
     description varchar(255)
 );
 
+
+--attribute_type
 CREATE TABLE IF NOT EXISTS attribute_type (
     id          smallint PRIMARY KEY,
     type        varchar(100) NOT NULL,
     description varchar(255) NOT NULL
 );
 
+
+--attribute
 CREATE TABLE IF NOT EXISTS attribute (
     id             bigint PRIMARY KEY,
     attribute_type smallint     NOT NULL,
@@ -17,12 +22,17 @@ CREATE TABLE IF NOT EXISTS attribute (
     FOREIGN KEY (attribute_type) REFERENCES attribute_type(id)
 );
 
+
+--attribute_value
 CREATE TABLE IF NOT EXISTS attribute_value (
-    id          bigint PRIMARY KEY,
-    entity_name varchar(200) NOT NULL,
-    entity_id   bigint       NOT NULL,
-    attribute   bigint       NOT NULL,
-    value       varchar(3000),
-    value_float numeric(30, 6),
+    id             bigint PRIMARY KEY,
+    entity_name    varchar(200) NOT NULL,
+    entity_id      bigint       NOT NULL,
+    attribute      bigint       NOT NULL,
+    value_text     varchar(3000),
+    value_bool     boolean,
+    value_datetime timestamptz,
+    value_int      bigint,
+    value_float    numeric(30, 6),
     FOREIGN KEY (attribute) REFERENCES attribute(id)
 );
--- data/view.sql
+++ data/view.sql
@@ -1,22 +1,26 @@
+--service_data
 CREATE VIEW service_data AS
 SELECT f.name                                                          as "Фильм",
-       CASE WHEN av.value::date = CURRENT_DATE THEN av.value::date END AS "Задачи актуальные на сегодня",
-       CASE WHEN av.value::date >= CURRENT_DATE + INTERVAL '20 days'
-                THEN av.value::date END                                AS "Задачи актуальные через 20 дней"
+       CASE WHEN av.value_datetime::date = CURRENT_DATE THEN av.value_datetime END AS "Задачи актуальные на сегодня",
+       CASE WHEN av.value_datetime >= CURRENT_DATE + INTERVAL '20 days'
+                THEN av.value_datetime END                                AS "Задачи актуальные через 20 дней"
 FROM film f
      INNER JOIN attribute_value av
                 on av.entity_id = f.id
                     AND av.entity_name = 'Film'
-                    AND av.attribute IN (3, 4, 5)
+                    AND av.value_datetime > CURRENT_DATE
      INNER JOIN attribute a
                 on av.attribute = a.id
-ORDER BY av.value;
+ORDER BY av.value_datetime;
 
+
+--marketing_data
 CREATE VIEW marketing_data AS
-SELECT f.name                                   AS "Фильм",
-       at.description                           AS "Тип атрибута",
-       a.name                                   AS "атрибут",
-       COALESCE(av.value, av.value_float::text) AS "Значение"
+SELECT f.name                       AS "Фильм",
+       at.description               AS "Тип атрибута",
+       a.name                       AS "атрибут",
+       COALESCE(av.value_datetime::text, av.value_float::text, av.value_bool::text, av.value_text,
+                av.value_int::text) AS "Значение"
 FROM film f
      INNER JOIN attribute_value av
                 on av.entity_id = f.id
@@ -26,7 +30,7 @@ FROM film f
      INNER JOIN attribute_type at
                 on a.attribute_type = at.id;
 
-
+--query
 SELECT *
 FROM service_data;
 

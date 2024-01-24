1fcc1c10498e5f09aa25c1fe0108d6592ad6ce81
Maxim <x0ttabbl4.2@gmail.com>
2024-01-24 21:52:03 +0300

    .

---
+++ src/Infrastructure/DBGateway/Tickets.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace src\Infrastructure\DBGateway;
+
+class Tickets
+{
+    private \PDO $pdo;
+
+    private \PDOStatement $selectStmt;
+    private \PDOStatement $insertStmt;
+    private \PDOStatement $updateStmt;
+    private \PDOStatement $deleteStmt;
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+
+        $this->selectStmt = $pdo->prepare(
+            "select price, showtime_id, customer_id, seat_in_hall_id from tikets where id = ?"
+        );
+        $this->insertStmt = $pdo->prepare(
+            "insert into tickets (price, showtime_id, customer_id, seat_in_hall_id) values (?, ?, ?, ?)"
+        );
+        $this->updateStmt = $pdo->prepare(
+            "update tickets set price = ?, showtime_id = ?, customer_id = ?, seat_in_hall_id = ? where id = ?"
+        );
+        $this->deleteStmt = $pdo->prepare("delete from tickets where id = ?");
+    }
+
+    public function getById(int $id): array
+    {
+        $this->selectStmt->setFetchMode(\PDO::FETCH_ASSOC);
+        $this->selectStmt->execute([$id]);
+        return (array)$this->selectStmt->fetch();
+    }
+}

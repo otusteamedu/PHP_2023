67be7bd62af72c35c6293ab62f6c2a1bac639786
Roman.Yuzyuk <roman.yuzyuk@lenvendo.ru>
2024-01-24 19:35:32 +0300

    hw 14 - fix

--- hasCycleSolution.php
+++ hasCycleSolution.php
@@ -8,14 +8,14 @@ class Solution
      * @param ListNode $head
      * @return Boolean
      */
-    public function hasCycle($head) {
+    public function hasCycle($head)
+    {
         if ($head === null) {
             return false;
         }
 
         $slow = $head;
         $fast = $head;
-
         while ($fast != null && $fast->next !=null) {
             $slow = $slow->next;
             $fast = $fast->next->next;

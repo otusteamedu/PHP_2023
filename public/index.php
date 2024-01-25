66c43c8e8cfb1c43b1f1df42c827ed03e4618b59
Artem Cherepanov <art.cherepan@gmail.com>
2024-01-25 09:03:24 +0900

    init

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
+use App\Helpers\Functions;
+
 require_once __DIR__ . '/../vendor/autoload.php';
+
+$func = new Functions();
+
+echo $func->fractionToDecimal(1, 5);
--- src/Helpers/Functions.php
+++ src/Helpers/Functions.php
@@ -4,5 +4,12 @@ namespace App\Helpers;
 
 class Functions
 {
-
+    /**
+     * @param Integer $numerator
+     * @param Integer $denominator
+     * @return String
+     */
+    function fractionToDecimal($numerator, $denominator) {
+        return sprintf('%F', $numerator / $denominator);
+    }
 }

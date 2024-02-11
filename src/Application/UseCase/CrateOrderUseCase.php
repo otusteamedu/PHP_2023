95d18656b4cb70878000fd3511b548afe731f1fe
Maxim <x0ttabbl4.2@gmail.com>
2024-02-11 20:03:48 +0300

    fix

--- src/Application/UseCase/CrateOrderUseCase.php
+++ src/Application/UseCase/CrateOrderUseCase.php
@@ -10,8 +10,7 @@ use src\Domain\Entity\Order;
 
 class CrateOrderUseCase
 {
-    public function __construct(private OrderBuilder $orderBuilder)
-    {}
+    public function __construct(private OrderBuilder $orderBuilder){}
 
     public function __invoke(CreateOrderRequest $request): Order
     {

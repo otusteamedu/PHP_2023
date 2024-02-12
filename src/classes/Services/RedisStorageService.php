83832437f1c0671b74b20fa00bf4b059f178a26f
kirill.lobkovsky <kiphirrr@gmail.com>
2024-02-13 02:04:41 +0300

    hw12: Redis

---
+++ src/classes/Services/RedisStorageService.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Klobkovsky\App\Services;
+
+use Klobkovsky\App\Interface\RedisStorageInterface;
+use Redis;
+
+class RedisStorageService implements RedisStorageInterface
+{
+    private Redis $redisClient;
+    private string $eventsKey = 'events';
+
+    /**
+     * @throws \RedisException
+     */
+    public function __construct(Redis $redisClient, $redisHost = 'redis', $redisPort = 6379, $redisDb = 0)
+    {
+        $this->redisClient = $redisClient;
+        $this->redisClient->connect($redisHost, $redisPort);
+        $this->redisClient->select($redisDb);
+    }
+
+    /**
+     * @throws \RedisException
+     */
+    public function add($priority, $conditions, $event): void
+    {
+        $event = [
+            'priority' => $priority,
+            'conditions' => $conditions,
+            'event' => $event,
+        ];
+
+        $this->redisClient->rpush($this->eventsKey, json_encode($event));
+    }
+
+    public function get($params): array
+    {
+        $events = $this->redisClient->lrange($this->eventsKey, 0, -1);
+        $matchingEvents = [];
+
+        foreach ($events as $eventJson) {
+            $event = json_decode($eventJson, true);
+            if ($this->conditionsMatch($event['conditions'], $params)) {
+                $matchingEvents[] = $event;
+            }
+        }
+
+        if (empty($matchingEvents)) {
+            return [];
+        }
+
+        usort($matchingEvents, function ($a, $b) {
+            return $b['priority'] - $a['priority'];
+        });
+
+        return $matchingEvents[0]['event'];
+    }
+
+    /**
+     * @throws \RedisException
+     */
+    public function clear(): void
+    {
+        $this->redisClient->del($this->eventsKey);
+    }
+
+    private function conditionsMatch($eventConditions, $userParams): bool
+    {
+        foreach ($eventConditions as $param => $value) {
+            if (!isset($userParams[$param]) || $userParams[$param] != $value) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
--- src/index.php
+++ src/index.php
@@ -4,3 +4,18 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
+$redisStorageService = new Klobkovsky\App\Services\RedisStorageService(new Redis(), $_ENV['REDIS_HOST'], (int)$_ENV['REDIS_PORT']);
+
+$redisStorageService->add(1000, ['param1' => 1], ['event' => '::event1::']);
+$redisStorageService->add(2000, ['param1' => 2, 'param2' => 2], ['event' => '::event2::']);
+$redisStorageService->add(3000, ['param1' => 1, 'param2' => 2], ['event' => '::event3::']);
+
+$userQuery = ['param1' => 1, 'param2' => 2];
+
+$matchingEvent = $redisStorageService->get($userQuery);
+
+if ($matchingEvent) {
+    echo "Наиболее подходящее событие: " . json_encode($matchingEvent) . PHP_EOL;
+} else {
+    echo "Нет подходящих событий." . PHP_EOL;
+}

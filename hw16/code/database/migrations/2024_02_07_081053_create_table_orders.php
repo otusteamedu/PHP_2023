19695b1b2fa8c41aa5b8da2a2addd044ea3870d8
Григорий Карман <g.karman@mail.ru>
2024-02-15 15:36:01 +0300

    Миграция создания таблицы orders

--- hw16/code/database/migrations/2024_02_07_081053_create_table_orders.php
+++ hw16/code/database/migrations/2024_02_07_081053_create_table_orders.php
@@ -8,17 +8,19 @@ return new class extends Migration
 {
     public function up(): void
     {
-        Schema::create('logistic.orders', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('email', 255);
-            $table->string('title', 50);
-            $table->string('description', 255);
+            $table->unsignedInteger('shop_id');
+            $table->string('phone', 50)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('delivery_address', 500)->nullable();
+            $table->unsignedSmallInteger('table_tend_number')->nullable();
             $table->timestamps();
         });
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('logistic.orders');
+        Schema::dropIfExists('orders');
     }
 };

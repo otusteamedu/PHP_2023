0a4288dde12e5767c1552f6e1174541a3a0ce075
Connected1993 <59499458+Connected1993@users.noreply.github.com>
2024-02-15 12:12:27 +0300

    Update task2.php
    add Solution

--- task2.php
+++ task2.php
@@ -1,35 +1,38 @@
 <?php
-function letterCombinations($digits) {
-  if (empty($digits)) {
-    return [];
-  }
+class Solution {
+    function letterCombinations($digits) {
+        if (empty($digits)) {
+            return [];
+        }
 
-  $mapping = [
-    '2' => ['a', 'b', 'c'],
-    '3' => ['d', 'e', 'f'],
-    '4' => ['g', 'h', 'i'],
-    '5' => ['j', 'k', 'l'],
-    '6' => ['m', 'n', 'o'],
-    '7' => ['p', 'q', 'r', 's'],
-    '8' => ['t', 'u', 'v'],
-    '9' => ['w', 'x', 'y', 'z']
-  ];
+        $mapping = [
+            '2' => ['a', 'b', 'c'],
+            '3' => ['d', 'e', 'f'],
+            '4' => ['g', 'h', 'i'],
+            '5' => ['j', 'k', 'l'],
+            '6' => ['m', 'n', 'o'],
+            '7' => ['p', 'q', 'r', 's'],
+            '8' => ['t', 'u', 'v'],
+            '9' => ['w', 'x', 'y', 'z']
+        ];
 
-  $combinations = [''];
+        $combinations = [''];
 
-  for ($i = 0; $i < strlen($digits); $i++) {
-    $new_combinations = [];
-    foreach ($combinations as $combination) {
-      foreach ($mapping[$digits[$i]] as $letter) {
-        $new_combinations[] = $combination . $letter;
-      }
+        for ($i = 0; $i < strlen($digits); $i++) {
+            $new_combinations = [];
+            foreach ($combinations as $combination) {
+                foreach ($mapping[$digits[$i]] as $letter) {
+                    $new_combinations[] = $combination . $letter;
+                }
+            }
+            $combinations = $new_combinations;
+        }
+
+        return $combinations;
     }
-    $combinations = $new_combinations;
-  }
-
-  return $combinations;
 }
 
 // Пример использования:
-// $digits = "23";
-// print_r(letterCombinations($digits)); // Выведет ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+$solution = new Solution();
+$digits = "23";
+print_r($solution->letterCombinations($digits)); // Выведет ["ad","ae","af","bd","be","bf","cd","ce","cf"]

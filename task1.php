fb9881e664d4e7b937b9b2bc6c4b85fc3abf7945
Connected1993 <59499458+Connected1993@users.noreply.github.com>
2024-02-15 12:09:56 +0300

    Update task1.php
    fix Solution

--- task1.php
+++ task1.php
@@ -1,37 +1,43 @@
 <?php
-class ListNode {
-  public $val = 0;
-  public $next = null;
-  function __construct($val = 0, $next = null) {
-    $this->val = $val;
-    $this->next = $next;
-  }
+class MyListNode {
+    public $val = 0;
+    public $next = null;
+
+    function __construct($val = 0, $next = null) {
+        $this->val = $val;
+        $this->next = $next;
+    }
 }
 
-function hasCycle($head) {
-  if ($head == null || $head->next == null) {
-    return false;
-  }
-
-  $slow = $head;
-  $fast = $head->next;
-
-  while ($fast != null && $fast->next != null) {
-    if ($slow == $fast) {
-      return true;
+class Solution {
+    function hasCycle($head) {
+        if ($head == null || $head->next == null) {
+            return false;
+        }
+        
+        $slow = $head;
+        $fast = $head;
+        
+        while ($fast != null && $fast->next != null) {
+            $slow = $slow->next;
+            $fast = $fast->next->next;
+            
+            if ($slow === $fast) {
+                return true; // Найден цикл
+            }
+        }
+        
+        return false; // Цикл не найден
     }
-    $slow = $slow->next;
-    $fast = $fast->next->next;
-  }
-
-  return false;
 }
 
 // Пример использования:
-// $head = new ListNode(3);
-// $head->next = new ListNode(2);
-// $head->next->next = new ListNode(0);
-// $head->next->next->next = new ListNode(-4);
-// $head->next->next->next->next = $head->next; // Делаем цикл: -4 -> 2
+$head = new MyListNode(3);
+$head->next = new MyListNode(2);
+$head->next->next = new MyListNode(0);
+$head->next->next->next = new MyListNode(-4);
+$head->next->next->next->next = $head->next; // Делаем цикл: -4 -> 2
 
-// var_dump(hasCycle($head)); // Вернет true
+$solution = new Solution();
+var_dump($solution->hasCycle($head)); // Вернет true
+

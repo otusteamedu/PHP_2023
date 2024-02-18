3b8a8f4826e5140e2603602637bfedbfc86a7387
PetrovSpace <vovapetrov94@gmail.com>
2024-02-19 00:39:26 +0300

    start env

--- profitable_movie.sql
+++
@@ -1,8 +0,0 @@
-SELECT films.name, SUM(t.price) AS total_price
-FROM films
-         JOIN cinema_session cs ON films.id = cs.film_id
-         JOIN tickets t ON cs.id = t.cinema_session_id
-GROUP BY name
-ORDER BY total_price DESC
-LIMIT 1;
-

774655af2676e7c6ee65deaa6716e86b6c1f3cac
daniel <palm6991@gmail.com>
2024-03-28 14:55:39 +0100

    done

--- public/worker.php
+++ public/worker.php
@@ -5,12 +5,20 @@ require __DIR__ . '/../vendor/autoload.php';
 use Api\Daniel\Services\FileManager;
 use Api\Daniel\Handlers\TaskHandler;
 use Api\Daniel\Services\RabbitMQClient;
+use Dotenv\Dotenv;
 
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$rabbitMQHost = $_ENV['RABBITMQ_HOST'];
+$rabbitMQPort = $_ENV['RABBITMQ_PORT'];
+$rabbitMQUser = $_ENV['RABBITMQ_USER'];
+$rabbitMQPassword = $_ENV['RABBITMQ_PASSWORD'];
 
 $fileManager = new FileManager();
 $taskHandler = new TaskHandler($fileManager, __DIR__ . '/../data/statuses.json');
 
-$rabbitMQClient = new RabbitMQClient('rabbitmq', 5672, 'user', 'password');
+$rabbitMQClient = new RabbitMQClient($rabbitMQHost, $rabbitMQPort, $rabbitMQUser, $rabbitMQPassword);
 $rabbitMQClient->declareQueue('task_queue');
 
 $callback = function($msg) use ($taskHandler) {
--- src/index.php
+++ src/index.php
@@ -5,8 +5,17 @@ require __DIR__ . '/../vendor/autoload.php';
 use Api\Daniel\Services\RabbitMQClient;
 use Api\Daniel\Services\FileManager;
 use Api\Daniel\Handlers\RequestHandler;
+use Dotenv\Dotenv;
 
-$rabbitMQClient = new RabbitMQClient('rabbitmq', 5672, 'user', 'password');
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$rabbitMQHost = $_ENV['RABBITMQ_HOST'];
+$rabbitMQPort = $_ENV['RABBITMQ_PORT'];
+$rabbitMQUser = $_ENV['RABBITMQ_USER'];
+$rabbitMQPassword = $_ENV['RABBITMQ_PASSWORD'];
+
+$rabbitMQClient = new RabbitMQClient($rabbitMQHost, $rabbitMQPort, $rabbitMQUser, $rabbitMQPassword);
 $rabbitMQClient->declareQueue('task_queue');
 
 $fileManager = new FileManager();

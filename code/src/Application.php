4f8cbd2be09d4ec258ec2ced671f8b24f5cfc4df
Dmitriy Matrenin <levitanoff@gmail.com>
2024-04-02 23:02:20 +0400

    Домашнее задание к уроку №5

---
+++ code/src/Application.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Dmatrenin\Bracket;
+
+use Exception;
+
+
+class Application
+{
+    public static function run() {
+        try {
+            http_response_code(200);
+            echo Validator::validate($_POST['string']);
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            echo $e->getMessage();
+        }
+    }
+}
---
+++ code/src/index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use Dmatrenin\Bracket\Application;
+
+require_once "../vendor/autoload.php";
+
+Application::run();
+

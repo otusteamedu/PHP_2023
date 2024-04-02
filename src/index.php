3f800bb8374489e84e2c84889e5d4fc4f00a8d61
daniel <palm6991@gmail.com>
2024-04-02 09:36:13 +0200

    fixed

--- src/index.php
+++ src/index.php
@@ -2,23 +2,7 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Api\Daniel\Services\RabbitMQClient;
-use Api\Daniel\Services\FileManager;
-use Api\Daniel\Handlers\RequestHandler;
-use Dotenv\Dotenv;
+use Api\Daniel\App;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
-$rabbitMQHost = $_ENV['RABBITMQ_HOST'];
-$rabbitMQPort = $_ENV['RABBITMQ_PORT'];
-$rabbitMQUser = $_ENV['RABBITMQ_USER'];
-$rabbitMQPassword = $_ENV['RABBITMQ_PASSWORD'];
-
-$rabbitMQClient = new RabbitMQClient($rabbitMQHost, $rabbitMQPort, $rabbitMQUser, $rabbitMQPassword);
-$rabbitMQClient->declareQueue('task_queue');
-
-$fileManager = new FileManager();
-$requestHandler = new RequestHandler($rabbitMQClient, $fileManager, __DIR__ . '/../data');
-
-$requestHandler->handleRequest();
+$app = new App();
+$app->run();

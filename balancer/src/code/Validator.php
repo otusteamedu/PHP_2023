0bbd5e43308478e12314e65d1b7cba96444f2914
Anton Shishak <ashishak@awardwallet.com>
2024-04-08 19:27:16 +0300

    Домашнее задание №4 - приведению к стандарту написания кода в github

--- balancer/src/code/Validator.php
+++ balancer/src/code/Validator.php
@@ -1,31 +1,31 @@
 <?php
 declare(strict_types=1);
+
 namespace Ashishak\Balancer\code;
 
 class Validator
 {
-    public static function ValidateText(string $postVar): bool{
+    public static function ValidateText(string $postVar): bool
+    {
         $counter = 0;
 
         $postVarArray = str_split($postVar);
 
-        foreach ($postVarArray as $value){
-            if ($value == '('){
+        foreach ($postVarArray as $value) {
+            if ($value == '(') {
                 $counter++;
-            } elseif($value == ')'){
+            } elseif ($value == ')') {
                 $counter--;
             }
-            if ($counter < 0){
+            if ($counter < 0) {
                 return false;
             }
         }
 
-        if ($counter === 0){
+        if ($counter === 0) {
             return true;
         } else {
             return false;
         }
-
     }
-
 }

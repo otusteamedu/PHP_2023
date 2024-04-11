50066bd4f5350dd5c4e37aaa9b3abebabd13ee04
Nikita Menshutin <info@nikita.global>
2024-04-11 19:21:49 +0300

    chore(hw31): done

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
--- app/src/Controller/ProjectController.php
+++ app/src/Controller/ProjectController.php
@@ -10,11 +10,30 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Project;
 use App\Message\QueueProject;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Attributes as OA;
 
 #[Route('/api', name: 'api_')]
 class ProjectController extends AbstractController
 {
+    /**
+     * Get all projects.
+     * 
+     * This call takes into account all confirmed awards, but not pending or refused awards.
+
+     */
     #[Route('/projects', name: 'project_index', methods:['get'])]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns the projects',
+        content: new OA\JsonContent(
+            type: 'array',
+            items: new OA\Items(ref: new Model(type: Project::class, groups: ['full']))
+        )
+    )]
+    #[OA\Tag(name: 'projects')]
+    #[Security(name: 'Bearer')]
     public function index(EntityManagerInterface $entityManager): JsonResponse
     {
         $products = $entityManager
@@ -34,9 +53,37 @@ class ProjectController extends AbstractController
 
         return $this->json($data);
     }
-
-
-    #[Route('/projects', name: 'project_create', methods:['post'])]
+    
+    #[Route('/projects', name: 'project_create', methods:['POST'])]
+    #[OA\RequestBody(
+        content: [new OA\MediaType(mediaType: "application/x-www-form-urlencoded",
+            schema: new OA\Schema(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "name", type: "string"),
+                    new OA\Property(property: "description", type: "string")
+                ],
+                required: ["name", "description"]
+            )
+        )]
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns the created project',
+        content: [new OA\MediaType(mediaType: "application/json",
+            schema: new OA\Schema(
+                type: "object",
+                properties: [
+                    new OA\Property(property: "id", type: "integer"),
+                    new OA\Property(property: "name", type: "string"),
+                    new OA\Property(property: "description", type: "string"),
+                    new OA\Property(property: "status", type: "string")
+                ]
+            )
+        )]
+    )]
+    #[OA\Tag(name: 'projects')]
+    #[Security(name: 'Bearer')]
     public function create(EntityManagerInterface $entityManager, Request $request, MessageBusInterface $bus): JsonResponse
     {
         $project = new Project();
@@ -59,8 +106,27 @@ class ProjectController extends AbstractController
     }
 
 
-    #[Route('/projects/{id}', name: 'project_show', methods:['get'])]
-    public function show(EntityManagerInterface $entityManager, int $id): JsonResponse
+   
+    #[Route('/projects/{id}', name: 'project_show', methods:['GET'])]
+    #[OA\Parameter(
+        name: "id",
+        in: "path",
+        description: "The ID of the project",
+        required: true,
+        schema: new OA\Schema(type: 'integer')
+    )]
+    #[OA\Response(
+        response: 200,
+        description: 'Returns the project details',
+        content: [new OA\MediaType(mediaType: "application/json")]
+    )]
+    #[OA\Response(
+        response: 404,
+        description: 'Project not found'
+    )]
+    #[OA\Tag(name: 'projects')]
+    #[Security(name: 'Bearer')]
+   public function show(EntityManagerInterface $entityManager, int $id): JsonResponse
     {
         $project = $entityManager->getRepository(Project::class)->find($id);
 
@@ -78,40 +144,5 @@ class ProjectController extends AbstractController
         return $this->json($data);
     }
 
-    #[Route('/projects/{id}', name: 'project_update', methods:['put', 'patch'])]
-    public function update(EntityManagerInterface $entityManager, Request $request, int $id): JsonResponse
-    {
-        $project = $entityManager->getRepository(Project::class)->find($id);
-
-        if (!$project) {
-            return $this->json('No project found for id ' . $id, 404);
-        }
-
-        $project->setName($request->request->get('name'));
-        $project->setDescription($request->request->get('description'));
-        $entityManager->flush();
-
-        $data =  [
-            'id' => $project->getId(),
-            'name' => $project->getName(),
-            'description' => $project->getDescription(),
-        ];
-
-        return $this->json($data);
-    }
-
-    #[Route('/projects/{id}', name: 'project_delete', methods:['delete'])]
-    public function delete(EntityManagerInterface $entityManager, int $id): JsonResponse
-    {
-        $project = $entityManager->getRepository(Project::class)->find($id);
-
-        if (!$project) {
-            return $this->json('No project found for id ' . $id, 404);
-        }
-
-        $entityManager->remove($project);
-        $entityManager->flush();
-
-        return $this->json('Deleted a project successfully with id ' . $id);
-    }
 }
+

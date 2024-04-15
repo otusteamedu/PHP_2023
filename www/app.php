f586b9363f9655d0fe5228d4c122c48b03995fe5
Dmitriy Repin <djdimakos@gmail.com>
2024-04-16 00:12:41 +0300

    hw6

---
+++ www/app.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Singurix\Chat\App;
+
+spl_autoload_register(function (string $className) {
+    require_once __DIR__ . '/src/' . str_replace('\\', '/', $className) . '.php';
+});
+
+try {
+    $app = new App($argv);
+    $app->start();
+} catch (Exception $e) {
+    fputs(STDOUT, $e->getMessage()."\n");
+}

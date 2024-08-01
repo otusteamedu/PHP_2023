85d93710cc5b5d18336c729f28aebcf0ef6536ee
PetrovSpace <vovapetrov94@gmail.com>
2024-08-01 12:42:53 +0300

    example

--- code/index.php
+++ code/index.php
@@ -25,10 +25,20 @@ try {
     echo $exception->getMessage();
 }
 
-echo "<br><br>";
-echo "Привет, Otus!<br>" . date("Y-m-d H:i:s") . "<br><br>";
+function writeLogFile($string, $clear = false): void
+{
+    $log_file_name = __DIR__ . "/message.txt";
+    $now = date("Y-m-d H:i:s");
+    if ($clear == false) {
+        file_put_contents($log_file_name, $now . " " . print_r($string, true) . "\r\n", FILE_APPEND);
+    } else {
+        file_put_contents($log_file_name, '');
+        file_put_contents($log_file_name, $now . " " . print_r($string, true) . "\r\n", FILE_APPEND);
+    }
+}
 
-echo "А тут изменился код";
+$data = file_get_contents('php://input');
+writeLogFile($data, true);
 
-phpinfo();
+// phpinfo();
 
